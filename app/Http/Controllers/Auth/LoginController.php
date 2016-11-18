178bd77ebeaa831ca8e014bbb70478b67145a801
Prateek Tulsyan <prateek@tagtaste.com>
2016-11-18 16:01:14 +0530

    pushed login and signup functionality with is active complete

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -25,7 +29,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -35,5 +39,42 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest', ['except' => 'logout']);
+    }
+
+    /**
+     * Show the application login form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * validates login.
+     *
+     * @param Request $value login form data
+     *
+     * @return [type] [description]
+     */
+    public function doLogin(Request $request)
+    {
+        if (Auth::attempt(['email' => $request['email'], 'password' => $request['password'], 'is_active' => "1"])) {
+            Session::put('admin', User::find(1));
+            return redirect($this->getRedirectPath());
+        } else {
+            return redirect('/login');
+        }
+    }
+
+    /**
+     * [getRedirectPath returns redirect path according to user role.
+     *
+     * @return [string] redirectPath
+     */
+    public function getRedirectPath()
+    {
+        return '/home';
     }
 }
