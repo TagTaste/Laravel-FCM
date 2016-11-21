2d47c268817966310ff80e3ede1f9fcf45a6f437
Amitabh Dhiwal <amitabh@tagtaste.com>
2016-11-21 14:40:45 +0530

    profile list order

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -17,7 +17,7 @@ class ProfileController extends Controller {
 	 */
 	public function index()
 	{
-		$profileTypes = ProfileType::all();
+		$profileTypes = ProfileType::orderBy('type','asc')->get();
 
 		$profiles = Profile::with('type','attribute')->orderBy('id', 'asc')->paginate(10);
 
