b56d40ce18fa1e878dce58f8d52946eb5e8c3774
Amitabh Dhiwal <amitabh@tagtaste.com>
2016-11-21 12:20:24 +0000

    add routes group

--- routes/web.php
+++ routes/web.php
@@ -26,16 +26,19 @@ Route::get('login', ['middleware' => ['web'], 'as' => 'login', 'uses' => 'Auth\L
 Route::post('verifyLogin', ['middleware' => ['web'], 'as' => 'login', 'uses' => 'Auth\LoginController@doLogin']);
 Route::get('logout', ['middleware' => ['web'], 'as' => 'logout', 'uses' => 'Auth\LoginController@logout']);
 
-Route::resource("profile_types","ProfileTypeController");
+Route::group(['middleware'=>'auth'],function(){
+	Route::resource("profile_types","ProfileTypeController");
 
-Route::resource("template_types","TemplateTypeController");
-Route::resource("templates","TemplateController");
-Route::resource("profile_attributes","ProfileAttributeController");
+	Route::resource("template_types","TemplateTypeController");
+	Route::resource("templates","TemplateController");
+	Route::resource("profile_attributes","ProfileAttributeController");
 
-Route::get("profile/file/{filename}",['as'=>'profile.fileDownload','uses'=>'ProfileController@fileDownload']);
-Route::get("profile/form/{typeId}",['as'=>'profile.form','uses'=>"ProfileAttributeController@form"]);
-Route::resource("profiles","ProfileController");
+	Route::get("profile/file/{filename}",['as'=>'profile.fileDownload','uses'=>'ProfileController@fileDownload']);
+	Route::get("profile/form/{typeId}",['as'=>'profile.form','uses'=>"ProfileAttributeController@form"]);
+	Route::resource("profiles","ProfileController");
 
-Route::get("follow/{chefId}", ['as'=>'chef.follow','uses'=>'FollowerController@follow']);
-Route::get("unfollow/{chefId}", ['as' => 'chef.unfollow', 'uses'=>'FollowerController@unfollow']);
-Route::resource("followers","FollowerController");
+	Route::get("follow/{chefId}", ['as'=>'chef.follow','uses'=>'FollowerController@follow']);
+	Route::get("unfollow/{chefId}", ['as' => 'chef.unfollow', 'uses'=>'FollowerController@unfollow']);
+	Route::resource("followers","FollowerController");
+});
+
