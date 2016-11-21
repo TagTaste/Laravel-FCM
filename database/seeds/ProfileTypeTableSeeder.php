8c2b8da4375d15c51e0a945c15444d6142196e6d
Amitabh Dhiwal <amitabh@tagtaste.com>
2016-11-21 09:32:40 +0000

    fix array

--- database/seeds/ProfileTypeTableSeeder.php
+++ database/seeds/ProfileTypeTableSeeder.php
@@ -11,11 +11,11 @@ class ProfileTypeTableSeeder extends Seeder {
     public function run()
     {
         ProfileType::insert(
-        	['type' => 'Chef', 'enabled' => 1, 'default' => 0]);
-        	['type' => 'Foodie', 'enabled' => 1, 'default' => 1]);
-        	['type' => 'Outlet', 'enabled' => 1, 'default' => 0]);
-        	['type' => 'Ingredients', 'enabled' => 1, 'default' => 0]);
-        	['type' => 'Expert', 'enabled' => 1, 'default' => 0]);
+        	[['type' => 'Chef', 'enabled' => 1, 'default' => 0],
+        	['type' => 'Foodie', 'enabled' => 1, 'default' => 1],
+        	['type' => 'Outlet', 'enabled' => 1, 'default' => 0],
+        	['type' => 'Ingredients', 'enabled' => 1, 'default' => 0],
+        	['type' => 'Expert', 'enabled' => 1, 'default' => 0]]);
     }
 
 }
