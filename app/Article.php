8a8742da3d7ebb311450f6c453008a6ba8bcf105
Amitabh Dhiwal <amitabh@tagtaste.com>
2016-11-22 07:43:55 +0000

    add relationships

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    protected $fillable = ['title','author_id','privacy_id','comments_enabled','status','template_id'];
+
+   	public function author() {
+   		return $this->belongsTo('\App\Profile','author_id');
+   	}
+
+   	public function privacy(){
+   		return $this->belongsTo('\App\Privacy','privacy_id');
+   	}
+
+   	public function template() {
+   		return $this->belongsTo('\App\Template','template_id');
+   	}
 }
