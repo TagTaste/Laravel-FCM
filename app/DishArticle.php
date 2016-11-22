d2e96c8ac9450b02f54ffd24a9ead6ab4e304bdc
Amitabh Dhiwal <amitabh@tagtaste.com>
2016-11-22 07:45:46 +0000

    add dish relationships

--- app/DishArticle.php
+++ app/DishArticle.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class DishArticle extends Model
 {
-    //
+    protectec $fillable = ['showcase','hasRecipe','article_id','chef_id'];
+
+    public function article() {
+    	return $this->belongsTo('\App\Article','article_id');
+    }
+
+    public function chef() {
+    	return $this->belongsTo('\App\Profile','chef_id');
+    }
 }
