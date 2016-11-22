0dbcdb4c5dd607faf3f687f33e6085a674d3940f
Amitabh Dhiwal <amitabh@tagtaste.com>
2016-11-22 10:35:06 +0000

    update inputs

--- resources/views/recipe_articles/forms/create.blade.php
+++ resources/views/recipe_articles/forms/create.blade.php
@@ -1,10 +1,10 @@
-<!-- <div class="form-group @if($errors->has('dish_id')) has-error @endif">
+<div class="form-group @if($errors->has('dish_id')) has-error @endif">
  <label for="dish_id-field">Dish_id</label>
  <input type="text" id="dish_id-field" name="dish_id" class="form-control" value="{{ old("dish_id") }}"/>
  @if($errors->has("dish_id"))
  <span class="help-block">{{ $errors->first("dish_id") }}</span>
  @endif
-</div> -->
+</div>
 <div class="form-group @if($errors->has('step')) has-error @endif">
  <label for="step-field">Step</label>
  <input type="text" id="step-field" name="step" class="form-control" value="{{ old("step") }}"/>
@@ -19,9 +19,15 @@
  <span class="help-block">{{ $errors->first("content") }}</span>
  @endif
 </div>
-
+<div class="form-group @if($errors->has('template_id')) has-error @endif">
+ <label for="template_id-field">Template_id</label>
+ <input type="text" id="template_id-field" name="template_id" class="form-control" value="{{ old("template_id") }}"/>
+ @if($errors->has("template_id"))
+ <span class="help-block">{{ $errors->first("template_id") }}</span>
+ @endif
+</div>
 <div class="form-group @if($errors->has('parent_id')) has-error @endif">
- <label for="parent_id-field">Parent</label>
+ <label for="parent_id-field">Parent_id</label>
  <input type="text" id="parent_id-field" name="parent_id" class="form-control" value="{{ old("parent_id") }}"/>
  @if($errors->has("parent_id"))
  <span class="help-block">{{ $errors->first("parent_id") }}</span>
