8f7150fca19041b1205bdbfacdd185f0670d09a4
Amitabh Dhiwal <amitabh@tagtaste.com>
2016-11-22 07:47:46 +0000

    add recipe relationships

--- app/RecipeArticle.php
+++ app/RecipeArticle.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class RecipeArticle extends Model
 {
-    //
+    $fillable = ['dish_id','step','content','template_id','parent_id'];
+
+    public function dish() {
+    	return $this->belongsTo('\App\DishRecipe','dish_id');
+    }
+
+    public function template(){
+    	return $this->belongsTo('\App\Template','template_id');
+    }
+
+    public function parent() {
+    	return $this->belongsTo('\App\RecipeArticle','parent_id');
+    }
 }
