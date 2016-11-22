e450202d5ece7d856d5cc7acf27159a89f9e9316
Amitabh Dhiwal <amitabh@tagtaste.com>
2016-11-22 17:25:48 +0530

    add another layout for dish

---
+++ resources/views/templates/article_dish2.blade.php
@@ -0,0 +1,9 @@
+<div class="col-md-12">
+        <div class="col-md-12">
+        	<h2> {{ $dish->article->title }} </h2>
+        </div>
+        <div class="col-md-9 col-md-offset-3">
+        	<p class='text-justify text-align-left'> {!! $dish->content !!}</p>
+        	
+        </div>
+</div>
