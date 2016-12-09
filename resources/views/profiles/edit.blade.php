343fd1847640b837c2aa2c531ddc4ec825e3baf4
Amitabh Dhiwal <amitabh@tagtaste.com>
2016-12-09 14:09:13 +0000

    update edit view

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -1,6 +1,7 @@
 @extends('layout')
 @section('css')
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/css/bootstrap-datepicker.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/css/bootstrap-datepicker.css"
+          rel="stylesheet">
 @endsection
 @section('header')
     <div class="page-header">
@@ -15,75 +16,79 @@
         <div class="col-md-12">
 
             <form action="{{ route('profiles.updateIndividual') }}" method="post">
-              {{ csrf_field() }}
-              <input type="hidden" name="typeId" value="{{ $typeId }}">
-              @foreach($profileAttributes as $attribute)
-                @php
-                  $value = $profile->get($attribute->id);
+                {{ csrf_field() }}
+                <input type="hidden" name="typeId" value="{{ $typeId }}">
+                @foreach($profileAttributes as $attribute)
+                    @php
+                        $value = $profile->get($attribute->id);
+                        $name = "attributes[$attribute->id]";
+                        $inputValue = null;
+                        if($value){
+                          if($attribute->inputType("text") || $attribute->inputType("textarea")){
+                            $v = $value->first();
 
-                  if($value){
-                    if($attribute->input_type == "text"){
-                      $value = $value->first();
+                                if($v){
+                                  $name = "profile[{$v->id}]";
+                                  $inputValue = $v->value;
+                                }
 
-                    } else {
-                      $value = $value->keyBy('value_id');
-                    }
 
-                  }
-                @endphp
-                <div class="form-group">
-                    <label> {{ $attribute->label}}</label>
-
-                    @if($attribute->input_type == "text")
-                        @php
-                          $name = "attributes[$attribute->id]";
-                          $inputValue = null;
-                          if($value){
-                            $name = "profile[{$value->id}]";
-                            $inputValue = $value->value;
+                          } else {
+                            $value = $value->keyBy('value_id');
                           }
-                        @endphp
-                    
-                      <input class="form-control" type="text" name="{{ $name }}" value="{{$inputValue}}" />
 
-                    @elseif($attribute->input_type == "checkbox")
-                          
-                          @foreach($attribute->values as $attributeValue)
-                            @php
-                              if($value){
-                                $profileValue = $value->get($attributeValue->id);
-                              }
-                              
-                              $checked = null;
-                              $name = "attributes[{$attribute->id}][value_id][]";
-                              
-                              if(isset($profileValue)) {
-                                $checked = "checked";
-                                $name = "profile[{$profileValue->id}][value_id][]";
-                              }
-                            @endphp
-                           
-                              <input name="{{ $name }}" type="checkbox" value="{{ $attributeValue->id }}" {{$checked}}> 
+                        }
+                    @endphp
+                    <div class="form-group">
+                        <label> {{ $attribute->label}}</label>
 
-                            
-                              {{ $attributeValue->name }} <br/>
+                        @if($attribute->inputType("text"))
 
-                          @endforeach
-                   @endif
+                            <input class="form-control" type="text" name="{{ $name }}" value="{{$inputValue}}"/>
+
+                        @elseif($attribute->inputType("textarea"))
+                            <textarea class="form-control" name="{{ $name }}" id="" cols="30"
+                                      rows="10">{{$inputValue}}</textarea>
+                        @elseif($attribute->inputType('file'))
+                            <input type="file" name="{{ $name }}" id="">
+                        @elseif($attribute->inputType("checkbox"))
+
+                            @foreach($attribute->values as $attributeValue)
+                                @php
+                                    if($value){
+                                      $profileValue = $value->get($attributeValue->id);
+                                    }
+
+                                    $checked = null;
+                                    $name = "attributes[{$attribute->id}][value_id][]";
+
+                                    if(isset($profileValue)) {
+                                      $checked = "checked";
+                                      $name = "profile[{$profileValue->id}][value_id][]";
+                                    }
+                                @endphp
+
+                                <input name="{{ $name }}" type="checkbox"
+                                       value="{{ $attributeValue->id }}" {{$checked}}>
+
+
+                                {{ $attributeValue->name }} <br/>
+
+                            @endforeach
+                        @endif
+                    </div>
+                @endforeach
+                <div class="form-group">
+                    <input class="btn btn-primary" type="submit" value="Save">
                 </div>
-              @endforeach
-              <div class="form-group">
-                <input class="btn btn-primary" type="submit" value="Save">
-              </div>
             </form>
 
         </div>
     </div>
 @endsection
 @section('scripts')
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/js/bootstrap-datepicker.min.js"></script>
-  <script>
-    $('.date-picker').datepicker({
-    });
-  </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/js/bootstrap-datepicker.min.js"></script>
+    <script>
+        $('.date-picker').datepicker({});
+    </script>
 @endsection
