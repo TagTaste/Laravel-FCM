2fecebb6fcd2d862a742c563ac32f6bbda1d34bf
Amitabh Dhiwal <amitabh@tagtaste.com>
2016-12-14 17:03:25 +0530

    update seeder with proper values

--- database/seeds/DefaultAdminValues.php
+++ database/seeds/DefaultAdminValues.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class DefaultAdminValues extends Seeder
 {
@@ -11,12 +12,10 @@ class DefaultAdminValues extends Seeder
      */
     public function run()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-    	DB::table('users')->truncate();
-    	DB::table('users')->insert([
-            'name' => 'Admin User',
+    	User::create([
+            'name' => 'Admin',
             'email' => 'admin@tagtaste.com',
-            'password' => '$2y$10$J0Vn9d63L1zTV2m6UMbItOEJkq3sh46caEmeRIvlCYXSfkbLzM3fa',
+            'password' => bcrypt('shadowqwerty'),
         ]);
     }
 }
