d8ef1b372ad06c876a686422f3a276d1418da871
Amitabh Dhiwal <amitabh@tagtaste.com>
2016-12-15 12:59:01 +0530

    add privacy seeder

--- database/seeds/PrivacyTableSeeder.php
+++ database/seeds/PrivacyTableSeeder.php
@@ -4,12 +4,16 @@ use Illuminate\Database\Seeder;
 
 // composer require laracasts/testdummy
 use Laracasts\TestDummy\Factory as TestDummy;
-
+use App\Privacy;
 class PrivacyTableSeeder extends Seeder {
 
     public function run()
     {
-        // TestDummy::times(20)->create('App\Post');
+        Privacy::insert([
+            ['name'=>'Public'],
+            ['name'=>'Network'],
+            ['name'=>'Private'],
+        ]);
     }
 
 }
