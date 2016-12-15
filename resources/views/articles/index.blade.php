6775ba6974e784fbe5d047b8be2c34cda346156d
Amitabh Dhiwal <amitabh@tagtaste.com>
2016-12-15 13:01:31 +0530

    remove duplicate code

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -13,33 +13,6 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-12">
-            <div class="col-md-9">
-                <h2>Dishes</h2>
-                @foreach($dishes as $dish)
-                    <div class="col-md-12">
-                        <h3> {{ $dish->article-> title }} </h3>
-                        <p class='text-justify'> {!! $dish->content !!}</p>
-                        @if(!is_null($dish->recipe))
-                            <h3> Recipe </h3>
-                            <ul>
-                                @foreach($dish->recipe as $recipe)
-                                    <li>
-                                        <h4> Step {{ $recipe->step }} </h4>
-                                        <p> {{ $recipe->content }} </p>
-                                    </li>
-                                @endforeach
-                            </ul>
-                         @endif
-                    </div>
-                    <hr>
-                @endforeach
-            </div>
-
-            
-
-        </div>
-
         <div class="col-md-9">
             @foreach($dishes as $dish)
                 @include($dish->article->template->view,$dish)
