621ff648fe0f92d78f47596e5267255099410edf
Amitabh Dhiwal <amitabh@tagtaste.com>
2016-12-16 17:01:55 +0530

    update fillable

--- app/User.php
+++ app/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'is_active', 'social_provider', 'social_provider_id',
+        'name', 'email', 'password', 'is_active', 'social_registration'
     ];
 
     /**
