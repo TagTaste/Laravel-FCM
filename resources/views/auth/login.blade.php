485beada822458844deb4b3894195196465773c8
Amitabh Dhiwal <amitabh@tagtaste.com>
2016-12-17 12:21:58 +0530

    change login layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,33 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/verifyLogin') }}" enctype="multipart/form-data">
-                        {{ csrf_field() }}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 col-md-offset-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading text-center">Login</div>
+                    <div class="panel-body">
+                        <form class="form-horizontal col-md-12" role="form" method="POST"
+                              action="{{ url('/verifyLogin') }}" enctype="multipart/form-data">
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            {{ csrf_field() }}
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <label for="email" class="">E-Mail Address</label>
+
+                                <input id="email" type="email" class="form-control" name="email"
+                                       value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
+
                             </div>
-                        </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
+                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                <label for="password" class="">Password</label>
                                 <input id="password" type="password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -35,41 +35,43 @@
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
-                            </div>
-                        </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
+                            </div>
+
+
+                            <div class="form-group">
+                                <div class="col-md-12 ">
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" name="remember"> Remember Me
+                                        </label>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
+                            <div class="form-group">
+                                <div class="col-md-12">
+                                    <button type="submit" class="btn btn-primary">
+                                        Login
+                                    </button>
 
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">
-                                    Forgot Your Password?
-                                </a>
+                                    <a class="btn btn-link" href="{{ url('/password/reset') }}">
+                                        Forgot Your Password?
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <a class="btn btn-primary" href="{{ route('social.login', ['facebook']) }}">Facebook</a>
-                                <a class="btn btn-primary" href="{{ route('social.login', ['google']) }}">Google</a>
-                                <a class="btn btn-primary" href="{{ route('social.login', ['instagram']) }}">Instagram</a>
+                            <div class="form-group text-center" style="border-top:thin solid #dedede">
+                                <div class="col-md-12" style="margin-top:2em;">
+                                    <a class="btn btn-primary"
+                                       href="{{ route('social.login', ['facebook']) }}">Facebook</a>
+                                    <a class="btn btn-primary" href="{{ route('social.login', ['google']) }}">Google</a>
+                                    <a class="btn btn-primary" href="{{ route('social.login', ['instagram']) }}">Instagram</a>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
