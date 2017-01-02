68834b4479afd0c1ce9309c185a0b76908ef83f1
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-01-02 15:23:30 +0530

    update relationships

--- app/Product.php
+++ app/Product.php
@@ -9,4 +9,12 @@ class Product extends Model
 	static $imagePath = 'app/product_images/';
 
     protected $fillable = ['name','price','image','moq'];
+
+    public function user(){
+    	return $this->belongsTo('\App\User');
+    }
+
+    public function profileType(){
+    	return $this->belongsTo('\App\ProfileType');
+    }
 }
