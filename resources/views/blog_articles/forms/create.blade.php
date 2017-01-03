4d23d7e2c4535288f5d6bcee7ee3dfffb1085479
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-01-03 16:40:22 +0530

    fix inputs

--- resources/views/blog_articles/forms/create.blade.php
+++ resources/views/blog_articles/forms/create.blade.php
@@ -1,13 +1,13 @@
 <div class="form-group @if($errors->has('content')) has-error @endif">
                        <label for="content-field">Content</label>
-                    <textarea class="form-control" id="content-field" rows="3" name="content">{{ old("content") }}</textarea>
+                    <textarea class="form-control" id="content-field" rows="3" name="blog[content]">{{ old("content") }}</textarea>
                        @if($errors->has("content"))
                         <span class="help-block">{{ $errors->first("content") }}</span>
                        @endif
                     </div>
                     <div class="form-group @if($errors->has('image')) has-error @endif">
                        <label for="image-field">Image</label>
-                    <input type="text" id="image-field" name="image" class="form-control" value="{{ old("image") }}"/>
+                    <input type="text" id="image-field" name="blog[image]" class="form-control" value="{{ old("image") }}"/>
                        @if($errors->has("image"))
                         <span class="help-block">{{ $errors->first("image") }}</span>
                        @endif
