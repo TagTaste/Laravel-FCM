c95a0265c7d4fdaa1ab7063e5b817f5602447441
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-01-03 17:16:52 +0530

    add relationship

--- app/BlogArticle.php
+++ app/BlogArticle.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class BlogArticle extends Model
 {
     protected $fillable = ['content','image','article_id'];
+
+    public function article() {
+        return $this->belongsTo('\App\Article','article_id');
+    }
 }
