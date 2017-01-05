c62e3965e407bdb85466a7b8c0a35b57f7bd264d
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-01-05 16:43:50 +0530

    autoindent

--- resources/views/ideabooks/ideas.blade.php
+++ resources/views/ideabooks/ideas.blade.php
@@ -1,6 +1,7 @@
 @extends('layout')
 @section('css')
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/css/bootstrap-datepicker.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.0/css/bootstrap-datepicker.css"
+          rel="stylesheet">
 @endsection
 @section('header')
     <div class="page-header">
@@ -12,29 +13,36 @@
     @include('error')
 
     <div class="row">
-       <div class="col-md-6 col-md-push-3">
-           <ul class="list-unstyled">
-               @foreach($articles as $article)
-                   <li>
-                      <div class="row">
-                          <div class="col-md-12">
-                              <h2><a href="{{ route("articles.show",$article->id) }}">{{ $article->title }}</a></h2>
-                          </div>
+        <div class="col-md-6 col-md-push-3">
+            <ul class="list-unstyled">
+                @if($articles->count())
+                    @foreach($articles as $article)
+                        <li>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h2><a href="{{ route("articles.show",$article->id) }}">{{ $article->title }}</a>
+                                    </h2>
+                                </div>
 
-                          <div class="col-md-12 text-justify">
-                              <p>{{ $article->getContent() }}</p>
-                          </div>
+                                <div class="col-md-12 text-justify">
+                                    <p>{{ $article->getContent() }}</p>
+                                </div>
 
-                          <div class="col-md-12 text-right">
-                              <a href="{{ route("ideas.remove",$article->id) }}">Remove from Ideabook</a>
+                                <div class="col-md-12 text-right">
+                                    <a href="{{ route("ideas.remove",$article->id) }}">Remove from Ideabook</a>
 
-                          </div>
-                      </div>
+                                </div>
+                            </div>
 
-                   </li>
-               @endforeach
-           </ul>
-       </div>
+                        </li>
+                    @endforeach
+                @else
+                    <p>
+                        You haven't saved any idea yet.
+                    </p>
+                @endif
+            </ul>
+        </div>
     </div>
 
 
