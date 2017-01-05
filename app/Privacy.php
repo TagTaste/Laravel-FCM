af08a4fc47d03ec35ba2e0325fdf98c90cae0cde
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-01-05 12:39:50 +0530

    change order of inputs

--- app/Privacy.php
+++ app/Privacy.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Privacy extends Model
 {
     public static function getAll(){
-    	return static::select('id','name')->get()->pluck('id','name');
+    	return static::select('id','name')->get()->pluck('name','id');
     }
 }
