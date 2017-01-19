5b83f54453160390323681ef583f62f01af0fc7a
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-01-19 19:21:07 +0530

    get api working

--- routes/api.php
+++ routes/api.php
@@ -12,12 +12,30 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group(['middleware'=>'jwt.auth'],function(){
+    Route::get('/profile', function (Request $request) {
+        return $request->user();
+    });
+});
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+Route::post('/user/create',function(Request $request){
+    //return response()->json($request->only('user'));
+    return response()->json(['status'=>'success','result'=> ['user'=>\App\User::create($request->input('user'))]]);
+    });
 
 Route::post('login',function(Request $request){
+    $credentials = $request->only('email','password');
 
-    return response()->json($request->only('email'));
+    try {
+        // attempt to verify the credentials and create a token for the user
+        if (! $token = \JWTAuth::attempt($credentials)) {
+            return response()->json(['error' => 'invalid_credentials'], 401);
+        }
+    } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+        // something went wrong whilst attempting to encode the token
+        return response()->json(['error' => 'could_not_create_token'], 500);
+    }
+
+    return response()->json(compact('token'));
+
 });
