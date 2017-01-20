a3cb8ff4d1adadbe6caf5a7e26f74df08cbbf062
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-01-20 18:11:12 +0530

    update fillable and dates

--- app/Profile/Award.php
+++ app/Profile/Award.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Award extends Model
 {
-    //
+    protected $fillable = ['name','description','date','profile_id'];
+
+    public function setDateAttribute($value)
+    {
+        $this->attributes['date'] = date('Y-m-d',strtotime($value));
+    }
+
+    public function getDateAttribute($value)
+    {
+        if(!$value){
+            return;
+        }
+        return date("d-m-Y",strtotime($value));
+    }
+
+
 }
