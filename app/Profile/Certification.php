2c16b262b519d319a62dba26af971808cf0a2abf
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-01-20 19:10:28 +0530

    add date mutators

--- app/Profile/Certification.php
+++ app/Profile/Certification.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Certification extends Model
 {
     protected $fillable = ['name','description','date','profile_id'];
+
+    public function setDateAttribute($value)
+    {
+        $this->attributes['date'] = date('Y-m-d',strtotime($value));
+    }
+
+    public function getDateAttribute($value)
+    {
+        if(!$value){
+            return;
+        }
+        return date("m-Y",strtotime($value));
+    }
 }
