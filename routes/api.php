d7f5a3b7acc6ea31f427e735440ed48dbaa6671c
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-01-27 19:05:38 +0530

    add line for readability

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,14 @@ use Illuminate\Http\Request;
 Route::group(['namespace'=>'Api',
     'as' => 'api.' //note the dot.
     ],function(){
+
+
     //unauthenticated routes.
     Route::post('/user/register',['uses'=>'UserController@register']);
     Route::get("profile/images/{id}.jpg",['as'=>'profile.image','uses'=>'ProfileController@image']);
     Route::get("profile/hero/{id}.jpg",['as'=>'profile.heroImage','uses'=>'ProfileController@heroImage']);
+
+
     //authenticated routes.
     Route::group(['middleware'=>'api.auth'],function(){
 
