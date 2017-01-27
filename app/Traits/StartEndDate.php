2c3873a424aa23de47c49a03bddfbfdb4be36d6a
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-01-27 18:22:49 +0530

    add start end date accessor and mutator trait

---
+++ app/Traits/StartEndDate.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amitabh
+ * Date: 27/01/17
+ * Time: 6:19 PM
+ */
+
+namespace App\Traits;
+
+
+trait StartEndDate
+{
+    public function setStartDateAttribute($value)
+    {
+        $this->attributes['start_date'] = date('Y-m-d',strtotime($value));
+    }
+
+    public function getStartDateAttribute($value)
+    {
+        if(!$value){
+            return;
+        }
+        return date("d-m-Y",strtotime($value));
+    }
+
+    public function setEndDateAttribute($value)
+    {
+        $this->attributes['end_date'] = date('Y-m-d',strtotime($value));
+    }
+
+    public function getEndDateAttribute($value)
+    {
+        if(!$value){
+            return;
+        }
+        return date("d-m-Y",strtotime($value));
+    }
+}
