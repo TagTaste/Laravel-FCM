28a7ade22dcf861731f095fa1a5d69afd43f9b7d
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-01-31 15:33:41 +0530

    show id too

--- app/Photo.php
+++ app/Photo.php
@@ -8,6 +8,8 @@ class Photo extends Model
 {
     protected $fillable = ['caption','file','album_id'];
 
+    protected $visible = ['id','caption','file','created_at'];
+
     public function album()
     {
         return $this->belongsTo('App\Album');
@@ -16,5 +18,10 @@ class Photo extends Model
     public function ideabooks()
     {
         return $this->belongsToMany('\App\Ideabook','ideabook_photos','photo_id','ideabook_id');
+    }
+
+    public function getCreatedAtAttribute($value)
+    {
+        return date("d-m-Y",strtotime($value));
     }
 }
