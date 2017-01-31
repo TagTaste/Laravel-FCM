83a4174baaff50c626d553d91cbd4637582cc818
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-01-31 14:26:41 +0530

    set attribute visibility

--- app/Album.php
+++ app/Album.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Album extends Model
 {
     protected $fillable = ['name','description','profile_id'];
+
+    protected $visible = ['id','name','description','profile_id','photos'];
     
     public function photos()
     {
