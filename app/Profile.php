dd9d125b36812fabb884b7378bd53c98332b3473
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-02-01 21:02:55 +0530

    add albums with each profile request

--- app/Profile.php
+++ app/Profile.php
@@ -13,14 +13,14 @@ class Profile extends Model
 
     //if you add a relation here, make sure you remove it from
     //App\Recommend to prevent any unwanted results like nested looping.
-    protected $with = ['experience','awards','certifications','tvshows','books'];
+    protected $with = ['experience','awards','certifications','tvshows','books','albums'];
 
     protected $visible = ['id','tagline','about','phone','address','dob','interests', 'imageUrl','heroImageUrl',
         'website_url','blog_url','facebook_url','linkedin_url','instagram_link',
         'youtube_channel',
         'followers','following',
         'experience','awards','certifications','tvshows','books','followingProfiles', 'followerProfiles',
-        'name'];
+        'name','albums'];
 
     protected $appends = ['imageUrl','heroImageUrl','followingProfiles','followerProfiles'];
 
