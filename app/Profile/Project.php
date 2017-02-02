f5705622c12966951df9c3c3fa1c2309334a633d
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-02-02 19:28:19 +0530

    setup projects

--- app/Profile/Project.php
+++ app/Profile/Project.php
@@ -6,9 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
+    protected $fillable = ['title','description','ongoing','start_date','end_date','url','profile_id'];
 
     public function profile()
     {
         return $this->belongsTo('App\Profile');
+    }
+
+    public function members()
+    {
+        return $this->hasMany('App\ProjectMember');
     }
 }
--- app/Profile/ProjectMember.php
+++ app/Profile/ProjectMember.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProjectMember extends Model
 {
-    //
+    protected $fillable = ['name','designation','description','project_id','profile_id'];
+
+    public function projects()
+    {
+        return $this->belongsToMany('App\Project');
+    }
+
+    public function profile()
+    {
+        return $this->belongsTo('App\Profile');
+    }
 }
