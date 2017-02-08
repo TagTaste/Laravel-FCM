b4a7d58c7bbbf4c8d74b21e4e19486af1ec42e9d
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-02-08 16:50:27 +0530

    add start end date trait

--- app/Education.php
+++ app/Education.php
@@ -2,10 +2,13 @@
 
 namespace App;
 
+use App\Traits\StartEndDate;
 use Illuminate\Database\Eloquent\Model;
 
 class Education extends Model
 {
+    use StartEndDate;
+
     public function profile()
     {
         return $this->belongsTo('App\Profile');
