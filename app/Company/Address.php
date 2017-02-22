a285110c55e470db5b9fa848cd20142eead9df9c
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-02-22 19:51:47 +0530

    add $visible, remove relationship. For now, we don't need to fetch company based on addresses.

--- app/Company/Address.php
+++ app/Company/Address.php
@@ -6,12 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
-    protected $table = ['company_addresses'];
+    protected $table = 'company_addresses';
 
     protected $fillable = ['address','country','phone','company_id'];
 
-    public function company()
-    {
-        return $this->belongsTo('App\Company');
-    }
+    protected $visible = ['id','address','country','phone'];
 }
