b9b8b18aa014bd8e918915c41d006aff79883727
Vibhav Yadav <vibhav@tagtaste.como>
2017-03-06 10:18:42 +0530

    Add new column validation key and altered the is_active default value to 0

---
+++ database/migrations/2017_03_06_101024_change_default_is_active_to_one.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeDefaultIsActiveToOne extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('users', function ($table) {
+            $table->integer('is_active')->default(0)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('users', function ($table) {
+            $table->integer('is_active')->default(1)->change();
+        });
+    }
+}
