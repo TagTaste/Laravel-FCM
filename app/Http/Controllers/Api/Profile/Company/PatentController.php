132c2c95ce472fa8e3365e2d84a055a696df14b3
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-03-23 17:27:38 +0530

    add docblock

--- app/Http/Controllers/Api/Profile/Company/PatentController.php
+++ app/Http/Controllers/Api/Profile/Company/PatentController.php
@@ -1,39 +1,37 @@
 <?php namespace App\Http\Controllers\Api\Profile\Company;
 
-use App\Http\Controllers\Api\Controller;
-
 use App\Company\Patent;
+use App\Http\Controllers\Api\Controller;
 use Illuminate\Http\Request;
 
+/**
+ * Class PatentController
+ * @package App\Http\Controllers\Api\Profile\Company
+ */
 class PatentController extends Controller {
-
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index(Request $request, $profileId, $companyId)
+    
+    
+    /**
+     * @param Request $request
+     * @param $profileId
+     * @param $companyId
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Request $request, $profileId, $companyId)
 	{
 		$this->model = Patent::where('company_id',$companyId)->paginate(10);
         return $this->sendResponse();
 	}
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param Request $request
-	 * @return Response
-	 */
-	public function store(Request $request, $profileId, $companyId)
+    
+    
+    /**
+     * @param Request $request
+     * @param $profileId
+     * @param $companyId
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Exception
+     */
+    public function store(Request $request, $profileId, $companyId)
 	{
 	    $company = $request->user()->companies()->where('id',$companyId)->first();
         if(!$company){
@@ -45,38 +43,30 @@ class PatentController extends Controller {
         $this->model = Patent::create(array_filter($inputs));
         return $this->sendResponse();
     }
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($profileId, $companyId, $id)
+    
+    
+    /**
+     * @param $profileId
+     * @param $companyId
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    
+    public function show($profileId, $companyId, $id)
 	{
 		$this->model = Patent::where('id',$id)->where('company_id',$companyId)->first();
 		return $this->sendResponse();
     }
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($profileId, $companyId, $id)
-	{
-
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @param Request $request
-	 * @return Response
-	 */
-	public function update(Request $request, $profileId, $companyId, $id)
+    
+    
+    /**
+     * @param Request $request
+     * @param $profileId
+     * @param $companyId
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, $profileId, $companyId, $id)
     {
         $userId = $request->user()->id;
         $inputs = $request->only(['title','description','number','issued_by','awarded_on']);
@@ -88,14 +78,16 @@ class PatentController extends Controller {
 
         return $this->sendResponse();
 	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy(Request $request, $profileId, $companyId, $id)
+    
+    
+    /**
+     * @param Request $request
+     * @param $profileId
+     * @param $companyId
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(Request $request, $profileId, $companyId, $id)
 	{
         $userId = $request->user()->id;
 
