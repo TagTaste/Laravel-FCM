697101601df1eb70b1092185c1d864abb7d15b91
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-04-04 03:43:53 +0530

    all jobs list

---
+++ app/Http/Controllers/Api/JobController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Job;
+use App\Scopes\SendsJsonResponse;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class JobController extends Controller
+{
+    use SendsJsonResponse;
+	/**
+	 * Variable to model
+	 *
+	 * @var job
+	 */
+	protected $model;
+
+	/**
+	 * Create instance of controller with Model
+	 *
+	 * @return void
+	 */
+	public function __construct(Job $model)
+	{
+		$this->model = $model;
+	}
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		$this->model = $this->model->paginate();
+
+		return $this->sendResponse();
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		$this->model = $this->model->findOrFail($id);
+		
+		return $this->sendResponse();
+	}
+}
