f55c427fe98ea1b87235ba53f001df83fe2e55c8
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-04-04 15:26:46 +0530

    portfolio save

--- app/Company/Portfolio.php
+++ app/Company/Portfolio.php
@@ -8,8 +8,15 @@ class Portfolio extends Model
 {
     protected $fillable = ['worked_for','description','company_id'];
     
+    protected $appends = ['total'];
+    
     public function company()
     {
         return $this->belongsTo('App\Company');
+    }
+    
+    public function getTotalAttribute()
+    {
+        return $this->where('company_id',$this->company_id)->count();
     }
 }
