f4769a3977ba5c019eaa1e0482098e0946f841c3
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-04-07 17:58:40 +0530

    migration album note class name update

--- database/migrations/2017_04_07_175215_alter_ideabook_albums_note.php
+++ database/migrations/2017_04_07_175215_alter_ideabook_albums_note.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterIdeabookAlbumsKeywords extends Migration
+class AlterIdeabookAlbumsNote extends Migration
 {
     private $table = 'ideabook_albums';
     /**
