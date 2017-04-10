e365027ebb4ef4954b9ee4230f75ac4f404e2cb2
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-04-10 12:36:07 +0530

    ideabook photo like count

--- app/Ideabook/Photo.php
+++ app/Ideabook/Photo.php
@@ -6,5 +6,7 @@ use App\Photo as BasePhoto;
 
 class Photo extends BasePhoto
 {
-    protected $visible = ['id','caption','file','hasLiked','pivot'];
+    protected $visible = ['id','caption','file','hasLiked','pivot','count'];
+    
+    protected $appends = ['count'];
 }
