fac543dd49b7a2790520fd4cfadd9c6dbf824ce6
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-04-11 17:56:03 +0530

    add foreign contraints

--- database/migrations/2017_04_11_160122_create_collaborates_table.php
+++ database/migrations/2017_04_11_160122_create_collaborates_table.php
@@ -24,6 +24,9 @@ class CreateCollaboratesTable extends Migration {
             $table->integer('profile_id')->unsigned();
             $table->integer('company_id')->unsigned();
             $table->timestamps();
+            
+            $table->foreign('profile_id')->references('id')->on('profiles');
+            $table->foreign('company_id')->references('id')->on('companies');
         });
 	}
 
