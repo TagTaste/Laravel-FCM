dcd369fa34529554ed759b52f7b0771c280be9eb
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-04-12 11:37:48 +0530

    remove unneeded method

--- app/Http/Controllers/Api/Profile/Photo/CommentController.php
+++ app/Http/Controllers/Api/Profile/Photo/CommentController.php
@@ -22,16 +22,6 @@ class CommentController extends Controller {
 	}
 
 	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		return view('comments.create');
-	}
-
-	/**
 	 * Store a newly created resource in storage.
 	 *
 	 * @param Request $request
