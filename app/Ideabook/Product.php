8f8eae36b89fe068faa504c1a7bbee3b55075b9d
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-04-13 15:57:18 +0530

    profile_id and company_id

--- app/Ideabook/Product.php
+++ app/Ideabook/Product.php
@@ -6,8 +6,15 @@ use App\Company\Product as BaseProduct;
 
 class Product extends BaseProduct
 {
-    protected $visible = ['id','name','pivot','imageUrl'];
+    protected $visible = ['id','name','pivot','imageUrl','company_id','profile_id'];
     
     protected $fillable = ['note'];
+    
+    protected $appends = ['profile_id'];
+    
+    public function getProfileIdAttribute()
+    {
+        return $this->company->user->profile->id;
+    }
     
 }
