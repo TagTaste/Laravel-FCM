431bb7e4391f96ae4b3fce8dce7fdb574735dfeb
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-04-13 18:44:21 +0530

    company photo comment and like

---
+++ app/Http/Controllers/Api/Profile/Company/Photo/CommentController.php
@@ -0,0 +1,98 @@
+<?php namespace App\Http\Controllers\Api\Profile\Company\Photo;
+
+use App\Comment;
+use App\Photo;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Api\Controller;
+
+class CommentController extends Controller {
+ 
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index($profileId, $companyId, $photoId)
+	{
+		$this->model = Comment::whereHas('photo',function($query) use ($photoId){
+		    $query->where('photo_id',$photoId);
+        })->orderBy('created_at', 'desc')->paginate(10);
+		
+		return $this->sendResponse();
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param Request $request
+	 * @return Response
+	 */
+	public function store(Request $request, $profileId, $companyId, $photoId)
+	{
+		$comment = new Comment();
+
+		$comment->content = $request->input("content");
+        $comment->user_id = $request->user()->id;
+		$comment->save();
+
+		$photo = Photo::find($photoId);
+		$photo->comments()->attach($comment->id);
+
+        $this->model = $comment;
+		return $this->sendResponse();
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		$this->model = Comment::findOrFail($id);
+        return $this->sendResponse();
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @param Request $request
+	 * @return Response
+	 */
+	public function update(Request $request, $profileId, $companyId, $id)
+	{
+        $userId = $request->user()->id;
+        $comment = Comment::where('user_id',$userId)->find($id);
+        if($comment === null){
+            $this->errors[] = 'Comment does not belong to the user';
+            return $this->sendResponse();
+        }
+        
+		$comment->content = $request->input("content");
+		$this->model = $comment->save();
+
+		return $this->sendResponse();
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy(Request $request, $profileId, $companyId, $id)
+	{
+        $userId = $request->user()->id;
+        $comment = Comment::where('user_id',$userId)->find($id);
+        if($comment === null){
+            $this->errors[] = 'Comment does not belong to the user';
+            return $this->sendResponse();
+        }
+		$this->model = $comment->delete();
+
+		return $this->sendResponse();
+	}
+
+}
---
+++ app/Http/Controllers/Api/Profile/Company/Photo/PhotoLikeController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Api\Profile\Company\Photo;
+
+use App\PhotoLike;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Api\Controller;
+
+class PhotoLikeController extends Controller
+{
+	/**
+	 * Variable to model
+	 *
+	 * @var photo_like
+	 */
+	protected $model;
+
+	/**
+	 * Create instance of controller with Model
+	 *
+	 * @return void
+	 */
+	public function __construct(PhotoLike $model)
+	{
+		$this->model = $model;
+	}
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index(Request $request,$profileId,$albumId,$photoId)
+	{
+		$photoLike = PhotoLike::where('photo_id',$photoId)->count();
+		
+		return $photoLike;
+		
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param Request $request
+	 * @return Response
+	 */
+	public function store(Request $request, $profileId, $photoId)
+	{
+        $profileId = $request->user()->profile->id;
+        $photoLike = PhotoLike::where('profile_id', $profileId)->where('photo_id', $photoId)->first();
+        if($photoLike != null) {
+            $this->model = PhotoLike::where('profile_id', $profileId)->where('photo_id', $photoId)->delete();
+        } else {
+            $this->model = PhotoLike::create(['profile_id' => $profileId, 'photo_id' => $photoId]);
+        }
+        return $this->sendResponse();
+	}
+
+	
+	
+}
