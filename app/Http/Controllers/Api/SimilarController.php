118e0a93817b57d0bb90965dbb933e510aeeb36f
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-04-17 14:10:38 +0530

    add similar jobs

--- app/Http/Controllers/Api/SimilarController.php
+++ app/Http/Controllers/Api/SimilarController.php
@@ -2,21 +2,22 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Similar\Job;
 use App\Similar\Photo;
+use App\Similar\Product;
 use App\Similar\Profile;
+use App\Similar\Recipe;
 use Illuminate\Http\Request;
 
 class SimilarController extends Controller
 {
     private $relationships = [
+        'job' => Job::class,
         'profile' => Profile::class,
-        'photo' => Photo::class
+        'photo' => Photo::class,
+        'product' => Product::class,
+        'Recipe' => Recipe::class
     ];
-    
-    private function getModel($model)
-    {
-        return isset($this->relationships[$model]) ? new $this->relationships[$model] : false;
-    }
     
     public function similar(Request $request, $relationship, $relationshipId)
     {
@@ -29,8 +30,13 @@ class SimilarController extends Controller
         if(!method_exists($model,'similar')){
             throw new \Exception("Similars not defined.");
         }
-    
+        
         $this->model = $model->similar();
         return $this->sendResponse();
+    }
+    
+    private function getModel($model)
+    {
+        return isset($this->relationships[$model]) ? new $this->relationships[$model] : false;
     }
 }
