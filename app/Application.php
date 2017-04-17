3d43fba2994c0894fa6a8f7dfc2d942d5ff19e94
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-04-17 16:47:33 +0530

    profile, current info

--- app/Application.php
+++ app/Application.php
@@ -7,9 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Application extends Model
 {
     protected $fillable = ['job_id', 'profile_id'];
-    protected $visible = ['created_at', 'profile'];
+    protected $visible = ['created_at', 'profile', 'name'];
     protected $with = ['profile'];
-    
+    protected $appends = ['name'];
     public function job()
     {
         return $this->belongsTo(\App\Job::class);
@@ -17,6 +17,11 @@ class Application extends Model
     
     public function profile()
     {
-        return $this->belongsTo(\App\Profile::class);
+        return $this->belongsTo(\App\Application\Profile::class);
+    }
+    
+    public function getNameAttribute()
+    {
+        return $this->profile->user->name;
     }
 }
