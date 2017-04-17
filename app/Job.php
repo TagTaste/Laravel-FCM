102d9a76c7066f2b7c13aedabcebee212a5e1831
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-04-17 12:24:54 +0530

    display company

--- app/Job.php
+++ app/Job.php
@@ -8,6 +8,8 @@ class Job extends Model
 {
     protected $fillable = ['title', 'description', 'type', 'location', 'annual_salary', 'functional_area', 'key_skills', 'xpected_role', 'experience_required', 'company_id'];
     
+    protected $with = ['company'];
+    
     public function company()
     {
         return $this->belongsTo(\App\Company::class);
@@ -15,12 +17,12 @@ class Job extends Model
     
     public function applications()
     {
-        return $this->hasManyThrough(\App\Profile::class,\App\Application::class);
+        return $this->hasMany(\App\Application::class);
     }
     
     public function apply($profileId)
     {
-        return \DB::table('applications')->insert(['job_id'=>$this->id,'profile_id'=>$profileId]);
+        return \DB::table('applications')->create(['job_id' => $this->id, 'profile_id' => $profileId]);
     }
     
     public function unapply($profileId)
