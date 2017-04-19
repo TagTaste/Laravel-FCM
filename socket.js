8a6ffa3a440d88bb9c653f618e31c0fabdd6f487
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-04-19 20:54:16 +0530

    read messages from redis channels

--- socket.js
+++ socket.js
@@ -2,27 +2,36 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var Redis = require('ioredis');
-var redis = new Redis();
 
-app.get('/', function(req, res){
-    res.send('<h1>Hello world</h1>');
-});
+var logErr = function(err,count){
+    if(err !== null) console.log(err);
+};
 
-redis.psubscribe('feed.*', function(err, count) {
-    console.log(err);
-});
-redis.on('pmessage', function(pattern, channel, message) {
+var emit = function(pattern, channel, message){
     console.log(pattern);
     console.log(channel);
     console.log('Message Recieved: ' + message);
     message = JSON.parse(message);
     io.emit(channel, message.data);
-});
+};
+
+var feed = new Redis();
+feed.psubscribe('feed.*', logErr);
+feed.on('pmessage', emit);
+
+var network = new Redis();
+network.psubscribe('network.*',logErr);
+network.on('pmessage',emit);
+
+var public = new Redis();
+public.psubscribe('public.*',logErr);
+public.on('pmessage',emit);
 
 io.on('connection', function(socket){
     console.log(socket);
     console.log('a user connected');
 });
+
 http.listen(3001, function(){
     console.log('Listening on Port 3001');
 });
