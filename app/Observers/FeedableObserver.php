7c6dabe3882ec1fb4bb5d4c318a6f8c2be47a33f
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-04-19 14:34:02 +0530

    throw exception if profile cannot be found.

--- app/Observers/FeedableObserver.php
+++ app/Observers/FeedableObserver.php
@@ -7,7 +7,7 @@ class FeedableObserver {
         //todo: check for privacy
         
         if(!method_exists($model,'profile')){
-            return;
+           throw new \Exception("Profile relationship not defined on Feedable " . $model);
         }
         
         $model->profile->pushToMyFeed($model);
