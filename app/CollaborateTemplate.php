f0d2cb64b630eb35f5b916022744e819b9f6754d
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-04-20 16:17:41 +0530

    add accessor and mutator

--- app/CollaborateTemplate.php
+++ app/CollaborateTemplate.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class CollaborateTemplate extends Model
 {
     protected $fillable = ['name', 'fields'];
+    
+    public function setFieldsAttribute($value)
+    {
+        $this->attributes['fields'] = json_encode($value);
+    }
+    
+    public function getFieldsAttribute()
+    {
+        return json_decode($this->fields,true);
+    }
 }
