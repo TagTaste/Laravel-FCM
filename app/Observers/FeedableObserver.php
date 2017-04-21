e2a60f19ef9cc491b21eb3d0611c29dd11a493a9
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-04-21 12:40:03 +0530

    check for owner

--- app/Observers/FeedableObserver.php
+++ app/Observers/FeedableObserver.php
@@ -4,11 +4,11 @@ class FeedableObserver {
     
     public function created($model)
     {
-        if(!method_exists($model,'profile')){
-           throw new \Exception("Profile relationship not defined on Feedable " . class_basename($model));
+        if(!method_exists($model,'owner')){
+           throw new \Exception("Owner not defined on Feedable " . class_basename($model));
         }
         
-        $model->profile->pushToMyFeed($model);
+        $model->owner()->pushToMyFeed($model);
         
         if(!method_exists($model,'privacy') || is_null($model->privacy)){
             //if Privacy is not defined on the model,
@@ -19,6 +19,7 @@ class FeedableObserver {
             \Log::warning("Not publishing it to network or public feed.");
             return;
         }
+        
         if($model->privacy->isNetwork() || $model->privacy->isPublic()){
             $model->profile->pushToNetwork($model);
         }
