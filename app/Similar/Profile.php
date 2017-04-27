c2e3cd8e30ba9f8ce55949c954e88afc5acfb92d
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-04-27 16:14:11 +0530

    update similar query

--- app/Similar/Profile.php
+++ app/Similar/Profile.php
@@ -8,13 +8,12 @@ class Profile extends BaseProfile
 {
     protected $with = [];
     
-    protected $visible = ['id','name','imageUrl','tagline'];
+    protected $visible = ['id','name','imageUrl','tagline','followingProfiles','followerProfiles'];
     
     public function similar()
     {
         return self::join('subscribers','subscribers.id','=','profiles.id')
-            ->where('subscribers.channel_name','not like','network.' . $this->id)
-            ->where('subscribers.channel_name','not like','public.' . $this->id)
+            ->where('subscribers.profile_id','!=',$this->id)
             ->paginate(5);
     }
     
