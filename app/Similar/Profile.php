ac53823108c4fb6c8daa0df2a9fe3b336425003c
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-04-27 14:14:03 +0530

    update similar profiles

--- app/Similar/Profile.php
+++ app/Similar/Profile.php
@@ -8,11 +8,14 @@ class Profile extends BaseProfile
 {
     protected $with = [];
     
-    protected $visible = ['id'];
+    protected $visible = ['id','name','image','tagline'];
     
     public function similar()
     {
-        return self::take(4)->get();
+        return self::join('subscribers','subscribers.id','=','profiles.id')
+            ->where('subscribers.channel_name','not like','network.' . $this->id)
+            ->where('subscribers.channel_name','not like','public.' . $this->id)
+            ->paginate(5);
     }
     
 }
