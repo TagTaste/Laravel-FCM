9df6a614f56417da2046b19ee675d90284d7fee6
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-05-01 12:21:10 +0530

    update payload when a shoutout is liked.

--- app/ShoutoutLike.php
+++ app/ShoutoutLike.php
@@ -2,9 +2,37 @@
 
 namespace App;
 
+use App\Events\UpdateFeedable;
 use Illuminate\Database\Eloquent\Model;
 
 class ShoutoutLike extends Model
 {
     protected $fillable = ['profile_id', 'shoutout_id'];
+    
+    public static function boot()
+    {
+        parent::boot();
+        
+        //Update the payload when a like is added or removed.
+            self::created(function($like){
+                event(new UpdateFeedable($like->shoutout));
+            });
+            
+        //UpdateFeedable is called and not DeleteFeedable
+        //since a like is child of the feed payload, and not the payload itself.
+        
+            self::deleting(function($like){
+                event(new UpdateFeedable($like->shoutout));
+            });
+    }
+    
+    public function shoutout()
+    {
+        return $this->belongsTo( Shoutout::class);
+    }
+    
+    public function profile()
+    {
+        return $this->belongsTo(Profile::class);
+    }
 }
