1741efcf3b2b29c19ba543af78e9331e36f1d4fc
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-05-01 17:44:14 +0530

    add newrelic monitoring for node

---
+++ newrelic.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['api sockets'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '1f21db7865a8b953a7daa766f904e11b62d52c2d',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+};
