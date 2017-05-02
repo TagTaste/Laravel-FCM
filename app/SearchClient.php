397b4147d75271e35fb37f3b59c8c18441c856fe
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-05-02 15:58:09 +0530

    add default

--- app/SearchClient.php
+++ app/SearchClient.php
@@ -6,6 +6,6 @@ class SearchClient
 {
     public static function get()
     {
-        return ClientBuilder::create()->setHosts([env('ELASTICSEARCH_HOST')])->build();
+        return ClientBuilder::create()->setHosts([env('ELASTICSEARCH_HOST','http://localhost:9200')])->build();
     }
 }
