f4510a7f1c4eb0e69a473ff804fd64da35d60092
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-05-04 11:01:57 +0530

    show expires_on

--- app/Job.php
+++ app/Job.php
@@ -20,7 +20,7 @@ class Job extends Model
         'annual_salary', 'functional_area', 'key_skills', 'expected_role',
         'experience_required',
         'company_id', 'type_id', 'company', 'profile_id',
-        'applications'
+        'applications','created_at', 'expires_on'
     ];
     
     protected $with = ['company', 'applications'];
