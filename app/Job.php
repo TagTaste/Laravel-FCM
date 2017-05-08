acc959b0154ceb48e395355ac145113ce9d82f0a
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-05-08 10:46:06 +0530

    make profile id fillable

--- app/Job.php
+++ app/Job.php
@@ -15,7 +15,7 @@ class Job extends Model implements Feedable
     
     protected $fillable = ['title', 'description', 'type', 'location',
         'annual_salary', 'functional_area', 'key_skills', 'expected_role',
-        'experience_required',
+        'experience_required','profile_id',
         'company_id', 'type_id'
 
     ];
