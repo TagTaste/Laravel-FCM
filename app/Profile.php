f9d9f57141de517843fb9eb92a74bb0e976f0e0d
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-05-10 10:51:53 +0530

    update queries

--- app/Profile.php
+++ app/Profile.php
@@ -236,9 +236,7 @@ class Profile extends Model
             ->join('channels','subscribers.channel_name','=','channels.name')
             ->where('subscribers.profile_id','=',$id)
             ->where('subscribers.channel_name','like','network.%')
-            ->where('subscribers.channel_name','not like','feed.' . $id)
-            ->where('subscribers.channel_name','not like','network.' . $id)
-            ->where('subscribers.channel_name','not like','public.' . $id)
+            ->where('subscribers.channel_name','not like','%.' . $id)
             ->whereNull('subscribers.deleted_at')
             ->get();
         
