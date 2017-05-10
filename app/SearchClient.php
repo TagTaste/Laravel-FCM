64297bb879612b6e915f6b0342fe846d0b799cee
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-05-10 12:12:15 +0530

    change host url

--- app/SearchClient.php
+++ app/SearchClient.php
@@ -6,6 +6,6 @@ class SearchClient
 {
     public static function get()
     {
-        return ClientBuilder::create()->setHosts([env('ELASTICSEARCH_HOST','http://localhost:9200')])->build();
+        return ClientBuilder::create()->setHosts([env('ELASTICSEARCH_HOST')])->build();
     }
 }
