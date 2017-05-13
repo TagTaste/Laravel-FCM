df2cea8b128d4190846e36954563176d26a76aa1
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-05-13 15:28:49 +0530

    remove create method

--- app/Http/Controllers/Api/Profile/Company/CompanyUserController.php
+++ app/Http/Controllers/Api/Profile/Company/CompanyUserController.php
@@ -38,16 +38,6 @@ class CompanyUserController extends Controller
 	}
 
 	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
-
-	/**
 	 * Store a newly created resource in storage.
 	 *
 	 * @param Request $request
