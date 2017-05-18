ce30b71e367a5ad3232b10228e67cc04fd4a5a90
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-05-18 16:18:40 +0530

    add relationship

--- app/CollaborationField.php
+++ app/CollaborationField.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class CollaborationField extends Model
 {
     protected $fillable = ['collaboration_id', 'field_id'];
+    
+    public function field()
+    {
+        return $this->belongsTo(Field::class);
+    }
+    
+    public function collaboration()
+    {
+        return $this->belongsTo(Collaborate::class);
+    }
 }
