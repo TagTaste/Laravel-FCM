083e4df218da98b07fbdbd47d28a786e48900f8a
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-05-19 12:30:19 +0530

    add description

--- app/Field.php
+++ app/Field.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Field extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['name','description'];
     
-    protected $visible = ['id','name'];
+    protected $visible = ['id','name','description'];
 }
