9c1fba3fdf627b98ae283f4beb1d8a9dcfb42702
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-05-19 10:58:41 +0530

    add field_id to templates

---
+++ database/migrations/2017_05_19_104622_create_collaborate_template_fields.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCollaborateTemplateFields extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('collaboration_template_fields',function(Blueprint $table){
+            $table->integer('template_id')->unsigned();
+            $table->integer('field_id')->unsigned();
+            
+            $table->foreign('template_id')->references("id")->on('templates')->onDelete('cascade');
+            $table->foreign('field_id')->references('id')->on('fields')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('collaborate_template_fields');
+    }
+}
