0197d5b7a75e45a921eb7f646ce3b594176e5ec1
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-05-21 02:44:23 +0530

    unique photo_id

---
+++ database/migrations/2017_05_21_024236_alter_profile_photos_unique.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterProfilePhotosUnique extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('profile_photos',function(Blueprint $table){
+            $table->integer("photo_id")->unique()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('profile_photos',function(Blueprint $table){
+            $table->integer("photo_id")->change();
+        });
+    }
+}
