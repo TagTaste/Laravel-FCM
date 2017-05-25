cc44f0c73f0b4910b1c3ff1a6f82cdc7d218d628
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-05-25 17:22:37 +0530

    add parent child fields

--- app/Field.php
+++ app/Field.php
@@ -9,4 +9,19 @@ class Field extends Model
     protected $fillable = ['name','description'];
     
     protected $visible = ['id','name','description'];
+    
+    public function parents()
+    {
+        return self::whereNull("parent_id")->get();
+    }
+    
+    public function children()
+    {
+        return self::where('parent_id',$this->id)->get();
+    }
+    
+    public static function getChildrenOf($parentId)
+    {
+        return self::where('parent_id',$parentId)->get();
+    }
 }
