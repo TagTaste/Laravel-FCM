f45869ad1909012591e22da283259cb2df464293
Tushar Verma <tushar@tagtaste.com>
2017-05-30 05:53:51 +0000

    location added

---
+++ database/migrations/2017_05_30_110333_alter_table_collaborates_add_location.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableCollaboratesAddLocation extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table("collaborates",function(Blueprint $table){
+            $table->string("location")->nullable();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+         Schema::table("collaborates",function(Blueprint $table){
+            $table->dropColumn("location");
+            
+        });
+    }
+}
