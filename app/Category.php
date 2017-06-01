915c494f1e158628e1fb9e54b7fa5a175d0350e7
ashokTagtaste <tagtaste@Vibhavs-MacBook-Air.local>
2017-06-01 18:31:14 +0530

    add braces

--- app/Category.php
+++ app/Category.php
@@ -11,10 +11,12 @@ class Category extends Model
     protected $visible=['id','name'];
     public static function checkExists( &$categoryDetails){
         $category=false;
-        if(array_key_exists('parent_id', $categoryDetails))
+        if(array_key_exists('parent_id', $categoryDetails)){
 		    $category = Category::where('parent_id', $categoryDetails['parent_id'])->where('name',$categoryDetails['name'])->exists();
-        else
+        }
+        else{
             $category = Category::where('name',$categoryDetails['name'])->exists();
+        }
 		return $category;
 	}
     
