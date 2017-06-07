e73c1c0b2de0b57b0ad94f9cd2537321efa2b4c5
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-06-07 13:42:36 +0530

    shareable collaborate

---
+++ database/migrations/2017_06_07_134011_create_collaborate_shares.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCollaborateShares extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create("collaborate_shares",function(Blueprint $table){
+            $table->unsignedInteger('collaborate_id');
+            $table->unsignedInteger('profile_id');
+            $table->unique(['collaborate_id','profile_id']);
+            $table->foreign('collaborate_id')->references('id')->on('collaborates');
+            $table->foreign('profile_id')->references('id')->on('profiles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop("collaborate_shares");
+    }
+}
