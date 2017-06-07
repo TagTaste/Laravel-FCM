21845a7fc01043941a43cc17fed9bd20c449e3e6
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-06-07 16:25:33 +0530

    set table name

--- app/Shareable/Share.php
+++ app/Shareable/Share.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Share extends Model
 {
-
+    public function __construct($attributes = [])
+    {
+        $class = strtolower(class_basename($this));
+        $this->table = $class . "_shares";
+        parent::__construct($attributes);
+    }
 }
