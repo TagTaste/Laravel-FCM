00dedb6632cf2a9c33eecffefd6aa43280cce08b
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-06-07 11:54:49 +0530

    create photo share migration

---
+++ database/migrations/2017_06_07_114536_photo_share.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PhotoShare extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('photo_shares',function(Blueprint $table){
+            $table->unsignedInteger('profile_id');
+            $table->unsignedInteger('photo_id');
+            
+            $table->foreign('profile_id')->references("id")->on("profiles");
+            $table->foreign('photo_id')->references("id")->on("photos");
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropTable('photo_shares');
+    }
+}
