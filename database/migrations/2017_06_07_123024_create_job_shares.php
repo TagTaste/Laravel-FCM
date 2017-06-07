3c00854395b0f80752c14be7a27dc8821db6aad7
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-06-07 13:39:46 +0530

    make jobs shareable

---
+++ database/migrations/2017_06_07_123024_create_job_shares.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobShares extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create("job_shares",function(Blueprint $table){
+            $table->unsignedInteger('job_id');
+            $table->unsignedInteger('profile_id');
+            $table->unique(['job_id','profile_id']);
+            $table->foreign('job_id')->references('id')->on('jobs');
+            $table->foreign('profile_id')->references('id')->on('profiles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('job_shares');
+    }
+}
