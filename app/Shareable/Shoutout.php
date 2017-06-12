976435d92e5cb5e2e5be35680809c362cea6109e
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-06-12 10:50:44 +0530

    add relationship

--- app/Shareable/Shoutout.php
+++ app/Shareable/Shoutout.php
@@ -8,12 +8,18 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Shoutout extends Share
 {
    
+    protected $with = ['shoutout'];
     
     public static function boot()
     {
         static::deleted(function($model){
             $model->payload->delete();
         });
+    }
+    
+    public function shoutout()
+    {
+        return $this->belongsTo(\App\Shoutout::class,'shoutout_id');
     }
 
 }
