3b6afece9f4aacbd88ed8212e284c35ddccbcd16
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-06-13 16:31:51 +0530

    use all instead of get

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 use Illuminate\Support\Facades\Route;
+
 Route::get('/redis',function(){
     //\Redis::publish('notifications', json_encode(['foo' => 'bar']));
     event(new \App\Events\TestEvent(str_random(32)));
@@ -159,7 +160,7 @@ Route::group(['middleware'=>'auth'],function(){
 
 Route::get('/addprof',function()
 {
-    $profile = \App\Profile::get();
+    $profile = \App\Profile::all();
     foreach ($profile as $profile) {
       \App\Documents\Profile::create($profile);
       return $profile->name.' added to elastic search';
