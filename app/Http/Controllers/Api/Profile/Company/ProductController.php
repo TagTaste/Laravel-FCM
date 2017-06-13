0907c1c6288edefe6a54e57e860df9b0993926c7
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-06-13 15:29:32 +0530

    reformat code

--- app/Http/Controllers/Api/Profile/Company/ProductController.php
+++ app/Http/Controllers/Api/Profile/Company/ProductController.php
@@ -9,44 +9,46 @@ use App\Http\Controllers\Api\Controller;
 use App\ProductCategory;
 use App\Category;
 
-class ProductController extends Controller {
-
+class ProductController extends Controller
+{
+    
     use SendsJsonResponse;
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index(Request $request, $profileId, $companyId)
-	{
-		$this->model = Product::where('company_id',$companyId)->orderBy('id', 'desc')->paginate(10);
-		return $this->sendResponse();
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param Request $request
-	 * @return Response
-	 */
-	public function store(Request $request,$profileid,$companyId)
-	{
-
-        $company = $request->user()->companies()->where('id',$companyId)->first();
-        if(!$company){
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index(Request $request, $profileId, $companyId)
+    {
+        $this->model = Product::where('company_id', $companyId)->orderBy('id', 'desc')->paginate(10);
+        return $this->sendResponse();
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request, $profileid, $companyId)
+    {
+        
+        $company = $request->user()->companies()->where('id', $companyId)->first();
+        if (!$company) {
             throw new \Exception("This company does not belong to user.");
         }
         
-		$product = new Product();
-		$product->name = $request->input("name");
+        $product = new Product();
+        $product->name = $request->input("name");
         $product->price = $request->input("price");
         $product->image = "http://placehold.it/10x10";
-        if($request->hasFile('image')){
-        	$filename = $request->user()->id . str_random(25) . ".jpeg";
-        	$request->image->storeAs('product_images',$filename);
-        	$product->image = $filename;
+        if ($request->hasFile('image')) {
+            $filename = $request->user()->id . str_random(25) . ".jpeg";
+            $request->image->storeAs('product_images', $filename);
+            $product->image = $filename;
         }
-
+        
         $product->moq = $request->input("moq");
         $product->type = $request->input("type");
         $product->about = $request->input("about");
@@ -56,51 +58,51 @@ class ProductController extends Controller {
         $product->shelf_life = $request->input("shelf_life");
         $product->mode = $request->input("mode");
         $product->company_id = $company->id;
-		$product->save();
-		
-		$categories=$request->input('categories');
-		$product->categories()->sync($categories);
-		
-        $this->model = $product;
-		return $this->sendResponse();
-	}
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($profileId, $companyId, $id)
-	{
-		$this->model = Product::where('id',$id)->where('company_id',$companyId)->first();
-		
-		return $this->sendResponse();
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @param Request $request
-	 * @return Response
-	 */
-	public function update(Request $request, $profileid, $companyId, $id)
-	{
-        $company = $request->user()->companies()->where('id',$companyId)->first();
+        $product->save();
         
-        if(!$company){
+        $categories = $request->input('categories');
+        $product->categories()->sync($categories);
+        
+        $this->model = $product;
+        return $this->sendResponse();
+    }
+    
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($profileId, $companyId, $id)
+    {
+        $this->model = Product::where('id', $id)->where('company_id', $companyId)->first();
+        
+        return $this->sendResponse();
+    }
+    
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int $id
+     * @param Request $request
+     * @return Response
+     */
+    public function update(Request $request, $profileid, $companyId, $id)
+    {
+        $company = $request->user()->companies()->where('id', $companyId)->first();
+        
+        if (!$company) {
             throw new \Exception("This company does not belong to user.");
         }
         
         
         $product = Product::findOrFail($id);
-
-		$product->name = $request->input("name");
+        
+        $product->name = $request->input("name");
         $product->price = $request->input("price");
-		if($request->hasFile('image')){
-        	$product->image = $request->image->store('product_images');
-        }        
+        if ($request->hasFile('image')) {
+            $product->image = $request->image->store('product_images');
+        }
         $product->moq = $request->input("moq");
         $product->type = $request->input("type");
         $product->about = $request->input("about");
@@ -109,29 +111,29 @@ class ProductController extends Controller {
         $product->portion_size = $request->input("portion_size");
         $product->shelf_life = $request->input("shelf_life");
         $product->mode = $request->input("mode");
-		$product->save();
-		
-		$catIds=$request->input('categories');
-		$product->categories()->sync($catIds);
-		
+        $product->save();
+        
+        $catIds = $request->input('categories');
+        $product->categories()->sync($catIds);
+        
         $this->model = $product;
-		return $this->sendResponse();
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy(Request $request, $profileId, $companyId, $id)
-	{
-        $company = $request->user()->companies()->where('id',$companyId)->first();
-        if(!$company){
+        return $this->sendResponse();
+    }
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy(Request $request, $profileId, $companyId, $id)
+    {
+        $company = $request->user()->companies()->where('id', $companyId)->first();
+        if (!$company) {
             throw new \Exception("This company does not belong to user.");
         }
-        $this->model = $company->products()->where('id',$id)->delete();
-		return $this->sendResponse();
-	}
-
+        $this->model = $company->products()->where('id', $id)->delete();
+        return $this->sendResponse();
+    }
+    
 }
