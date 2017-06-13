ad4499a9bae454ab53563eda772827375893d779
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-06-13 15:18:45 +0530

    code reformat

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -7,119 +7,120 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-	/**
-	 * Variable to model
-	 *
-	 * @var category
-	 */
-	protected $model;
-
-	/**
-	 * Create instance of controller with Model
-	 *
-	 * @return void
-	 */
-	public function __construct(Category $model)
-	{
-		$this->model = $model;
-	}
-
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		$categories = $this->model->paginate();
-
-		return $this->sendResponse();
-	}
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		return view('categories.create');
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param Request $request
-	 * @return Response
-	 */
-	public function store(Request $request)
-	{
-		$inputs = $request->all();
-		$category=Category::checkExists($inputs);
-		if($category)
-		{
-			return $this->sendError("This category already exists.");
-		}
-
-		$this->model =$this->model->create($inputs);
-		return $this->sendResponse();
-	}
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		$category = $this->model->findOrFail($id);
-
-		$catId=\DB::table("categories")->where("parent_id",$id)->get();
-		return $catId;
-	}
-
-	
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		$category = $this->model->findOrFail($id);
-		
-		return view('categories.edit', compact('category'));
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @param Request $request
-	 * @return Response
-	 */
-	public function update(Request $request, $id)
-	{
-		$inputs = $request->all();
-
-		$category = $this->model->findOrFail($id);		
-		$category->update($inputs);
-
-		return redirect()->route('categories.index')->with('message', 'Item updated successfully.');
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		$this->model->destroy($id);
-
-		return redirect()->route('categories.index')->with('message', 'Item deleted successfully.');
-	}
+    /**
+     * Variable to model
+     *
+     * @var category
+     */
+    protected $model;
+    
+    /**
+     * Create instance of controller with Model
+     *
+     * @return void
+     */
+    public function __construct(Category $model)
+    {
+        $this->model = $model;
+    }
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $categories = $this->model->paginate();
+        
+        return $this->sendResponse();
+    }
+    
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        return view('categories.create');
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        $inputs = $request->all();
+        
+        $category = Category::checkExists($inputs);
+        
+        if ($category) {
+            return $this->sendError("This category already exists.");
+        }
+        
+        $this->model = $this->model->create($inputs);
+        return $this->sendResponse();
+    }
+    
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        $category = $this->model->findOrFail($id);
+        
+        $catId = \DB::table("categories")->where("parent_id", $id)->get();
+        return $catId;
+    }
+    
+    
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        $category = $this->model->findOrFail($id);
+        
+        return view('categories.edit', compact('category'));
+    }
+    
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int $id
+     * @param Request $request
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        $inputs = $request->all();
+        
+        $category = $this->model->findOrFail($id);
+        $category->update($inputs);
+        
+        return redirect()->route('categories.index')->with('message', 'Item updated successfully.');
+    }
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        $this->model->destroy($id);
+        
+        return redirect()->route('categories.index')->with('message', 'Item deleted successfully.');
+    }
 }
