2b4de66dedd05e1a3e3eeb1697749d5683c3d63c
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-06-13 15:15:52 +0530

    cleanup

--- app/Category.php
+++ app/Category.php
@@ -6,18 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = [ 'name', 'parent_id'];
+    protected $fillable = ['name', 'parent_id'];
     
-    protected $visible=['id','name'];
-    public static function checkExists( &$categoryDetails){
-        $category=false;
-        if(array_key_exists('parent_id', $categoryDetails)){
-		    $category = Category::where('parent_id', $categoryDetails['parent_id'])->where('name',$categoryDetails['name'])->exists();
+    protected $visible = ['id', 'name'];
+    
+    public static function checkExists(&$categoryDetails)
+    {
+        $category = false;
+        if (array_key_exists('parent_id', $categoryDetails)) {
+            $category = Category::where('parent_id', $categoryDetails['parent_id'])->where('name', $categoryDetails['name'])->exists();
+        } else {
+            $category = Category::where('name', $categoryDetails['name'])->exists();
         }
-        else{
-            $category = Category::where('name',$categoryDetails['name'])->exists();
-        }
-		return $category;
-	}
+        return $category;
+    }
     
 }
