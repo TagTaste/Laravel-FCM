14f4153ac61f8a71838b165a139ab0bd3df92bdf
vagrant <vagrant@homestead>
2017-06-14 11:03:40 +0000

    changes in test api of tagboard

--- tests/Feature/TagboardTest.php
+++ tests/Feature/TagboardTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Feature;
 
-use Tests\TestCase;
+use \Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -18,11 +18,21 @@ class TagboardTest extends TestCase
     {
         $this->get('/api/profiles/1/tagboards')
             ->assertSuccessful();
-    }
 
-    public function tagboardGet()
-    {
-        $this->get('/api/profiles/1/tagboards')
-            ->assertSuccessful();
+
+
+        $response = $this->json('POST', '/api/profiles/1/tagboards', ['name'=>'ideabook','description'=>'testing','keywords'=>'test','privacy_id'=>3]);
+        $response->assertStatus(200);
+
+        $data = json_encode($response->send(), true);
+        \Log::info("log is here ".$data);
+//        $this->get('/api/profiles/1/tagboards/1')
+//            ->assertSuccessful();
+//
+//        $this->put('/api/profiles/1/tagboards/10',['name'=>'ideabook1','description'=>'testing','keywords'=>'test','privacy_id'=>1])
+//            ->assertSuccessful();
+//
+//        $this->delete('/api/profiles/1/tagboards/14')
+//            ->assertSuccessful();
     }
 }
