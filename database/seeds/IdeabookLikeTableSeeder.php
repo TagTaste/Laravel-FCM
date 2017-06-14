d5d303550c7ea8d826108036e00c445de629eb33
vagrant <vagrant@homestead>
2017-06-14 12:03:41 +0000

    changes table name

--- database/seeds/IdeabookLikeTableSeeder.php
+++ database/seeds/IdeabookLikeTableSeeder.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Seeder;
 // composer require laracasts/testdummy
 use Laracasts\TestDummy\Factory as TestDummy;
 
-class TagboardLikeTableSeeder extends Seeder {
+class IdeabookLikeTableSeeder extends Seeder {
 
     public function run()
     {
