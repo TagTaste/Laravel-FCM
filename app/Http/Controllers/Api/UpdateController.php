ba39e2c40ceab401b9664e9fe6bfc662d91bd52e
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-06-16 15:28:20 +0530

    remove unwanted methods

--- app/Http/Controllers/Api/UpdateController.php
+++ app/Http/Controllers/Api/UpdateController.php
@@ -30,92 +30,13 @@ class UpdateController extends Controller
 	 *
 	 * @return Response
 	 */
-	public function index()
+	public function index(Request $request)
 	{
-		$updates = $this->model->paginate();
+	    $profileId = $request->user()->profile->id;
+	    
+		$this->model = $this->model->where('profile_id',$profileId)->orderBy('created_at','desc')->paginate();
 
-		return view('updates.index', compact('updates'));
-	}
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		return view('updates.create');
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param Request $request
-	 * @return Response
-	 */
-	public function store(Request $request)
-	{
-		$inputs = $request->all();
-		$this->model->create($inputs);
-
-		return redirect()->route('updates.index')->with('message', 'Item created successfully.');
-	}
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($profileId)
-	{
-        $data = Update::where('profile_id',$profileId)->get();
-        $data['notification_count']=Update::where('profile_id',$profileId)->count();
-        $this->model=$data;
 		return $this->sendResponse();
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		$update = $this->model->findOrFail($id);
-		
-		return view('updates.edit', compact('update'));
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @param Request $request
-	 * @return Response
-	 */
-	public function update(Request $request, $id)
-	{
-		$inputs = $request->all();
-
-		$update = $this->model->findOrFail($id);		
-		$update->update($inputs);
-
-		return redirect()->route('updates.index')->with('message', 'Item updated successfully.');
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		$this->model->destroy($id);
-
-		return redirect()->route('updates.index')->with('message', 'Item deleted successfully.');
 	}
 
 	public function isRead(Request $request, $modelName,$modelId)
