8ad1a4feec50808d31f27230f54e8ece54f4e09c
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-06-16 17:03:21 +0530

    simplify

--- app/Listeners/UpdateNotification.php
+++ app/Listeners/UpdateNotification.php
@@ -29,9 +29,6 @@ class UpdateNotification
      */
     public function handle(UpdateEvent $event)
     {
-        $notification=new \App\Update();
-
-        $notification->storeData($event->modelId,$event->modelName,$event->profileId,$event->content);
-
+        \App\Update()::create(['model_id'=>$event->modelId,'model_name'=>$event->modelName,'profile_id'=>$event->profileId,'content'=>$event->content]);
     }
 }
