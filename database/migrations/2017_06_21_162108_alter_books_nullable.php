c831c6a3c4810929fe970e6717ad0cdb5530d2d7
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-06-21 16:24:43 +0530

    make some fields nullable for books

---
+++ database/migrations/2017_06_21_162108_alter_books_nullable.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterBooksNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('profile_books',function(Blueprint $table){
+            $table->text('publisher')->nullable()->change();
+            $table->date('release_date')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('profile_books',function(Blueprint $table){
+            $table->text('publisher')->change();
+            $table->date('release_date')->change();
+        });
+    }
+}
