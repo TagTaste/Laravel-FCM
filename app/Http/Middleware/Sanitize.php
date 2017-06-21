58a67f2f45008f51fac47dd26ace8522501b5915
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-06-21 17:28:55 +0530

    handle nested arrays

--- app/Http/Middleware/Sanitize.php
+++ app/Http/Middleware/Sanitize.php
@@ -16,8 +16,35 @@ class Sanitize
     public function handle($request, Closure $next)
     {
         $inputs = $request->all();
-        $sanitized = \Purify::clean($inputs);
-        $request->replace($sanitized);
+        $sanitizedInputs = [];
+        $this->sanitize($inputs,$sanitizedInputs);
+        $request->replace($sanitizedInputs);
         return $next($request);
+    }
+    
+    private function sanitize(&$inputs,&$sanitized,$parent = null)
+    {
+        if(!is_array($inputs)){
+            $sanitized[] = \Purify::clean($inputs);
+            return;
+        }
+        
+        //is_array
+        foreach($inputs as $key => $value) {
+            if (!is_array($value)) {
+//                if(isset($sanitized[$key]) && is_array($sanitized[$key])){
+//                    $sanitized[$key][] = \Purify::clean($value);
+//                } else {
+                    $sanitized[$key] = \Purify::clean($value);
+//                }
+                continue;
+            }
+            
+            if(!isset($sanitized[$key])){
+                $sanitized[$key] = [];
+            }
+            
+            $this->sanitize($value, $sanitized[$key]);
+        }
     }
 }
