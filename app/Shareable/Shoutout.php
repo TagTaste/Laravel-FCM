0f55b9bae15c709ab73dad3688e52df8bf490289
ashokTagtaste <vagrant@homestead>
2017-06-23 14:44:53 +0000

    add meta for share

--- app/Shareable/Shoutout.php
+++ app/Shareable/Shoutout.php
@@ -18,4 +18,22 @@ class Shoutout extends Share
         return $this->belongsTo(\App\Shoutout::class,'shoutout_id');
     }
 
+    public function like()
+    {
+        return $this->hasMany(\App\Shareable\Sharelikable\Shoutout::class,'shoutout_share_id');
+    }
+
+    public function getMetaFor($profileId){
+        $meta = [];
+        $meta['hasLiked'] = $this->like()->where('profile_id',$profileId)->first() !== null;
+//        $meta['likeCount'] = \Redis::hget("shoutout_shares:" . $this->id . ":meta","like") ?: 0;
+
+        $idLiked = $this->like()->select('profile_id')->take(3)->get();
+        $meta['peopleLiked'] = \App\User::whereIn('id',$idLiked)->select('name')->get();
+
+        $meta['commentCount'] = $this->comments()->count();
+
+        return $meta;
+    }
+
 }
