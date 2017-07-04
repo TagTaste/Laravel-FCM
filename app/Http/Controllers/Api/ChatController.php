5a6fec060c734f28cf6b32e1326c6b73f2ca0323
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-07-04 18:25:16 +0530

    log

--- app/Http/Controllers/Api/ChatController.php
+++ app/Http/Controllers/Api/ChatController.php
@@ -144,7 +144,7 @@ class ChatController extends Controller
             ->leftJoin('chat_members','chat_members.chat_id','=','chats.id')
             ->where('chats.profile_id',$profileId)
             ->where('chat_members.profile_id','=',$profileId)->get();
-        
+        \Log::info("profile: " . $profileId);
         \Log::info($this->model);
         return $this->sendResponse();
 	}
