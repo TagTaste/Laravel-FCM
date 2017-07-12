140698151e6d69d3de344cfac9e0bce12892d070
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-07-12 10:32:50 +0530

    paginate

--- app/Http/Controllers/Api/Profile/TagBoardController.php
+++ app/Http/Controllers/Api/Profile/TagBoardController.php
@@ -16,7 +16,7 @@ class TagBoardController extends Controller
      */
     public function index(Request $request,$profileId)
     {
-        $ideabooks = Ideabook::profile($profileId)->orderBy('updated_at','desc')->orderBy('created_at','desc')->get();
+        $ideabooks = Ideabook::profile($profileId)->orderBy('updated_at','desc')->orderBy('created_at','desc')->paginate();
         $this->model = [];
         if($ideabooks->count() ){
             foreach($ideabooks as $ideabook){
