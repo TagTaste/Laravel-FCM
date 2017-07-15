c015a5961970f381f3c21576a63f060a6f931eb5
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-07-15 09:38:05 +0530

    cleanup

--- app/Shareable/Sharelikable/Collaborate.php
+++ app/Shareable/Sharelikable/Collaborate.php
@@ -6,10 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Collaborate extends Like
 {
-    //
-
     protected $table = 'collaborate_share_likes';
-    public $timestamps = false;
-
     protected $fillable = ['id','profile_id'];
 }
--- app/Shareable/Sharelikable/Photo.php
+++ app/Shareable/Sharelikable/Photo.php
@@ -7,7 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Photo extends Like
 {
 	protected $table = 'photo_share_likes';
-     public $timestamps = false;
-
     protected $fillable = ['id','profile_id'];
 }
