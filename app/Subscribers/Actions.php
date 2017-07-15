6299b02e4cad363a9690b3a8b8e3090933b90b1f
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-07-15 11:03:17 +0530

    change operator

--- app/Subscribers/Actions.php
+++ app/Subscribers/Actions.php
@@ -20,15 +20,14 @@ class Actions
     public function addOrUpdateSubscriber($event)
     {
         $modelId = $event->model->id;
-        $model = addslashes(get_class($event->model));
+        $model = get_class($event->model);
         $profiles = Profile::select('profiles.*')->join('model_subscribers','model_subscribers.profile_id','=','profiles.id')
-            ->where('model_subscribers.model','like',$model)
+            ->where('model_subscribers.model','=',$model)
             ->where('model_subscribers.model_id','=',$modelId)
             ->where('model_subscribers.profile_id','!=',$event->who['id'])
             ->whereNull('muted_on')
             ->whereNull('model_subscribers.deleted_at')->get();
         
-        //$profiles = ModelSubscriber::getProfiles($event->model,$event->who['id']);
         //send notification
         if($profiles->count() === 0) {
             \Log::info("No model subscribers. Not sending notification.");
