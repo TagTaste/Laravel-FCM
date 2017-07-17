4835d6b0730e25137c84a872bdb380d7e40acf00
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-07-17 14:39:31 +0530

    add tag event, notification

---
+++ app/Notifications/Actions/Tag.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Notifications\Actions;
+
+use App\Notifications\Action;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class Tag extends Action
+{
+
+}
