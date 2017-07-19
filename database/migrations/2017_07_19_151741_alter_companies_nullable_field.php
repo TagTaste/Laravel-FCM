20036e3d2cb9b379759ab76ac4e8d7d3b9d34906
ashokTagtaste <vagrant@homestead>
2017-07-19 09:59:30 +0000

    nullable about and email required field in companies

---
+++ database/migrations/2017_07_19_151741_alter_companies_nullable_field.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterCompaniesNullableField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('companies',function(Blueprint $table){
+            $table->text('about')->nullable()->change();
+            $table->string('email')->nullable(false)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('companies',function(Blueprint $table){
+            $table->text('about')->change();
+            $table->string('email')->nullable()->change();
+        });
+    }
+}
