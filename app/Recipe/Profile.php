349ae20740df83d514ea79962553549714bcf40e
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-07-20 18:30:16 +0530

    add relationships

--- app/Recipe/Profile.php
+++ app/Recipe/Profile.php
@@ -18,5 +18,94 @@ class Profile extends BaseProfile
     {
        return $this->professional !== null ? $this->professional->designation : null;
     }
-
+    
+    public function experience()
+    {
+        return $this->hasMany('App\Profile\Experience');
+    }
+    
+    public function awards()
+    {
+        return $this->belongsToMany('App\Profile\Award','profile_awards','profile_id','award_id');
+    }
+    
+    public function certifications()
+    {
+        return $this->hasMany('App\Profile\Certification');
+    }
+    
+    public function tvshows()
+    {
+        return $this->hasMany('App\Profile\Show');
+    }
+    
+    public function books()
+    {
+        return $this->hasMany('App\Profile\Book');
+    }
+    
+    public function patents()
+    {
+        return $this->hasMany('App\Profile\Patent');
+    }
+    
+    public function recipes()
+    {
+        return $this->hasMany(\App\Recipe::class);
+    }
+    
+    public function photos()
+    {
+        return $this->belongsToMany('App\Photo','profile_photos','profile_id','photo_id');
+    }
+    
+    public function projects()
+    {
+        return $this->hasMany('App\Profile\Project');
+    }
+    
+    public function education()
+    {
+        return $this->hasMany('App\Education');
+    }
+    
+    public function companies()
+    {
+        return $this->hasManyThrough('\App\Company','App\User');
+    }
+    
+    public function professional()
+    {
+        return $this->hasOne('\App\Professional');
+    }
+    
+    public function ideabooks()
+    {
+        return $this->belongsToMany('\App\Ideabook','ideabook_profiles','profile_id','ideabook_id');
+    }
+    
+    public function collaborate()
+    {
+        return $this->hasMany(\App\Collaborate::class);
+    }
+    
+    public function channels()
+    {
+        return $this->hasMany(Channel::class);
+    }
+    
+    public function subscribers()
+    {
+        return $this->hasMany(Subscriber::class);
+    }
+    
+    public function shoutouts()
+    {
+        return $this->hasMany(Shoutout::class);
+    }
+    
+    public function jobs()
+    {
+        return $this->hasMany(Job::class);
+    }
 }
