d590ae7efec182d65c2a51f7604dc1272b191e76
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-07-22 16:09:42 +0530

    change similar

--- app/Similar/Company.php
+++ app/Similar/Company.php
@@ -10,8 +10,26 @@ class Company extends BaseCompany
     
     public function similar($skip,$take)
     {
-        return self::skip($skip)
-            ->take($take)
+        /*
+            select distinct companies.id from companies
+            where companies.id not in (select distinct channels.company_id from channels
+            join subscribers on subscribers.channel_name = channels.name
+            where subscribers.channel_name like 'network.6' or subscribers.company_id = 6)
+        */
+        $distinctCompanies = \DB::table('companies')->selectRaw(\DB::raw('distinct companies.id'))
+            ->whereRaw(
+                \DB::raw(
+                    'companies.id not in (select distinct channels.company_id from channels join subscribers on subscribers.channel_name = channels.name where subscribers.channel_name like \'company.network.' . $this->id .'\' or subscribers.company_id = ' . $this->id . ')'
+                )
+            )
             ->get();
+        
+        if($distinctCompanies->count()){
+            $dist = $distinctCompanies->pluck('id')->toArray();
+            return self::whereIn('id',$dist)->skip($skip)
+                ->take($take)
+                ->get();
+        }
+        return false;
     }
 }
