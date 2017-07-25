04563342e81a731f819e4fce9829888f2e74226e
ashokTagtaste <ashok@tagtaste.com>
2017-07-25 13:21:28 +0530

    fix profile tv show

--- app/Profile/Show.php
+++ app/Profile/Show.php
@@ -11,10 +11,9 @@ class Show extends Model
     use StartEndDate, Profile;
 
     protected $table = 'profile_shows';
-    protected $fillable = ['id','title','description','channel',
-        'current','start_date','end_date','url','appeared_as'];
+    protected $fillable = ['id','title','description','channel','date','url','appeared_as'];
     protected $visible = ['id','title','description','channel',
-        'current','start_date','end_date','url','appeared_as','total'];
+        'date','url','appeared_as','total'];
     
     protected $appends = ['total'];
     
---
+++ database/migrations/2017_07_25_131324_alter_profile_tvshow_change.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterProfileTvshowChange extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table("profile_shows",function(Blueprint $table){
+            $table->renameColumn('start_date', 'date');
+            $table->dropColumn('end_date');
+            $table->dropColumn('current');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('profile_shows',function(Blueprint $table){
+            $table->renameColumn('date', 'datstart_datee');
+            $table->date('end_date')->nullable();
+            $table->boolean('current')->default('0');
+        });
+    }
+}
