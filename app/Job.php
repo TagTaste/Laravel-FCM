088763736fd186d5d79ee2e087b8f8a1a0fa2165
ashokTagtaste <ashok@tagtaste.com>
2017-08-01 18:04:13 +0530

    add resume required

--- app/Job.php
+++ app/Job.php
@@ -22,7 +22,7 @@ class Job extends Model implements Feedable
     protected $visible = ['title', 'description','why_us', 'type', 'location','key_skills',
         'profile_id','salary_min','salary_max','experience_min','experience_max','joining',
         'company_id', 'type_id', 'company', 'profile', 'profile_id','minimum_qualification',
-        'applications','created_at', 'expires_on','job_id','privacy_id'
+        'applications','created_at', 'expires_on','job_id','privacy_id','resume_required'
     ];
     
     protected $with = ['company','profile', 'applications'];
