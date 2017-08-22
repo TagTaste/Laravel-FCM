b8bacd9fca25bd41e1e49b273ca9e9a2da0c23a7
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-08-22 14:02:42 +0530

    return company profiles

--- app/Company.php
+++ app/Company.php
@@ -255,13 +255,11 @@ class Company extends Model
     /**
      * Get Company User Profiles
      *
-     * @return \App\Recipe\Profile|null
+     * @return \App\CompanyUser[]|null
      */
     public function getUsers()
     {
-        return \App\Recipe\Profile::
-            join('company_users','company_users.user_id','=','profiles.user_id')
-            ->where('company_users.company_id',$this->id)->get();
+        return \App\CompanyUser::with('profile')->where("company_id",$this->id)->get();
     }
     
     
