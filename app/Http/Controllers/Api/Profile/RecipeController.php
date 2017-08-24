de703d891953fda1b56c6a4d30e2e7d0a6f7d8c9
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-08-24 13:59:37 +0530

    save image path as well

--- app/Http/Controllers/Api/Profile/RecipeController.php
+++ app/Http/Controllers/Api/Profile/RecipeController.php
@@ -90,7 +90,7 @@ class RecipeController extends Controller
                     $count--;
                     continue;
                 }
-                $images[] = ['recipe_id' => $this->model->id, 'image' => $imageName, 'show_case' => $request->input("images.$count.showCase")];
+                $images[] = ['recipe_id' => $this->model->id, 'image' => $path . DIRECTORY_SEPARATOR . $imageName, 'show_case' => $request->input("images.$count.showCase")];
                 $count--;
             }
 
@@ -207,9 +207,9 @@ class RecipeController extends Controller
                 if ($request->input("images.$count.id") != null) {
                         $this->model->images()->where('recipe_id', $id)
                             ->where('id', $request->input("images.$count.id"))
-                            ->update(['image' => $imageName, 'show_case' => $request->input("images.$count.showCase") ?: 0]);
+                            ->update(['image' => $path . DIRECTORY_SEPARATOR . $imageName, 'show_case' => $request->input("images.$count.showCase") ?: 0]);
                 } else {
-                    $newImages[] = ['recipe_id' => $id, 'image' => $imageName,
+                    $newImages[] = ['recipe_id' => $id, 'image' => $path . DIRECTORY_SEPARATOR . $imageName,
                         'show_case' => $request->input("images.$count.showCase") ?: 0];
                 }
                 $count--;
