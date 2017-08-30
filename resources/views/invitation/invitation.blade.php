69f638d2b7a9c1da67941159d320e0bb2bf181cf
ashokTagtaste <ashok@tagtaste.com>
2017-08-30 15:44:26 +0530

    change foreign key

--- resources/views/invitation/invitation.blade.php
+++ resources/views/invitation/invitation.blade.php
@@ -1,5 +1,5 @@
 <p>Hi,</p>
 
 
-<h3>{{$userName}} has invited you to access their account</h3>
-Click the following link to verify your email {{url("/register?invite=".$inviteCode)}}
+<h3>{{$userName}} has invited you on Tagtaste account</h3>
+Click the following link for registration {{url("/register?invite=".$inviteCode)}}
