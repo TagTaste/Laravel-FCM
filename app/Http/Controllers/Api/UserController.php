756676bff6d0d69af8f929ec83643f8ab4b5f0ac
ashokTagtaste <ashok@tagtaste.com>
2017-08-30 15:37:00 +0530

    change name

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -31,14 +31,14 @@ class UserController extends Controller
         $result = ['status'=>'success'];
         if($request->input("invite_code"))
         {
-            $inviteCodeCheck = \App\Invitation::where('invite_code', $request->input("invite_code"))
+            $invitation = \App\Invitation::where('invite_code', $request->input("invite_code"))
                         ->where('email',$request->input('user.email'))->first();
-            if(!$inviteCodeCheck)
+            if(!$invitation)
             {
                 return $this->sendError("please use correct invite code");
             }
             $accepted_at = \Carbon\Carbon::now()->toDateTimeString();
-            $inviteCodeCheck->update(["accepted_at"=>$accepted_at]);
+            $invitation->update(["accepted_at"=>$accepted_at]);
         }
         $user = \App\Profile\User::addFoodie($request->input('user.name'),$request->input('user.email'),$request->input('user.password'));
         $result['result'] = ['user'=>$user];
