be9d899df12397168a55646529583353f167fec9
ashokTagtaste <ashok@tagtaste.com>
2017-08-30 13:23:21 +0530

    change code generate

--- resources/views/invitation/invitation.blade.php
+++ resources/views/invitation/invitation.blade.php
@@ -1,6 +1,5 @@
 <p>Hi,</p>
 
-<p>Someone has invited you to access their account.</p>
 
-<h1>{{$userName}} has invited you to access their account</h1>
+<h3>{{$userName}} has invited you to access their account</h3>
 Click the following link to verify your email {{url("/register?invite=".$inviteCode)}}
