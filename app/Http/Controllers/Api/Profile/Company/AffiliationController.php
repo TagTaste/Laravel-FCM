3a0bf6b8fb89de42b800ba0b271c0a34681d4c5a
ashokTagtaste <ashok@tagtaste.com>
2017-09-05 15:50:33 +0530

    add affiliations

---
+++ app/Http/Controllers/Api/Profile/Company/AffiliationController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers\Api\Profile\Company;
+
+use App\Http\Controllers\Api\Controller;
+use App\Company\Affiliation;
+use Illuminate\Http\Request;
+
+class AffiliationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request, $profileId, $companyId)
+    {
+        $this->model = Affiliation::where('company_id',$companyId)->get();
+        return $this->sendResponse();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request,$profileId, $companyId)
+    {
+        $userId = $request->user()->id;
+        $company = \App\Company::where('id',$companyId)->where('user_id',$userId)->first();
+
+        if(!$company){
+            throw new \Exception("User does not belong to this company.");
+        }
+        $data = $request->except(['_method','_token','company_id']);
+        $data['company_id'] = $companyId;
+        $this->model = $company->affiliation()->create($data);
+        return $this->sendResponse();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request,$profileId,$companyId,$id)
+    {
+        $this->model = Affiliation::where('company_id',$companyId)->where('id',$id)->first();
+        if(!$this->model){
+            throw new \Exception("Core team not found.");
+        }
+        return $this->sendResponse();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $profileId,$companyId,$id)
+    {
+        $userId = $request->user()->id;
+        $company = \App\Company::where('id',$companyId)->where('user_id',$userId)->first();
+
+        if(!$company){
+            throw new \Exception("User does not belong to this company.");
+        }
+        $data = $request->except(['_method','_token','company_id']);
+        $this->model = $company->affiliation()->where('id',$id)->update($data);
+
+
+        return $this->sendResponse();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request, $profileId, $companyId, $id)
+    {
+        $userId = $request->user()->id;
+        $company = \App\Company::where('id',$companyId)->where('user_id',$userId)->first();
+
+        if(!$company){
+            throw new \Exception("User does not belong to this company.");
+        }
+
+        $this->model = $company->affiliation()->where('id',$id)->delete();
+
+        return $this->sendResponse();
+    }
+}
