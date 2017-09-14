d1d1f4493f5f536974df53cc4e92e1cba9feae7b
Amitabh Dhiwal <amitabh@dhiwal.com>
2017-09-14 13:13:02 +0530

    change date formatting

--- app/Http/Controllers/Api/Profile/ProjectController.php
+++ app/Http/Controllers/Api/Profile/ProjectController.php
@@ -86,8 +86,7 @@ class ProjectController extends Controller
     {
         $input = $request->except(['_method','_token']);
         if(isset($input['completed_on'])){
-            $input['completed_on'] = "01-".$input['completed_on'];
-            $input['completed_on'] = empty($input['completed_on']) ? null : date("Y-m-d",strtotime(trim($input['completed_on'])));
+            $input['completed_on'] = empty($input['completed_on']) ? null : date("Y-m-d",strtotime("01-".trim($input['completed_on'])));
         }
         $this->model = Project::where('id',$id)->where('profile_id',$request->user()->profile->id)->update($input);
         return $this->sendResponse();
