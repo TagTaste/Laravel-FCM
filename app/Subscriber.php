356fda089e6cabda99b3e336afa68f147585d23b
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-09-19 13:55:33 +0530

    update redis with subscribers

--- app/Subscriber.php
+++ app/Subscriber.php
@@ -10,6 +10,22 @@ class Subscriber extends Model
     use SoftDeletes;
     protected $fillable = ['channel_name', 'profile_id','timestamp'];
     
+    public static function boot()
+    {
+        parent::boot();
+        
+        self::created(function($model){
+            \Redis::sAdd("subscribers:" . $model->channel_name, $model->profile_id);
+        });
+        
+        self::restored(function($model){
+            \Redis::sAdd("subscribers:" . $model->channel_name, $model->profile_id);
+        });
+        
+        self::deleting(function($model){
+            \Redis::sRem("subscribers:" . $model->channel_name, $model->profile_id);
+        });
+    }
     public function profile()
     {
         return $this->belongsTo('profile_id');
