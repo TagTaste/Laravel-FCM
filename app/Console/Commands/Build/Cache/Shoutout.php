552ec09f674a27baf5b43a4dcc890857304c9ff6
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-09-21 13:33:30 +0530

    shoutout cache rebuild

---
+++ app/Console/Commands/Build/Cache/Shoutout.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands\Build\Cache;
+
+use Illuminate\Console\Command;
+
+class Recipe extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'build:cache:shoutouts';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'rebuild shoutout cache';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        \App\Shoutout::chunk(200,function($models){
+            foreach($models as $model){
+                $model->addToCache();
+            }
+        });
+    }
+}
