7efb8fe291b7c311e4cac898454c81ff0786728b
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-09-21 06:35:06 +0530

    build filters for profile

---
+++ app/Console/Commands/Build/Filters/Profile.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands\Build\Filters;
+
+use Illuminate\Console\Command;
+
+class Profile extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'build:filter:profiles';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Build profile filter cache';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        \App\Recipe\Profile::chunk(200,function($models){
+            foreach($models as $model){
+                new \App\Cached\Filter\Profile($model);
+            }
+        });
+    }
+}
