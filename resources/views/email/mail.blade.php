66184ca6380af7b58911060987837aa6e96b5b56
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-09-28 17:30:12 +0530

    add message

--- resources/views/email/mail.blade.php
+++ resources/views/email/mail.blade.php
@@ -6,24 +6,15 @@
             <div class="col-md-5 col-md-offset-4">
                 <div class="panel panel-default">
                     <div class="panel-heading text-center">Send Welcome Mail</div>
+                    @if(isset($message))
+                    <p>
+                        {{ $message }}
+                    </p>
+                    @endif
                     <div class="panel-body">
                         <form class="form-horizontal col-md-12" role="form" method="POST"
                               action="/mail" >
 
-                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                            <div class="form-group{{ $errors->has('to') ? ' has-error' : '' }}">
-                                <label for="to" class="">To</label>
-
-                                <input id="to" type="email" class="form-control" name="to"
-                                       value="{{ old('to') }}" required autofocus>
-
-                                @if ($errors->has('to'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('to') }}</strong>
-                                    </span>
-                                @endif
-
-                            </div>
                             <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                                 <label for="name" class="">Name</label>
 
@@ -37,9 +28,24 @@
                                 @endif
 
                             </div>
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                            <div class="form-group{{ $errors->has('to') ? ' has-error' : '' }}">
+                                <label for="to" class="">Send mail to (email)</label>
+
+                                <input id="to" type="email" class="form-control" name="to"
+                                       value="{{ old('to') }}" required autofocus>
+
+                                @if ($errors->has('to'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('to') }}</strong>
+                                    </span>
+                                @endif
+
+                            </div>
+
 
                             <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                <label for="email" class="">E-Mail Address</label>
+                                <label for="email" class="">Tagtaste E-Mail</label>
 
                                 <input id="email" type="email" class="form-control" name="email"
                                        value="{{ old('email') }}" required autofocus>
@@ -54,7 +60,7 @@
 
                             <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                                 <label for="password" class="">Password</label>
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                <input id="password" type="text" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
