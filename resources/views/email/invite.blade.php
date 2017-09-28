51917b2f04a62e9c5ee14ceaf635048d5084ef41
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-09-28 20:56:59 +0530

    move font to the top

--- resources/views/email/invite.blade.php
+++ resources/views/email/invite.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>Tagtasteemails</title>
 </head>
-<body>
+<body style="font-family: helvetica neue;">
 <table width="100%" border ="0" cellspacing ="0" cellpadding = "0" bgcolor="#FAFAFA" style="border-bottom: 2px solid #D9222A;font-family:helvetica neue">
     <tr>
         <td>
@@ -20,58 +20,58 @@
                             </td>
                         </tr>
                         <tr>
-                            <td style="font-size: 24px;	font-weight: bold;font-family: helvetica neue;">
-                                <h1  style="font-size: 24px;font-weight: bold;padding:0px;margin: 0px 0px 20px 0px;color:#000;"> Welcome aboard,<span style="color: #D9222A;"> {{$name}}! </span></h1>
+                            <td style="font-size: 24px;	font-weight: bold;">
+                                <h1  style="font-size: 24px;font-weight: bold;padding:0px;margin: 0px 0px 20px 0px;color:#000;"> Welcome aboard,<span style="color: #D9222A;"> {{$name}}. </span></h1>
                             </td>
                         </tr>
                         {{--<tr>--}}
-                            {{--<td style="color: rgba(0,0,0,0.9);font-size: 16px;font-family: helvetica neue;">--}}
+                            {{--<td style="color: rgba(0,0,0,0.9);font-size: 16px;">--}}
                                 {{--<p style="margin:0px 0px 20px 0px;color: rgba(0,0,0,0.9);"> Dear {{$name}},</p>--}}
                             {{--</td>--}}
                         {{--</tr>--}}
                         <tr>
-                            <td padding="11px 0px 0px 0px" style="color: rgba(0,0,0,0.9);font-size: 16px;font-family: helvetica neue;">
+                            <td padding="11px 0px 0px 0px" style="color: rgba(0,0,0,0.9);font-size: 16px;">
                                 <p style="margin: 0px 0px 0px 0px;">
-                                    We are delighted to share with you that TagTaste is live in Beta!
+                                    We are delighted to share with you that TagTaste is live in Beta Phase!
                                 </p>
                             </td>
                         </tr>
                         <tr>
-                            <td padding="11px 0px 0px 0px" style="color: rgba(0,0,0,0.9);font-size: 16px;font-family: helvetica neue;">
+                            <td padding="11px 0px 0px 0px" style="color: rgba(0,0,0,0.9);font-size: 16px;">
                                 <p style="margin: 0px 0px 20px 0px">
                                     Please explore the platform using these credentials:
                                 </p>
                             </td>
                         </tr>
                         <tr>
-                            <td padding="11px 0px 0px 0px" style="color: rgba(0,0,0,0.9);font-size: 16px;font-family: helvetica neue;">
+                            <td padding="11px 0px 0px 0px" style="color: rgba(0,0,0,0.9);font-size: 16px;">
                                 <p style="margin-bottom: 0px;margin-top:0px;">Login ID: <span style="text-decoration:none;color:#0027F9;">{{$email}}</span></p>
                                 <p style="margin-bottom: 20px;margin-top:0px;">Password: <span style="color:#0027F9;">{{$password}}</span></p>
                             </td>
                         </tr>
 
                         <tr>
-                            <td padding="11px 0px 0px 0px" style="color: rgba(0,0,0,0.9);font-size: 16px;font-family: helvetica neue;">
+                            <td padding="11px 0px 0px 0px" style="color: rgba(0,0,0,0.9);font-size: 16px;">
                                 <p style="margin: 0px 0px 40px 0px;margin-top:0px;">We are honoured to be working closely with you to make TagTaste, the de facto platform for food professionals across the world.</p>
                             </td>
                         </tr>
-                        <tr padding="15px 0px 0px 10px" style="font-family: helvetica neue;">
+                        <tr padding="15px 0px 0px 10px" style="">
                             <td align="center">
                                 <a href="https://www.tagtaste.com" style="color: #FFFFFF;text-decoration:none;padding: 7px 23px;border-radius: 4px;background-color: #D9222A;box-shadow: none;border: none;font-size: 18px;cursor:pointer;">Login to your account</a>
                             </td>
                         </tr>
-                        <tr style="font-family: helvetica neue;">
+                        <tr style="">
                             <td align="center">
                                 <p style="color: rgba(0,0,0,0.6);font-size: 12px;margin:10px 0px 0px 0px"> You can also copy/paste this link in your browser: <a href="https://www.tagtaste.com" style="color: #0027F9;text-decoration:none;">https://www.tagtaste.com</a></p>
                             </td>
                         </tr>
                         <tr>
-                            <td padding="11px 0px 0px 0px" style="color: rgba(0,0,0,0.9);font-size: 16px;font-family: helvetica neue;">
+                            <td padding="11px 0px 0px 0px" style="color: rgba(0,0,0,0.9);font-size: 16px;">
                                 <p style="margin: 40px 0px 0px 0px;"><span style="font-weight: bold">Note:</span> We are currently in beta phase and the platform works best on the desktop. We plan to release the mobile apps soon after incorporating your valuable feedback. Please share your experience at  <a href="mailto:feedback@tagtaste.com" style="text-decoration:none;color:#0027F9 ">feedback@tagtaste.com</a>.</p>
                             </td>
                         </tr>
 
-                        {{--<tr style="font-family: helvetica neue;">--}}
+                        {{--<tr style="">--}}
                             {{--<td>--}}
                                 {{--<p style="font-size: 18px;font-weight: bold; margin: 20px 0px 0px 0px;color:#000">What is TagTaste?</p>--}}
                                 {{--<p style="color: rgba(0,0,0,0.8);font-size: 16px; margin: 0px;">TagTaste is World's first online community for food professionals to discover, network and collaborate with each other.</p>--}}
@@ -79,13 +79,13 @@
                         {{--</tr>--}}
 
 
-                        <tr style="font-family: helvetica neue;">
+                        <tr style="">
                             <td>
                                 <p style="font-size: 18px;font-weight: bold; margin: 20px 0px 0px 0px;color:#000">Need Help?</p>
-                                <p style="color: rgba(0,0,0,0.8);font-size: 16px; margin: 0px;">Please contact us at <a href="mailto:contact@tagtaste.com" style="text-decoration:none;color: #0027F9;">contact@tagtaste.com</a>. We’ll be happy to respond.</p>
+                                <p style="color: rgba(0,0,0,0.8);font-size: 16px; margin: 0px;">Please feel free to contact us at <a href="mailto:contact@tagtaste.com" style="text-decoration:none;color: #0027F9;">contact@tagtaste.com</a> for any assistance.</p>
                             </td>
                         </tr>
-                        <tr style="font-family: helvetica neue;">
+                        <tr style="">
                             <td>
                                 <p style="color: rgba(0,0,0,0.9);font-size: 16px;font-weight: 400;margin:25px 0px 0px 0px;">Cheers</p>
                                 <p  style="color:rgba(0,0,0,0.9);font-size: 16px;font-weight: 600;margin:0px">Team TagTaste</p>
@@ -96,7 +96,7 @@
             </table>
         </td>
     </tr>
-    <tr style="font-family: helvetica neue;">
+    <tr style="">
         <td align="center" style="color:#000000">
             <table class="container" width= "620" align="center" border="0" cellspacing="0" cellpadding="0" border-collapse="collapse">
                 <p style="margin:10px 0px 50px 0px;text-align:center"> &copy; TagTaste Food Pvt. Ltd. | 2017</p>
