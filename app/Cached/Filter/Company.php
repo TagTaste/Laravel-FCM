133726ce5b4f511a3095facfe32d62e1a4d373d3
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-10-03 18:38:47 +0530

    add required attributes

--- app/Cached/Filter/Company.php
+++ app/Cached/Filter/Company.php
@@ -1,7 +1,32 @@
 <?php namespace App\Cached\Filter;
 
+use App\Company\Type;
 
 class Company extends BaseFilter
 {
-    protected $attributes = ['city','keywords','speciality'];
+    protected $attributes = ['location','keywords','speciality','type'];
+    
+    public function getValueOfLocation()
+    {
+        return $this->model->city;
+    }
+    
+    public function getValueOfKeywords()
+    {
+        return explode(",",$this->model->keywords);
+    }
+    
+    public function getValueOfSpeciality()
+    {
+        return explode(",",$this->model->speciality);
+    }
+    
+    public function getValueOfType()
+    {
+        $types = \Cache::remember("company_types",10,function(){
+            return Type::select("id","name")->get()->pluck("name",'id');
+        });
+        
+        return !is_null($this->model->type) && isset($types[$this->model->type]) ? $types[$this->model->type] : false;
+    }
 }
