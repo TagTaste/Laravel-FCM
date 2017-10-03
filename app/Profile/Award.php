fa59e6bbbba9fa8607a643d2da715ec38d3a2320
ashokTagtaste <ashok@tagtaste.com>
2017-10-03 10:58:23 +0530

    change in award in profile

--- app/Profile/Award.php
+++ app/Profile/Award.php
@@ -25,19 +25,6 @@ class Award extends Model
             $builder->orderBy('date', 'desc');
         });
     }
-
-    public function setDateAttribute($value)
-    {
-        if(!empty($value)){
-            $value = "01-".$value;
-            $this->attributes['date'] = date('Y-m-d',strtotime($value));
-        }
-    }
-
-    public function getDateAttribute($value)
-    {
-        return is_null($value) ? null : date("m-Y",strtotime($value));
-    }
     
     public function profile()
     {
