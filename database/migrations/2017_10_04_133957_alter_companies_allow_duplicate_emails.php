826d2bec6010c4e04f9d257b56dfa251929bbc7b
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-10-04 14:10:52 +0530

    drop unique contraint

--- database/migrations/2017_10_04_133957_alter_companies_allow_duplicate_emails.php
+++ database/migrations/2017_10_04_133957_alter_companies_allow_duplicate_emails.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AlterCompaniesAllowDuplicateEmails extends Migration
 {
@@ -13,9 +13,11 @@ class AlterCompaniesAllowDuplicateEmails extends Migration
      */
     public function up()
     {
+        Schema::disableForeignKeyConstraints();
         Schema::table("companies",function(Blueprint $table){
-            $table->dropUnique("companies_email_unique");
+            $table->dropIndex('companies_email_unique');
         });
+        Schema::enableForeignKeyConstraints();
     }
 
     /**
