17fea82fd474f78c37df5c9888fd4cdba849c4b1
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-10-06 17:30:08 +0530

    send model and id

--- app/Http/Controllers/Api/HandleController.php
+++ app/Http/Controllers/Api/HandleController.php
@@ -2,32 +2,27 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Profile;
-use App\User;
-use App\Company;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class HandleController extends Controller
 {
-	public function show(Request $request,$handle)
-	{   
-		$profile = \App\Profile\User::whereHas("profile",function($query) use ($handle){
-            $query->where('handle',$handle);
-            })->first();
-
+	public function getId(Request $request,$handle)
+	{
+	    $profile = \DB::table("profiles")->select("id")->where('handle','like',$handle)->first();
+     
 		if($profile !== null){
-			$this->model = $profile;
+		    $this->model = ['model'=>'profile','id'=>$profile->id];
 			return $this->sendResponse();
 		}
 
-        $company = Company::where('handle',$handle)->first();
+        $company = \DB::table('companies')->select('id')->where('handle',$handle)->first();
 
     	if($company !== null)
     	{
-    		$this->model = ['company'=>$company]; 	 
+    		$this->model = ['model'=>'company','id'=>$company->id];
     		return $this->sendResponse();
     	}
-        return $this->sendError("Handle not found.");
+    	
+        return $this->sendError("$handle Handle not found.");
 	}
 }
