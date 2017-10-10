3fc3efcaa9caf076582fce3d3a8273ac3d8d048d
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-10-10 09:54:19 +0530

    add more columns to search

--- app/Documents/Company.php
+++ app/Documents/Company.php
@@ -4,5 +4,5 @@ class Company extends Document
 {
     public $type = 'company';
     
-    public $bodyProperties = ['name','cuisines','profileId','productCatalogue'];
+    public $bodyProperties = ['name','cuisines','profileId','productCatalogue','keywords','about','city','registered_address'];
 }
