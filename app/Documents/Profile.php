4d58e71004347eb69ed326e0eba6f4297e99c092
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-10-10 10:17:57 +0530

    update profile document

--- app/Documents/Profile.php
+++ app/Documents/Profile.php
@@ -4,5 +4,5 @@ class Profile extends Document
 {
     public $type = 'profile';
     
-    public $bodyProperties = ['name','handle','ingredients'];
+    public $bodyProperties = ['name','handle','ingredients','about','address','interests','expertise','keywords','city'];
 }
