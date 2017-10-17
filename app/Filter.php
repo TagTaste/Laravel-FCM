8719205b2830f19cba6d1bf8e1533c20f5e6e4eb
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-10-17 19:32:36 +0530

    make solution generic

--- app/Filter.php
+++ app/Filter.php
@@ -10,6 +10,102 @@ class Filter extends Model
     public $separator = ',';
     public static $cacheKey = null;
     
+    public static function addKey($relatedColumnId, $key, $value, $delimiter=false)
+    {
+        if(!$delimiter){
+            return static::insert(
+                [
+                    static::$relatedColumn => $$relatedColumnId,
+                    'key' => $key,
+                    'value' => $value
+                ]);
+        }
+        
+        $data = [];
+        $value = explode(',',$value);
+        foreach($value as $v){
+            $data[] = [
+                static::$relatedColumn=>$relatedColumnId,
+                'key' => $key,
+                'value' => $v
+            ];
+        }
+        
+        return static::insert($data);
+    }
+    
+    public static function removeKey($relatedColumnId,$key,$value = null)
+    {
+        $filter = static::where(static::$relatedColumn,$relatedColumnId)
+            ->where('key',$key);
+        
+        if($value){
+            $filter = $filter->where('value',$value);
+        }
+        
+        return $filter->delete();
+    }
+    
+    public static function updateKey($relatedColumnId, $key, $value, $separator=false)
+    {
+        static::removeKey($relatedColumnId,$key);
+        
+        //create new filter
+        return static::addKey($relatedColumnId,$key,$value,$separator);
+        
+    }
+    
+    public static function addModel($model)
+    {
+        $self = new self;
+        foreach($self->csv as $filter){
+            if(isset($model->{$filter})){
+                static::updateKey($model->id,$filter,$model->{$filter},',');
+            }
+        }
+        
+        foreach($self->strings as $filter){
+            if(isset($model->{$filter})){
+                static::updateKey($model->id,$filter,$model->{$filter});
+            }
+        }
+        
+    }
+    
+    public static function getFilters()
+    {
+        $filters = static::select('key','value',\DB::raw('count(`key`) as count'))
+            ->groupBy('key','value')->orderBy('count','desc')->take(10)->get()->groupBy('key');
+        
+        foreach($filters as $key=>&$sub){
+            foreach($sub as &$filter){
+                unset($filter->key);
+            }
+        }
+        return $filters;
+    }
+    
+    public static function getModels($filters)
+    {
+        $models = null;
+        foreach($filters as $filter => $value){
+            
+            $model = static::selectRaw('distinct ' . static::$relatedColumn)->where('key',$filter)->whereIn('value',$value)->get()
+                ->pluck(static::$relatedColumn);
+            if(is_null($models)){
+                $models = $model;
+                continue;
+            }
+            $models = $model->intersect($models);
+        }
+        
+        if(count($models) == 0){
+            return $models;
+        }
+        
+        return static::getFromCache($models);
+    }
+    
     public static function getFromCache(&$modelIds)
     {
         $models = [];
