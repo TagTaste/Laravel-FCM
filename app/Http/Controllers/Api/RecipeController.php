3548ff4f1dc0f825a97f9405a91f18f1aaba904e
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-10-18 14:32:41 +0530

    new recipe filters

--- app/Http/Controllers/Api/RecipeController.php
+++ app/Http/Controllers/Api/RecipeController.php
@@ -18,82 +18,9 @@ class RecipeController extends Controller
         $recipes = Recipe::orderBy('created_at')->whereNull('deleted_at');
 
         $filters = $request->input('filters');
-        if (!empty($filters['cuisine_id'])) {
-            $recipes = $recipes->whereIn('cuisine_id', $filters['cuisine_id']);
-        }
-
-        if (!empty($filters['level'])) {
-            $recipes = $recipes->whereIn('level', $filters['level']);
-        }
-
-        if (!empty($filters['type'])) {
-            $recipes = $recipes->whereIn('type', $filters['type']);
-        }
-
-        if (!empty($filters['vegetarian'])) {
-            $recipes = $recipes->where('is_vegetarian', $filters['vegetarian']);
-        }
-        
-        if(!empty($filters['preparation_time'])){
-            $start = [
-                0 => 1,
-                1 => 1,
-                2 => 4
-            ];
-    
-            $end = [
-                0 => 1,
-                1 => 4,
-                2 => 8
-            ];
-            $startAt = 999; //a very high number
-            $endAt = 0;
-            foreach($filters['preparation_time'] as $prepTime){
-                if($start[$prepTime] < $startAt){
-                    $startAt = $start[$prepTime];
-                }
-                
-                if($end[$prepTime] > $endAt){
-                    $endAt = $end[$prepTime];
-                }
-            }
-    
-            $recipes = $recipes->where('preparation_time','>=',$startAt)
-                ->where('preparation_time','<=',$endAt);
-        }
-        
-        if(!empty($filters['ingredients'])){
-            $recipes = $recipes->whereHas('ingredients',function($query) use (&$filters){
-                $query->whereIn('id',$filters['ingredients']);
-            });
-        }
-    
-        if(!empty($filters['cooking_time'])){
-            $start = [
-                0 => 1,
-                1 => 1,
-                2 => 4
-            ];
-        
-            $end = [
-                0 => 1,
-                1 => 4,
-                2 => 8
-            ];
-            $startAt = 999; //a very high number
-            $endAt = 0;
-            foreach($filters['cooking_time'] as $prepTime){
-                if($start[$prepTime] < $startAt){
-                    $startAt = $start[$prepTime];
-                }
-            
-                if($end[$prepTime] > $endAt){
-                    $endAt = $end[$prepTime];
-                }
-            }
-        
-            $recipes = $recipes->where('cooking_time','>=',$startAt)
-                ->where('cooking_time','<=',$endAt);
+        if(!empty($filters)){
+            $this->model = \App\Filter\Recipe::getModels($filters);
+            return $this->sendResponse();
         }
 
         $this->model = [];
