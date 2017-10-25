2116f6ea75f474e435a127c9f62ce75bbd716009
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-10-25 15:27:12 +0530

    add version middleware

---
+++ app/Http/Middleware/Api/VersionCheck.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware\Api;
+
+use App\Version;
+use Closure;
+
+class VersionCheck
+{
+    private $versionKey = 'X-API-VERSION';
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $version = $request->header($this->versionKey);
+    
+        $api = Version::getVersion();
+        //if version key not specified, we've got a badass. Let 'em through.
+        //or if the version is compatible;
+        if(!$version || $api->isCompatible($version)){
+            $response = $next($request);
+            $response->headers->add($api->toHeaders());
+            return $response;
+        }
+        
+        return response()->json(['error'=>'incompatible_version',
+            'message'=>'force_update'],400);
+    
+    }
+}
--- app/Version.php
+++ app/Version.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Version extends Model
 {
     protected $fillable = ['compatible_version', 'latest_version'];
+    
+    public function isCompatible($version)
+    {
+        return $this->compatible_version === $version;
+    }
+    
+    public static function getVersion()
+    {
+        return static::select("compatible_version","latest_version")->first();
+    }
+    
+    public function toHeaders()
+    {
+        return ['X-Compatible-Version'=>$this->compatible_version,'X-Latest-Version'=>$this->latest_version];
+    }
 }
