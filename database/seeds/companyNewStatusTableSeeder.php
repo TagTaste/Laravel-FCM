8014eb23c995658c34dc8de88f9a5f5d154456ec
ashokTagtaste <ashok@tagtaste.com>
2017-11-16 19:46:08 +0530

    add seeder for company types and statuses

---
+++ database/seeds/companyNewStatusTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class companyNewStatusTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Company\Status::where('id',1)->update(['name'=>'Operational']);
+        \App\Company\Status::where('id',2)->update(['name'=>'Operational Subsidiary']);
+        \App\Company\Status::where('id',3)->update(['name'=>'Out of Business ']);
+        \App\Company\Status::where('id',4)->update(['name'=>'Acquired']);
+        \App\Company\Status::where('id',5)->update(['name'=>'Others']);
+    }
+}
