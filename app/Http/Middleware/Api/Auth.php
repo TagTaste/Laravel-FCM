9c0c83be7a3c2c9a5eb37db5ff96b827bf88ddf8
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-11-23 16:02:15 +0530

    log

--- app/Http/Middleware/Api/Auth.php
+++ app/Http/Middleware/Api/Auth.php
@@ -24,12 +24,15 @@ class Auth extends GetUserFromToken
             $user = \App\User::find(1);
             $token = \JWTAuth::fromUser($user);
             $this->auth->authenticate($token);
+            
             return $next($request);
         }
         
         if (! $token = $this->auth->setRequest($request)->getToken()) {
             return $this->respond('tymon.jwt.absent', 'token_not_provided', 400);
         }
+        
+        \Log::info($request->url());
         \Log::info("token:" . $token);
         try {
             $user = $this->auth->authenticate($token);
