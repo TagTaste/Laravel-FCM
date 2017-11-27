bdfd5a27e03ce0c8ef0dbf90160cad66794eb563
ashokTagtaste <ashok@tagtaste.com>
2017-11-27 16:25:41 +0530

    add new queue

---
+++ app/Jobs/ChangeOtp.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Profile;
+use GuzzleHttp\Client;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class ChangeOtp implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public $loggedInProfileId;
+    public function __construct($loggedInProfileId)
+    {
+        $this->loggedInProfileId = $loggedInProfileId;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Profile::where('id',$this->loggedInProfileId)->update(['otp'=>null]);
+    }
+}
--- app/Jobs/PhoneVerify.php
+++ app/Jobs/PhoneVerify.php
@@ -3,6 +3,7 @@
 namespace App\Jobs;
 
 use App\Profile;
+use Carbon\Carbon;
 use GuzzleHttp\Client;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
@@ -39,8 +40,9 @@ class PhoneVerify implements ShouldQueue
         $client = new Client();
         $response = $client->get("http://websmsapp.in/api/mt/SendSMS?APIKey=".env('TOP10SMS_API_KEY')."&senderid=".env('TOP10SMS_SENDERID')."&channel=Trans&DCS=0&flashsms=0&number=91".$this->phone."&text=".$otp." is your One Time Password to verify your Number with TagTaste. Valid for 5 min&route=2");
         $this->model = Profile::where('id',$loggedInProfileId)->update(['otp'=>$otp]);
-        sleep(300);
-        Profile::where('id',$loggedInProfileId)->update(['otp'=>null]);
+        $job = ((new ChangeOtp($loggedInProfileId))->onQueue('phone_verify'))->delay(Carbon::now()->addMinutes(5));
+
+        dispatch($job);
 
     }
 }
