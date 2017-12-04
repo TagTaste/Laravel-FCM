5df920baf831398a31ebc9189a94b8775afd9436
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-12-04 21:47:58 +0530

    use custom monolog handler

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,5 @@
 <?php
-
+ use Maxbanton\Cwh\Handler\CloudWatch;
 /*
 |--------------------------------------------------------------------------
 | Create The Application
@@ -51,5 +51,27 @@ $app->singleton(
 | from the actual running of the application and sending responses.
 |
 */
+
+$app->configureMonologUsing( function($monolog) {
+    
+    if(env('log_to_file')){
+        $formatter = new \Monolog\Formatter\LineFormatter(null, null, false, true);
+        $infoHandler = new \Monolog\Handler\StreamHandler('/home/vagrant/Code/webapp/storage/logs/laravel.log');
+        $infoHandler->setFormatter($formatter);
+        $monolog->pushHandler($infoHandler);
+    }
+   
+    $connection = new \PhpAmqpLib\Connection\AMQPStreamConnection(
+        env('rabbitmq_host','localhost'),
+        env('rabbitmq_port','5672'),
+        env('rabbitmq_user','test'),
+        env('rabbitmq_password','test'));
+    $channel = $connection->channel();
+    $channel->exchange_declare('log', 'direct', false, false, false);
+    
+    $rmqhandler = new \App\Handler\RabbitMQHandler($channel);
+    $monolog->pushHandler($rmqhandler);
+    
+});
 
 return $app;
