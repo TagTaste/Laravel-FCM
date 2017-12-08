d093c914b3fd3a6b1be97cbfb6b9199d06599f4d
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-12-08 12:24:38 +0530

    add chat limit check

--- app/Policies/ChatLimitPolicy.php
+++ app/Policies/ChatLimitPolicy.php
@@ -2,6 +2,7 @@
 
 namespace App\Policies;
 
+use App\ChatLimit;
 use App\User;
 use App\Chat;
 use Illuminate\Auth\Access\HandlesAuthorization;
@@ -9,18 +10,9 @@ use Illuminate\Auth\Access\HandlesAuthorization;
 class ChatLimitPolicy
 {
     use HandlesAuthorization;
-
-    /**
-     * Determine whether the user can view the chat.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Chat  $chat
-     * @return mixed
-     */
-    public function view(User $user, Chat $chat)
-    {
-        //
-    }
+    
+    private static $MAX = 5;
+    private static $REMAINING = 5;
 
     /**
      * Determine whether the user can create chats.
@@ -30,19 +22,36 @@ class ChatLimitPolicy
      */
     public function create(User $user)
     {
-        //
+        $profileId = $user->profile->id;
+        
+        return $this->checkLimit($profileId);
     }
-
-    /**
-     * Determine whether the user can update the chat.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Chat  $chat
-     * @return mixed
-     */
-    public function update(User $user, Chat $chat)
+    
+    private function checkLimit($profileId)
     {
-        //
+        $currentLimit = ChatLimit::where('profile_id',$profileId)->first();
+    
+        if(!$currentLimit){
+            $currentLimit = $this->createLimit($profileId,self::$MAX,self::$REMAINING);
+        }
+        
+        //for admins, marketing people
+        if($currentLimit->max == null){
+            return true;
+        }
+        
+        if($currentLimit->remaining > 0){
+            $currentLimit->remaining--;
+            $currentLimit->save();
+            return true;
+        }
+        
+        return false;
+    }
+    
+    private function createLimit(&$profileId,$max=null,$remaining=5)
+    {
+        return ChatLimit::create(['profile_id'=>$profileId,'max'=>$max,'remaining'=>$remaining]);
     }
 
     /**
@@ -54,6 +63,23 @@ class ChatLimitPolicy
      */
     public function delete(User $user, Chat $chat)
     {
-        //
+        $profileId = $user->profile->id;
+    
+        $currentLimit = ChatLimit::where('profile_id',$profileId)->first();
+        
+        if(!$currentLimit){
+            $currentLimit = $this->createLimit($profileId,self::$MAX,self::$REMAINING);
+        }
+        
+        if($currentLimit->max == null){
+            return true;
+        }
+        
+        if($currentLimit->max > $currentLimit->remaining){
+            $currentLimit->remaining++;
+            $currentLimit->save();
+        }
+        
+        return true;
     }
 }
