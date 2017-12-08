fcc954ac6008e8bf945c2781a598ea91a2159503
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-12-08 12:06:39 +0530

    init chat policy

---
+++ app/Policies/ChatLimitPolicy.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Chat;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ChatLimitPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the chat.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Chat  $chat
+     * @return mixed
+     */
+    public function view(User $user, Chat $chat)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create chats.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the chat.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Chat  $chat
+     * @return mixed
+     */
+    public function update(User $user, Chat $chat)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the chat.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Chat  $chat
+     * @return mixed
+     */
+    public function delete(User $user, Chat $chat)
+    {
+        //
+    }
+}
