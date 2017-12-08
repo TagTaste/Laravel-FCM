2824fa824bc9507aa16b03dad3c28a134e4338fe
Amitabh Dhiwal <amitabh@tagtaste.com>
2017-12-08 12:06:52 +0530

    register chat policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Chat;
+use App\Policies\ChatLimitPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -13,7 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+        Chat::class => ChatLimitPolicy::class
     ];
 
     /**
