4695f400fd799a1b9ead235527517a86ceb03c0d
ashokTagtaste <ashok@tagtaste.com>
2017-12-13 18:30:51 +0530

    change code

--- app/Profile/User.php
+++ app/Profile/User.php
@@ -256,7 +256,7 @@ class User extends BaseUser
             Profile::where('id',$this->profile->id)->update(['image'=>'images/p/'.$this->profile->id.'/'.$filename]);
         }
 
-        \App\User::where('id',request()->user()->id)->update(['verified_at'=>\Carbon\Carbon::now()->toDateTimeString()]);
+        \App\User::where('email',$this->email)->update(['verified_at'=>\Carbon\Carbon::now()->toDateTimeString()]);
     }
 
     public function getSocial($typeId)
