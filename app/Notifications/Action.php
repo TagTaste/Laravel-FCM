7c79f48f08f2dc34ee94e9768902e7a75ecf2078
ashokTagtaste <ashok@tagtaste.com>
2017-12-23 14:49:24 +0530

    change

--- app/Notifications/Action.php
+++ app/Notifications/Action.php
@@ -39,7 +39,7 @@ class Action extends Notification
      */
     public function via($notifiable)
     {
-        return ['database',FCMPush::class,'broadcast','mail'];
+        return ['database',FCMPush::class,'broadcast'];
     }
 
     /**
