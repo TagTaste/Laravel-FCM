676cad3cc5e7aa3c576c14babd28c7822d860236
ashokTagtaste <ashok@tagtaste.com>
2017-12-29 18:19:21 +0530

    job joining nullable

--- database/migrations/2017_12_29_180025_alter_job_joining_nullable.php
+++ database/migrations/2017_12_29_180025_alter_job_joining_nullable.php
@@ -25,7 +25,7 @@ class AlterJobJoiningNullable extends Migration
      */
     public function down()
     {
-        Schema::table("users",function(Blueprint $table){
+        Schema::table("jobs",function(Blueprint $table){
             $table->string('joining')->change();
         });
     }
