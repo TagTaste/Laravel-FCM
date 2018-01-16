a77c855a2558f517c70c816e9afee23380a116d0
ashokTagtaste <ashok@tagtaste.com>
2018-01-16 19:14:18 +0530

    change

--- app/Shoutout.php
+++ app/Shoutout.php
@@ -175,6 +175,7 @@ class Shoutout extends Model implements Feedable
         $data['description'] = substr($this->content,0,155);
         $data['ogTitle'] = 'Shared post on Tagtaste';
         $data['ogDescription'] = substr($this->content,0,65);
+        $data['ogImage'] = null;
         $data['ogUrl'] = 'http://139.59.59.78:8081/preview/shoutout/'.$this->id;
 
         return $data;
