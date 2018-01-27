602c0a3898c7c36fa4d2e13c67a025da90c7b56b
Amitabh Dhiwal <amitabh@tagtaste.com>
2018-01-27 18:45:13 +0530

    log

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -628,6 +628,7 @@ class ProfileController extends Controller
             $data = \Redis::mget($profileIds);
 
         }
+        \Log::info("followerscount:" . count($data));
         foreach($data as &$profile){
             if(empty($profile)){
                 continue;
@@ -636,6 +637,7 @@ class ProfileController extends Controller
             $profile->isFollowing = \Redis::sIsMember("followers:profile:".$profile->id,$loggedInProfileId) === 1;
             $profile->self = false;
         }
+        \Log::info($data);
         $this->model = array_filter($data);
         return $this->sendResponse();
     }
