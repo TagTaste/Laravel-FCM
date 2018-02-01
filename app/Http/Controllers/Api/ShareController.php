86f3edd42cc51670033b9bf179b6bcb4d0da70b7
Amitabh Dhiwal <amitabh@tagtaste.com>
2018-02-01 15:27:41 +0530

    log

--- app/Http/Controllers/Api/ShareController.php
+++ app/Http/Controllers/Api/ShareController.php
@@ -70,6 +70,7 @@ class ShareController extends Controller
     
         if($loggedInProfileId != $sharedModel->profile_id){
             $this->model->profile_id = $sharedModel->profile_id;
+            \Log::info($this->model);
             event(new Share($this->model,$request->user()->profile));
         }
         
