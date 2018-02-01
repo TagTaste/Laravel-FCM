f969e6de69df058144a4320904b5d2f2ac0fe40c
Amitabh Dhiwal <amitabh@tagtaste.com>
2018-02-01 14:18:05 +0530

    set profile id

--- app/Http/Controllers/Api/ShareController.php
+++ app/Http/Controllers/Api/ShareController.php
@@ -64,14 +64,15 @@ class ShareController extends Controller
         
         //push to feed
         event(new NewFeedable($this->model, $request->user()->profile));
-        $sharedModel->shared_id = $this->model->id;
-        \Log::info($sharedModel);
-        if($loggedInProfileId != $sharedModel->profile_id){
-            event(new Share($this->model,$request->user()->profile));
-        }
-    
+        
         //add model subscriber
         event(new Create($this->model,$request->user()->profile));
+    
+        if($loggedInProfileId != $sharedModel->profile_id){
+            $this->model->profile_id = $sharedModel->profile_id;
+            event(new Share($this->model,$request->user()->profile));
+        }
+        
         return $this->sendResponse();
     }
     
