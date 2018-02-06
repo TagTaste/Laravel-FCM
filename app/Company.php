66301810f1df1dc0ca92434d2adbdb3408eadd9a
aman95 <aman1995k@gmail.com>
2018-02-06 13:14:36 +0530

    Added preview content to profile and company models

--- app/Company.php
+++ app/Company.php
@@ -526,4 +526,22 @@ class Company extends Model
         ];
     }
 
+    public function getPreviewContent()
+    {
+        $data = [];
+        $data['title'] = 'Check out '.$this->name.' on TagTaste';
+        $data['description'] = substr($this->about,0,155);
+        $data['ogTitle'] = 'Check out '.$this->name.' on TagTaste';
+        $data['ogDescription'] = substr($this->about,0,65);
+        $data['ogImage'] = $this->logo;
+        $data['cardType'] = 'summary_large_image';
+        $data['ogUrl'] = env('APP_URL').'/companies/'.$this->id;
+        if(empty($this->logo)) {
+            $data['cardType'] = 'summary';
+        }
+
+        return $data;
+
+    }
+
 }
