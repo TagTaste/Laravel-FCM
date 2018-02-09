f532bcd3f5c23d2b1a6579d17517786553db946e
aman95 <aman1995k@gmail.com>
2018-02-09 14:52:51 +0530

    Fixed self like notification

--- app/Notifications/Actions/Like.php
+++ app/Notifications/Actions/Like.php
@@ -16,4 +16,20 @@ class Like extends Action
         parent::__construct($event);
         $this->notification = $this->data->who['name'] . " liked a post.";
     }
+
+    // Overriding this function to prevent self like notification
+    public function via($notifiable)
+    {
+        if($this->data->who['id'] == $notifiable->id) {
+            return [];
+        }
+
+        $via = ['database',FCMPush::class,'broadcast'];
+
+        if($this->view && view()->exists($this->view)){
+            $via[] = 'mail';
+
+        }
+        return $via;
+    }
 }
