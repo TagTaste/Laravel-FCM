d241b4c4a3045bc4f10b4bdd948273f165fa0c86
aman95 <aman1995k@gmail.com>
2018-02-12 18:51:56 +0530

    change

--- resources/views/emails/comment.blade.php
+++ resources/views/emails/comment.blade.php
@@ -132,7 +132,7 @@
                                                             <tr>
                                                                 <td valign="top" align="center"  bgcolor="#FFFFFF" >
                                                                 
-                                                                        <a href="{{ \App\Deeplink::getShortLink($model['name'], $model['id']) }}" style="text-decoration:none;padding: 15px 36px;border-radius: 4px;color: #FFFFFF;background-color: #D9222A;box-shadow: none;border: none;font-size: 18px;border-radius: 30px; font-weight: normal">VIEW ON TAGTASTE</a>
+                                                                        <a href="{{ \App\Deeplink::getLongLink($model['name'], $model['id']) }}" style="text-decoration:none;padding: 15px 36px;border-radius: 4px;color: #FFFFFF;background-color: #D9222A;box-shadow: none;border: none;font-size: 18px;border-radius: 30px; font-weight: normal">VIEW ON TAGTASTE</a>
                                                                 
                                                                 </td>
                                                             </tr>
