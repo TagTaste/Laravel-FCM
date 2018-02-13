aea81613ef5503e33a5e254887923818b65b6aba
aman95 <aman1995k@gmail.com>
2018-02-13 15:22:56 +0530

    Added check in comment mailer

--- app/Notifications/Actions/Comment.php
+++ app/Notifications/Actions/Comment.php
@@ -3,6 +3,7 @@
 
 namespace App\Notifications\Actions;
 
+use App\FCMPush;
 use App\Notifications\Action;
 
 class Comment extends Action
@@ -18,5 +19,18 @@ class Comment extends Action
         $this->sub = $this->data->who['name'] ." commented on your post";
         $this->notification = $this->sub;
 
+    }
+
+    public function toMail($notifiable)
+    {
+        if($notifiable->id != $this->model->profile_id) {
+            $this->sub = $this->data->who['name'] ." commented on a post on which you have commented";
+        }
+
+        if(view()->exists($this->view)){
+            return (new MailMessage())->subject($this->sub)->view(
+                $this->view, ['data' => $this->data,'model'=>$this->allData,'notifiable'=>$notifiable,'content'=>$this->getContent($this->allData['content'])]
+            );
+        }
     }
 }
