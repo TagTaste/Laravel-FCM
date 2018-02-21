f13af5aa30001c4c7a40a3c15c51bf4f34123e69
ashokTagtaste <ashok@tagtaste.com>
2018-02-21 20:25:24 +0530

    add

--- resources/views/emails/expire-job.blade.php
+++ resources/views/emails/expire-job.blade.php
@@ -105,7 +105,7 @@
                                             <tr>
                                                 <td valign="top" align="center" bgcolor="#FFFFFF">
                                                     <a href="{{ $data['master_btn_url'] }}"
-                                                       style="display: inline-block;text-decoration: none;pa;padding: 14px 24px;color: #FFFFFF;background-color: #D9222A;box-shadow: none;border: none;font-size: 16px;margin: 31px 0px 31px 0px;border-radius: 24px;font-weight: normal;">{{ $data['master_btn_text'] }}</a>
+                                                       style="display: inline-block;text-decoration: none;padding: 14px 24px;color: #FFFFFF;background-color: #D9222A;box-shadow: none;border: none;font-size: 16px;margin: 31px 0px 31px 0px;border-radius: 24px;font-weight: normal;">{{ $data['master_btn_text'] }}</a>
                                                 </td>
                                             </tr>
 
