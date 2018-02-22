822043e87d0cdb6ce916a3d0a4c3cdcfc80bcd55
ashokTagtaste <ashok@tagtaste.com>
2018-02-22 11:16:15 +0530

    add

--- resources/views/emails/serviceInterruption.blade.php
+++ resources/views/emails/serviceInterruption.blade.php
@@ -5,7 +5,7 @@
     We are pleased  to inform you that all our services have been restored to normal. We thank you for your patience and sincerely regret the inconvenience caused to you.
     <br>
     <br>
-    If you are still facing any issue, please do not hesitate to write to us at support@tagtaste.com, or call us at nhi pta mujhe.
+    If you are still facing any issue, please do not hesitate to write to us at support@tagtaste.com, or call us at +91 - 9958657225.
     <br>
     <br>
     Regards,
