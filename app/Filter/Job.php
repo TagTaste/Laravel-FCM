8199eabf6bfe6e65b7f834509c0f085400fddcee
ashokTagtaste <ashok@tagtaste.com>
2018-03-14 18:17:03 +0530

    add filter

--- app/Filter/Job.php
+++ app/Filter/Job.php
@@ -9,7 +9,7 @@ class Job extends Filter {
 
     protected $table = "job_filters";
 
-    protected $strings = ['location','expected_role','Joining'=>'joining','experience','compensation_min'=>'salary_min'
+    protected $strings = ['location','expected_role','Joining'=>'joining','experience_min','experience_max','compensation_min'=>'salary_min'
         ,'compensation_max'=>'salary_max'];
     
     protected $models = ['By Company'=>'company.name','Job Type' => 'jobType.name',
@@ -19,7 +19,7 @@ class Job extends Filter {
     
     public static $relatedColumn = 'job_id';
 
-    public static $filterOrder = ['Job Type','location','Experience','compensation_min','compensation_max'
+    public static $filterOrder = ['Job Type','location','Experience','Compensation'
         ,'By Company','By Person','Joining In'];
 
     public function getprofile_nameattribute($model)
@@ -46,13 +46,21 @@ class Job extends Filter {
             foreach($order as $key){
                 $count = 0;
                 $singleFilter = $allFilters->get($key);
-                if(!$singleFilter || $key == 'experience_min' || $key == 'experience_max' || $key == 'compensation_min' || $key == 'compensation_max')
+                if(!$singleFilter || $key == 'experience_min' || $key == 'experience_max')
+                {
+                    $filters['Experience'][0]['value'] = '0 - 2 years';
+                    $filters['Experience'][1]['value'] = '2 - 5 years';
+                    $filters['Experience'][2]['value'] = '5 - 8 years';
+                    $filters['Experience'][3]['value'] = '8 - 10 years';
+                    $filters['Experience'][4]['value'] = '> 10 years';
+                    continue;
+                }
+                if($key == 'compensation_min' || $key == 'compensation_max')
                 {
                     $filters['Experience'][0]['value'] = '0 - 3.5 LPA';
                     $filters['Experience'][1]['value'] = '3.5 - 7 LPA';
                     $filters['Experience'][2]['value'] = '7 - 15 LPA';
-                    $filters['Experience'][3]['value'] = '>15 LPA';
-                    continue;
+                    $filters['Experience'][3]['value'] = '> 15 LPA';
                 }
                 foreach($singleFilter as &$filter)
                 {
@@ -92,35 +100,74 @@ class Job extends Filter {
     public static function getModelIds(&$filters,$skip = null,$take = null)
     {
         $models = null;
-
-        foreach($filters as $filter => $value){
-            if(array_key_exists('experience_max',$filters) && array_key_exists('experience_min',$filters))
+        if(array_key_exists('Experience',$filters))
+        {
+            $experience = explode('years', $filters['Experience'][0]);
+            if($experience[0] == '>')
             {
-                $model = \DB::table('job_filters as j1')->select('j1.job_id')->JOIN('job_filters as j2','j2.job_id','=', 'j1.job_id')
-                    ->where('j2.key','experience_min')->where('j2.value','>=',$filters['experience_min'])
-                    ->where('j1.key','experience_max')->where('j1.value','<=',$filters['experience_max']);
-            }
-            else if(array_key_exists('compensation_max',$filters) && array_key_exists('compensation_min',$filters))
-            {
-                $model = \DB::table('job_filters as j1')->select('j1.job_id')->JOIN('job_filters as j2','j2.job_id','=', 'j1.job_id')
-                    ->where('j2.key','compensation_min')->where('j2.value','>=',$filters['compensation_min'])
-                    ->where('j1.key','compensation_max')->where('j1.value','<=',$filters['compensation_max']);
-            }
-            else if($filter == 'experience_max' || $filter == 'compensation_max')
-            {
-                $model = static::selectRaw('distinct ' . static::$relatedColumn)
-                    ->where('key',$filter)->where('value','<=',$value);
-            }
-            else if($filter == 'experience_min' || $filter == 'compensation_min')
-            {
-                $model = static::selectRaw('distinct ' . static::$relatedColumn)
-                    ->where('key',$filter)->where('value','>=',$value);
+                $experience = explode('> ', $experience[0]);
+                $minExperience = null;
+                $maxExperience = $experience[0];
+                \Log::info($experience[0]);
             }
             else
             {
-                $model = static::selectRaw('distinct ' . static::$relatedColumn)
-                    ->where('key',$filter)->whereIn('value',$value);
+                $experience = explode(' - ', $experience[0]);
+                $minExperience = $experience[0];
+                $maxExperience = $experience[1];
             }
+            $model = \DB::table('job_filters as j1')->select('j1.job_id')->JOIN('job_filters as j2','j2.job_id','=', 'j1.job_id')
+                    ->where(function($query) use ($minExperience){
+                $query->where('j2.key','experience_min')->where('j2.value','>=',$minExperience);;
+                })->where(function($query) use ($maxExperience){
+                    where('j1.key','experience_max')->where('j1.value','<=',$maxExperience);
+                });
+            if((null !== $skip) || (null !== $take)){
+                $model = $model->skip($skip)->take($take);
+            }
+
+            $model = $model->orderBy(static::$relatedColumn)
+                ->get()
+                ->pluck(static::$relatedColumn);
+            if(is_null($models)){
+                $models = $model;
+            }
+            else{
+                $models = $model->intersect($models);
+
+            }
+        }
+        else if(array_key_exists('Compensation',$filters))
+        {
+            $compensation = explode('LPA', $filters['Cxperience'][0]);
+            $compensation = explode(' - ', $compensation[0]);
+            $minCompensation = $compensation[0];
+            $maxCompensation = $compensation[1];
+            $model = \DB::table('job_filters as j1')->select('j1.job_id')->JOIN('job_filters as j2','j2.job_id','=', 'j1.job_id')
+                ->where(function($query) use ($minCompensation){
+                    $query->where('j2.key','compensation_min')->where('j2.value','>=',$minCompensation);;
+                })->where(function($query) use ($maxCompensation){
+                    where('j1.key','compensation_max')->where('j1.value','<=',$maxCompensation);
+                });
+
+            if((null !== $skip) || (null !== $take)){
+                $model = $model->skip($skip)->take($take);
+            }
+
+            $model = $model->orderBy(static::$relatedColumn)
+                ->get()
+                ->pluck(static::$relatedColumn);
+            if(is_null($models)){
+                $models = $model;
+            }
+            else{
+                $models = $model->intersect($models);
+
+            }
+        }
+        foreach($filters as $filter => $value){
+            $model = static::selectRaw('distinct ' . static::$relatedColumn)
+                ->where('key',$filter)->whereIn('value',$value);
 
             if((null !== $skip) || (null !== $take)){
                 $model = $model->skip($skip)->take($take);
