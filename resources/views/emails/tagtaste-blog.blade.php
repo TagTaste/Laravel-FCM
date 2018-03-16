139bafc5378eaf37615d54c91aca69f956e53501
ashokTagtaste <ashok@tagtaste.com>
2018-03-16 19:12:27 +0530

    add email

--- resources/views/emails/tagtaste-blog.blade.php
+++ resources/views/emails/tagtaste-blog.blade.php
@@ -26,7 +26,7 @@
                                                     <table border="0" cellpadding="0" cellspacing="0" width="100%" bgcolor="#FFFFFF">
                                                         <tr>
                                                             <td bgcolor="#FFFFFF">
-                                                                    <div style="font-size: 16px;color: #181818;">Networks don’t merely expand markets, news, and innovation, they revolutionize the nature of their power.</div>
+                                                                    <div style="font-size: 16px;color: #181818;">Networks don’t merely expand markets, news or innovation, they revolutionize the nature of their power.</div>
                                                             </td>
                                                         </tr>
                                                     </table>
@@ -35,90 +35,31 @@
                                         </table>
                                     </td>
                                 </tr>
-                                    <tr>
+                                <tr>
                                         <td bgcolor="#FFFFFF" align= "center">
                                             <div style="padding: 0px 40px;">
                                                 <div style="padding: 20px 0px;">
                                                     <a href="https://blog.tagtaste.com/networks-dont-merely-expand-markets-news-and-innovation-they-revolutionize-the-nature-of-their-3abb7ae72a6b">
-                                                        <img src="https://s3.ap-south-1.amazonaws.com/static3.tagtaste.com/images/blog/chris+zandee.png" width="350px"/>
+                                                        <img src="https://s3.ap-south-1.amazonaws.com/static3.tagtaste.com/images/blog/Image+of+chris5.png" width="350px"/>
                                                     </a>
                                                 </div>
                                             </div>
                                         </td>
                                     </tr>
-
-                                {{--<tr>--}}
-                                {{--<td style="width:100%!important;background-color:#FFFFFF!important;padding: 20px 40px 0px 40px;">--}}
-                                {{--<table width= "100%" align="center" border="0" cellspacing="0" cellpadding="0"  border-collapse="collapse" bgcolor="#FFFFFF">--}}
-                                {{--@if($comment)--}}
-                                {{--<tr>--}}
-                                {{--<td align="center" width="50px" class="templateColumnContainer">--}}
-                                {{--<table border="0" cellpadding="0" cellspacing="0" width="100%">--}}
-                                {{--<tr>--}}
-                                {{--<td>--}}
-                                {{--<img src="https://i1.wp.com/www.femmehub.com/wp-content/uploads/2015/04/foods-wallpaper.jpg" height="50px" width="50px" style="border-radius: 50%;"/>--}}
-                                {{--</td>--}}
-                                {{--</tr>--}}
-                                {{--</table>--}}
-                                {{--</td>--}}
-                                {{--<td align="center" valign="top" width="373px" class="templateColumnContainer" style="padding:0px 0px 0px 10px;">--}}
-                                {{--<table border="0" cellpadding="0" cellspacing="0" width="100%" bgcolor="#FAFAFA" style="border-radius:12px;" >--}}
-                                {{--<tr>--}}
-                                {{--<td style="padding: 13px 14px;">--}}
-                                {{--<div style="color: #181818;font-weight: bold;font-size: 16px;">--}}
-                                {{--Arun Tangri--}}
-                                {{--</div>--}}
-                                {{--<div style="padding-top:8px;color: #717171;">--}}
-                                {{--<!-- message -->--}}
-                                {{--we do source and market...<span style="color: #4397E7;">(more)</span>                                             --}}
-                                {{--</div>--}}
-                                {{--</td>--}}
-                                {{--</tr>--}}
-                                {{--</table>--}}
-                                {{--</td>--}}
-                                {{--</tr>--}}
-                                {{--@else--}}
-                                {{--<tr>--}}
-                                {{--<td align="center" valign="top" width="20%" class="templateColumnContainer">--}}
-                                {{--<table border="0" cellpadding="0" cellspacing="0" width="100%" >--}}
-                                {{--<tr>--}}
-                                {{--<td align="center">--}}
-                                {{--<img src="https://i1.wp.com/www.femmehub.com/wp-content/uploads/2015/04/foods-wallpaper.jpg" height="50px" width="50px" style="border-radius: 50%;"/>--}}
-                                {{--</td>--}}
-                                {{--</tr>--}}
-                                {{--<tr>--}}
-                                {{--<td align="center">--}}
-                                {{--<div style="margin:20px 0px 0px 0px;">--}}
-                                {{--<span style="color: #181818;font-size: 16px;font-weight:bold;">--}}
-                                {{--Naman--}}
-                                {{--</span>--}}
-                                {{--</div>--}}
-                                {{--</td>--}}
-                                {{--</tr>--}}
-                                {{--</table>--}}
-                                {{--</td>--}}
-                                {{--</tr>--}}
-                                {{--@endif--}}
-                                {{--</table>--}}
-                                {{--</td>--}}
-                                {{--</tr>--}}
-                                {{--<tr>--}}
-                                {{--<td style="width:100%!important;background-color:#FFFFFF!important;padding: 20px 0px 0px 0px;">--}}
-                                {{--<table width= "100%" align="center" border="0" cellspacing="0" cellpadding="0"  border-collapse="collapse" bgcolor="#FFFFFF">--}}
-                                {{--<tr>--}}
-                                {{--<td valign="top" align="center"  bgcolor="#FFFFFF">--}}
-                                {{--<button style="padding: 18px 36px;border-radius: 4px;color: #FFFFFF;background-color: #D9222A;box-shadow: none;border: none;font-size: 18px;margin: 31px 0px 31px 0px;border-radius: 30px;">VIEW ON TAGTASTE</button>--}}
-                                {{--</td>--}}
-                                {{--</tr>--}}
-                                {{----}}
-                                {{--</table>--}}
-                                {{--</td>--}}
-                                {{--</tr> --}}
+                                <tr>
+                                    <td bgcolor="#FFFFFF" align= "center">
+                                        <div style="padding: 0px 40px;">
+                                            <div style="padding: 0px 0px;font-size: 16px;">
+                                                Continue reading on
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
                                 <tr>
                                     <td  style="width:100%!important;background-color:#FFFFFF!important;">
                                         <table width= "100%" align="center" border="0" cellspacing="0" cellpadding="0"  border-collapse="collapse" bgcolor="#FFFFFF">
                                             <tr>
-                                                <td style="height:31px;background-color:#FFFFFF">
+                                                <td style="height:20px;background-color:#FFFFFF">
                                                 </td>
                                             </tr>
                                             <tr>
@@ -128,7 +69,7 @@
                                                         <tr>
                                                             <td valign="top" align="center"  bgcolor="#FFFFFF" >
 
-                                                                <a href="https://blog.tagtaste.com/networks-dont-merely-expand-markets-news-and-innovation-they-revolutionize-the-nature-of-their-3abb7ae72a6b" style="text-decoration:none;padding: 19px 35px 19px 35px;border-radius: 4px;color: #FFFFFF;background-color: #D9222A;box-shadow: none;border: none;font-size: 18px;border-radius: 30px; font-weight: normal">VIEW</a>
+                                                                <a href="https://blog.tagtaste.com/networks-dont-merely-expand-markets-news-and-innovation-they-revolutionize-the-nature-of-their-3abb7ae72a6b" style="text-decoration:none;padding: 19px 35px 19px 35px;border-radius: 4px;color: #FFFFFF;background-color: #D9222A;box-shadow: none;border: none;font-size: 18px;border-radius: 30px; font-weight: normal">TAGTASTE BLOG</a>
 
                                                             </td>
                                                         </tr>
