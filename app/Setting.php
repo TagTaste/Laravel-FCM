421e7280fd8329ad3cfb9b004bf08c0a1bb5a48f
aman95 <aman1995k@gmail.com>
2018-03-16 12:21:59 +0530

    Added logging

--- app/Setting.php
+++ app/Setting.php
@@ -42,6 +42,7 @@ class Setting
             ->where('action', $action)
             ->where('sub_action', $sub_action)->where('model', $model)
             ->first();
+        \Log::info("SETTING.PHP profile_id=$profileId action=$action setting=".print_r($setting,true));
         if(is_null($setting)) {
             return null;
         }
