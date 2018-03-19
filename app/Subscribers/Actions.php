d8ad5ae318d326c623440a97332d3c56308d16d0
aman95 <aman1995k@gmail.com>
2018-03-19 19:18:28 +0530

    add log

--- app/Subscribers/Actions.php
+++ app/Subscribers/Actions.php
@@ -27,7 +27,7 @@ class Actions
             ->where('model_subscribers.model_id','=',$modelId)
             ->where('model_subscribers.profile_id','!=',$event->who['id'])
             ->whereNull('muted_on')
-            ->whereNull('model_subscribers.deleted_at')->get();
+            ->whereNull('model_subscribers.deleted_at')->get()->pluck('id');
 
         \Log::info($profilesIds);
 
