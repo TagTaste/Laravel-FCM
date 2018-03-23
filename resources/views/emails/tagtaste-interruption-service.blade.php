9c187d79f77536b1ed5b6d10f67b8ae6ebb7b62a
ashokTagtaste <ashok@tagtaste.com>
2018-03-23 21:53:15 +0530

    change mail

--- resources/views/emails/tagtaste-interruption-service.blade.php
+++ resources/views/emails/tagtaste-interruption-service.blade.php
@@ -1,14 +1,70 @@
-Dear {{$name}},
-<br>
-<br>
+@include("emails.header.definition")
+<body style="margin: 0; padding: 0;">
+<center>
+    <table width="100%" border="0" cellspacing="0" cellpadding="0" style="font-family:Arial" bgcolor="#F8F6F9">
+        @include("emails.header.header")
+        <tr>
+            <td align="center" valign="top">
+                <table class="container" width="620" align="center" border="0" cellspacing="0" cellpadding="0"
+                       border-collapse="collapse">
+                    <tr>
+                        <td align="center" width="100%" bgcolor="#F8F6F9">
+                            <table width="550" align="center" border="0" cellspacing="0" cellpadding="0"
+                                   border-collapse="collapse">
+                                <tr>
+                                    <td align="center" bgcolor="#FFFFFF" style="padding:20px 0px 20px 0px;border-radius: 10px 10px 0 0;width:100%!important;" width="100%">
+                                        <div style="font-size: 20px;font-weight: bold;color: #181818;padding: 0 40px 0 40px">
+                                            Interruption in the services
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="background-color: #FAFAFA;height: 10px;">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="width:100%!important;background-color:#FFFFFF!important;padding: 0px 40px 0px 40px;border-bottom: 1px solid rgba(0, 0, 0, 0.04);">
+                                        <table style="margin: 20px 0px 20px 0px;" width="100%" bgcolor="#FFFFFF"
+                                               style="border-bottom: 1px solid rgba(0, 0, 0, 0.04);">
+                                            <tr>
+                                                <td align="center" valign="top" width="100%"
+                                                    class="templateColumnContainer">
+                                                    <table border="0" cellpadding="0" cellspacing="0" width="100%"
+                                                           bgcolor="#FFFFFF">
+                                                        <tr>
+                                                            <td>
+                                                                <div>
+                                                                    <p style="font-size: 14px;color: #181818;margin:0px">
+                                                                        Dear {{$name}}
 
-    Please be advised that the platform will be under maintainance from 12:00 AM to 2:00 AM IST on March 24th, 2018. This period of downtime is scheduled for necessary updates to be applied to the network servers.
+                                                                        <br>
+                                                                        <br>
+                                                                        Please be advised that the platform will be under maintainance from 12:00 AM to 2:00 AM IST on March 24th, 2018. This period of downtime is scheduled for necessary updates to be applied to the network servers.
+                                                                        We apologise for the inconvenience that this may cause.
 
-            We apologise for the inconvenience that this may cause.
-
-
-<br>
-<br>
-Kind regards,
-<br>
-Team TagTaste
+                                                                        <br>
+                                                                        <br>
+                                                                        Regards,
+                                                                        <br>
+                                                                        Team TagTaste
+                                                                    </p>
+                                                                </div>
+                                                            </td>
+                                                        </tr>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                                @include("emails.footer.download")
+                                @include("emails.footer.footer")
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</center>
+</body>
