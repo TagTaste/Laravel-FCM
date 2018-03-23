6429a59deb6e6936313203cefaf9df2bd73b38f4
ashokTagtaste <ashok@tagtaste.com>
2018-03-23 21:27:05 +0530

    Add email for service intrruption

---
+++ resources/views/emails/tagtaste-interruption-service.blade.php
@@ -0,0 +1,14 @@
+Dear {{$name}},
+<br>
+<br>
+
+    Please be advised that the platform will be under maintainance from 10:00 PM to 11:59 AM IST on March 23th, 2018. This period of downtime is scheduled for necessary updates to be applied to the network servers.
+
+            We apologise for the inconvenience that this may cause.
+
+
+<br>
+<br>
+Kind regards,
+<br>
+Team TagTaste
