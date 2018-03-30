28a7793fc6ec34ca17f01a005e41f7b5c6ac7865
aman95 <aman1995k@gmail.com>
2018-03-30 15:58:47 +0530

    Added condition for null profile

--- app/Notifications/Chat/NewMessage.php
+++ app/Notifications/Chat/NewMessage.php
@@ -47,7 +47,7 @@ class NewMessage extends Notification
     {
         $data = [
             'action' => 'chat',
-            'profile' => $this->data->profile
+            'profile' => isset(request()->user()->profile) ? request()->user()->profile : \App\Recipe\Profile::find($this->data->profile->id)->toArray(),
         ];
         $chat = \DB::table('chats')->where('id',$this->data->chatId)->first();
         $data['model'] = [
