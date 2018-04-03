f8f9f62a4455f7054eb7bef09066b9aaef2901c9
ashokTagtaste <ashok@tagtaste.com>
2018-04-03 19:24:34 +0530

    change comment section

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -188,8 +188,8 @@ class CommentController extends Controller {
         $page = $previousPage + 1;
         $this->model['previous_page'] = $previousPage == 0 ? null : $previousPage;
         $nextPage = intval($model->comments()->where('id','<',$id)->count()/10) +1;
-        $this->model['next_page'] = $nextPage == $previousPage || count($this->model['data']) < 10 ? null : $previousPage + 2;
         $this->model['data'] = $model->comments()->orderBy('created_at','desc')->skip($previousPage*10)->take(10)->get();
+        $this->model['next_page'] = $nextPage == $previousPage || count($this->model['data']) < 10 ? null : $previousPage + 2;
         return $this->sendResponse();
     }
 
