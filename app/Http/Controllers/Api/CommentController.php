4b67339a35eb45d6759d261ba8794f79123c0149
ashokTagtaste <ashok@tagtaste.com>
2018-04-04 15:53:45 +0530

    add int type

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -62,7 +62,7 @@ class CommentController extends Controller {
         $model = $this->getModel($model, $modelId);
         
         $this->checkRelationship($model);
-        $page = $request->input('page') ? $request->input('page') : 0;
+        $page = $request->input('page') ? intval($request->input('page')) : 0;
         $this->model['data'] = $model->comments()->orderBy('created_at','desc')->skip(($page - 1) * 10)->take(10)->get();
         $this->model['next_page'] = $page > 1 ? $page - 1 : null;
         $this->model['previous_page'] = count($this->model['data']) >= 10 ? $page + 1 : null;
