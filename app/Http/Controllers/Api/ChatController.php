d642909b970a2e47549921c86747e123d60cf456
ashokTagtaste <ashok@tagtaste.com>
2018-04-06 15:55:14 +0530

    Add api for chat

--- app/Http/Controllers/Api/ChatController.php
+++ app/Http/Controllers/Api/ChatController.php
@@ -212,5 +212,61 @@ class ChatController extends Controller
         return $this->sendResponse();
 
     }
+
+    public function chatMessage(Request $request)
+    {
+        $inputs = $request->except(['_method','_token','isSingle']);
+
+        //set profile_id to logged in user automatically.
+        //all profileIds passed in request would be added to Chat\Member;
+        $profileIds = $inputs['profile_id'];
+        if(!is_array($profileIds)){
+            $profileIds = [$profileIds];
+        }
+        $user = $request->user();
+        $loggedInProfileId = $user->profile->id;
+        $inputs['profile_id'] = $loggedInProfileId;
+        //check for existing chats only for single profileId.
+        if(is_array($profileIds) && count($profileIds) === 1 && $request->input('isSingle') == 1){
+            $existingChats = Chat::open($profileIds[0],$loggedInProfileId);
+            \Log::info($existingChats);
+            if(!is_null($existingChats) && $existingChats->count() > 0){
+                $this->messages[] = "chat_open";
+                $this->model = $existingChats;
+                return $this->sendResponse();
+            }
+
+            if(!\Redis::sIsMember("followers:profile:".$loggedInProfileId,$profileIds[0]))
+            {
+                if(!\App\ChatLimit::checkLimit($loggedInProfileId,$profileIds[0])){
+                    return $this->sendError("max_limit_reached");
+                }
+            }
+        }
+
+        $this->model = \App\Chat::create($inputs);
+        if($request->hasFile("image")){
+            $imageName = str_random("32") . ".jpg";
+            $path = Chat::getImagePath($this->model->id);
+            $response = $request->file('image')->storeAs($path,$imageName,['visibility'=>'public']);
+            if(!$response){
+                throw new \Exception("Could not save image " . $imageName . " at " . $path);
+            }
+            $this->model->update(['image'=>$response]);
+        }
+
+        //add members to the chat
+        $now = \Carbon\Carbon::now()->toDateTimeString();
+        $data = [];
+        $chatId = $this->model->id;
+        //for add login profile id in member model
+        $data[] = ['chat_id'=>$chatId,'profile_id'=>$loggedInProfileId, 'created_at'=>$now,'updated_at'=>$now,'is_admin'=>1,'is_single'=>$request->input('isSingle')];
+        foreach($profileIds as $profileId){
+            $data[] = ['chat_id'=>$chatId,'profile_id'=>$profileId, 'created_at'=>$now,'updated_at'=>$now,'is_admin'=>0,'is_single'=>$request->input('isSingle')];
+        }
+        $this->model->members()->insert($data);
+
+        return $this->sendResponse();
+    }
  
 }
--- routes/api.php
+++ routes/api.php
@@ -93,6 +93,7 @@ Route::group(['namespace'=>'Api', 'as' => 'api.' //note the dot.
                 Route::post("chats/{chatId}/messages/{id}/markRead",'Chat\\MessageController@markRead');
                 Route::resource("chats/{chatId}/members",'Chat\\MemberController');
                 Route::resource("chats/{chatId}/messages",'Chat\\MessageController');
+                Route::post('chatMessage','ChatController');
                 Route::resource("chats","ChatController");
     
             //product categories
