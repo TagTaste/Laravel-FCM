89d0458f955508628aa2588ef853de4e630374e4
ashokTagtaste <ashok@tagtaste.com>
2018-04-23 12:29:53 +0530

    find deleted profile

--- app/Console/Commands/ProfileDelete.php
+++ app/Console/Commands/ProfileDelete.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Subscriber;
 use Illuminate\Console\Command;
 
 class ProfileDelete extends Command
@@ -11,7 +12,7 @@ class ProfileDelete extends Command
      *
      * @var string
      */
-    protected $signature = 'profile:delete {profileId}';
+    protected $signature = 'profile:delete';
     
     private $profileId;
 
@@ -39,16 +40,16 @@ class ProfileDelete extends Command
      */
     public function handle()
     {
-        $profileId = $this->argument('profileId');
-        
-        if(!$this->confirm("Delete profile $profileId?")){
-            $this->info("NOT deleting.");
-            return;
-        }
-        
-        $this->info("deleting profile $profileId");
-        $this->profileId = $profileId;
-        $this->delete();
+//        $profileId = $this->argument('profileId');
+//
+//        if(!$this->confirm("Delete profile $profileId?")){
+//            $this->info("NOT deleting.");
+//            return;
+//        }
+//
+//        $this->info("deleting profile $profileId");
+//        $this->profileId = $profileId;
+//        $this->delete();
     }
     
     private function delete()
@@ -84,20 +85,41 @@ class ProfileDelete extends Command
     
     private function deleteModel()
     {
-        $this->info("Deleting model " . $this->profileId);
-        $profile = \App\Profile::where('id',$this->profileId)->first();
-        if(!$profile){
-            echo "Could not find profile.";
-        }
-    
-        if($this->confirm("Delete " . $profile->id . "?")){
-            \DB::table("social_accounts")->where('user_id',$profile->user_id)->delete();
-            
-            if($profile->user){
-                $profile->user->delete();
-            }
-            
-            $profile->delete();
-        }
+//        $this->info("Deleting model " . $this->profileId);
+//        $profile = \App\Profile::where('id',$this->profileId)->first();
+//        if(!$profile){
+//            echo "Could not find profile.";
+//        }
+//
+//        if($this->confirm("Delete " . $profile->id . "?")){
+//            \DB::table("social_accounts")->where('user_id',$profile->user_id)->delete();
+//
+//            if($profile->user){
+//                $profile->user->delete();
+//            }
+//
+//            $profile->delete();
+//        }
+
+        Subscriber::join("profiles",'profiles.id','=','subscribers.profile_id')
+            ->whereNull('profiles.deleted_at')
+            ->whereNull('subscribers.deleted_at')->chunk(200,function($subscribers){
+                echo "************* Count " . $subscribers->count() . "\n\n\n\n\n";
+                foreach($subscribers as $model){
+
+                    $channel = explode(".",$model->channel_name);
+                    $channelOwnerProfileId = last($channel);
+                    echo "profile id which is deleted .".$channelOwnerProfileId . "\n\n";
+
+                    $channelOwnerProfileId = "profile:small:".$channelOwnerProfileId;
+                    $profile = \Redis::mget($channelOwnerProfileId);
+
+                    if(is_null($profile))
+                    {
+                        echo "profile id which is deleted .".last($channel) . "\n\n";
+                    }
+
+                }
+            });
     }
 }
