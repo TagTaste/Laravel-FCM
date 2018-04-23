f631594cd513be08627ef2bf2c1230aeb2211cb9
ashokTagtaste <ashok@tagtaste.com>
2018-04-23 19:13:32 +0530

    add check

--- app/Http/Controllers/Api/CollaborateController.php
+++ app/Http/Controllers/Api/CollaborateController.php
@@ -327,8 +327,8 @@ class CollaborateController extends Controller
 
         $page = $request->input('page');
         list($skip,$take) = \App\Strategies\Paginator::paginate($page);
-        $applications = \App\Collaboration\Collaborator::whereNull('archived_at')
-            ->where('collaborate_id',$id)->with('profile','collaborate','company');
+        $applications = \App\Collaboration\Collaborator::join('profiles','collaborators.profile_id','=','profiles.id')->whereNull('collaborators.archived_at')
+            ->whereNull('profiles.deleted_at')->where('collaborate_id',$id)->with('profile','collaborate','company');
         $this->model['count'] = $applications->count();
         $this->model['application'] = $applications->skip($skip)->take($take)->get();
         return $this->sendResponse();
