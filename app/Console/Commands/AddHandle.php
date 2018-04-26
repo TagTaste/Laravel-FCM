bae71f8fec1e024b18e7bab3dcab4313ec673e35
ashokTagtaste <ashok@tagtaste.com>
2018-04-26 12:14:35 +0530

    Add handle

---
+++ app/Console/Commands/AddHandle.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Profile;
+use App\User;
+use Illuminate\Console\Command;
+
+class AddHandle extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'addHandleOfProfile';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'add handle of profile';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Profile::whereNull('handle')->whereNull('deleted_at')->chunk(50,function($models){
+            foreach ($models as $model)
+            {
+                $name = $model->name;
+                $name = str_replace(' ', '.', $name);
+                \Log::info($name);
+                $hanleExist = Profile::where('handle',$name)->exists();
+                if($hanleExist)
+                {
+                    $name = $name.'.'.mt_rand(100,999);
+                }
+                \Log::info($name);
+                Profile::where('id',$model->id)->update(['handle'=>$name]);
+            }
+        });
+    }
+}
