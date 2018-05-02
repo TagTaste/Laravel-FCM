ea20ad3b4c04b0c46941e968cbcfd5c4af70cae6
ashokTagtaste <ashok@tagtaste.com>
2018-05-02 16:18:03 +0530

    Add response

--- routes/api.php
+++ routes/api.php
@@ -30,15 +30,13 @@ Route::post('login',function(Request $request){
 //        return response()->json(['error' => 'Please verify your email address'], 401);
 //    }
     try {
-        \Log::info("req is here");
-        \Log::info($request->all());
         // attempt to verify the credentials and create a token for the user
         if (! $token = \JWTAuth::attempt($credentials)) {
             return response()->json(['error' => 'invalid_credentials','message'=>'The username or password is incorrect.'], 401);
         }
     } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
         // something went wrong whilst attempting to encode the token
-        return response()->json(['error' => 'could_not_create_token'], 500);
+        return response()->json(['error' => 'invalid_credentials','message'=>'The username or password is incorrect.'], 401);
     }
     
     return response()->json(compact('token'));
