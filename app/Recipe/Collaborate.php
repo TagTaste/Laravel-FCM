fb1f1da8555bef619e55e9f00cf7dbe9698a7d7b
ashokTagtaste <ashok@tagtaste.com>
2018-05-14 18:04:38 +0530

    change and made collaborate short

--- app/Recipe/Collaborate.php
+++ app/Recipe/Collaborate.php
@@ -11,11 +11,10 @@ class Collaborate extends BaseCollaborate
 
     protected $visible = ['id','title', 'i_am', 'looking_for',
         'expires_on','video','location','categories',
-        'description','project_commences','images',
+        'description','project_commences',
         'duration','financials','eligibility_criteria','occassion',
         'profile_id', 'company_id','template_fields','template_id','notify','privacy_id',
-        'profile','company','created_at','deleted_at',
-        'applicationCount','file1','deliverables','start_in','state','updated_at'];
+        'profile','company','created_at','deleted_at','file1','deliverables','start_in','state','updated_at'];
 
 
 }
