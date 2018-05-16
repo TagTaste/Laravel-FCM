7df77d6dda650fa9a8664b9e651487562fb9e676
ashokTagtaste <ashok@tagtaste.com>
2018-05-16 16:47:25 +0530

    add company and profile in collab

--- app/Recipe/Collaborate.php
+++ app/Recipe/Collaborate.php
@@ -14,7 +14,7 @@ class Collaborate extends BaseCollaborate
         'description','project_commences',
         'duration','financials','eligibility_criteria','occassion',
         'profile_id', 'company_id','template_fields','template_id','notify','privacy_id',
-        'created_at','deleted_at','file1','deliverables','start_in','state','updated_at'];
+        'created_at','deleted_at','file1','deliverables','start_in','state','updated_at','profile','company'];
 
     protected $with = ['profile','company'];
 
