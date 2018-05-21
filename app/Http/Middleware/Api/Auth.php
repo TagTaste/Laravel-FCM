94776c37267f307ab3a616bf8ec29d566eea9c2c
ashokTagtaste <ashok@tagtaste.com>
2018-05-21 16:05:42 +0530

    change base upar

--- app/Http/Middleware/Api/Auth.php
+++ app/Http/Middleware/Api/Auth.php
@@ -21,7 +21,7 @@ class Auth extends GetUserFromToken
     {
         if(env('APP_ENV') === 'testing'){
             \Log::warning("Auto-generating token for " . env('APP_ENV') . " environment.");
-            $user = \App\User::find(3);
+            $user = \App\User::find(1);
             $token = \JWTAuth::fromUser($user);
             $this->auth->authenticate($token);
             
