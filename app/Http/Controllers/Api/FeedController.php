4020bb950bfab2d9e7efee69da7689f3e695b2bf
ashokTagtaste <ashok@tagtaste.com>
2018-05-22 16:13:30 +0530

    add new feed api for testing

--- app/Http/Controllers/Api/FeedController.php
+++ app/Http/Controllers/Api/FeedController.php
@@ -148,7 +148,7 @@ class FeedController extends Controller
             //Don't remove this where query.
             //Ofcourse, unless you know what you are doing.
 //            ->whereRaw(\DB::raw('channel_payloads.created_at >= subscribers.created_at'))
-            ->where('subscribers.channel_name','like','%public.%' . $profileId)
+            ->where('subscribers.channel_name','like','%public.%')
             ->orderBy('channel_payloads.created_at','desc')
             ->skip($skip)
             ->take($take)
