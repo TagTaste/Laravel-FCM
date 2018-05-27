978241bb4f490453df2aed19c9a3320f065c2075
ashokTagtaste <ashok@tagtaste.com>
2018-05-27 13:27:43 +0530

    add

--- app/Filter.php
+++ app/Filter.php
@@ -163,6 +163,9 @@ class Filter extends Model
         }
         $allFilters = $filterClass::select('key','value',\DB::raw('count(`key`) as count'))
             ->groupBy('key','value')->orderBy('count','desc')->get()->groupBy('key');
+
+        return $allFilters;
+
         $filters = [];
         //$allFilters = $allFilters->keyBy('key');
         $order = $filterClass::$filterOrder;
