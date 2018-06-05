3dcc61140c3af36d33d76d590fdf6856bb39be6f
ashokTagtaste <ashok@tagtaste.com>
2018-06-05 13:38:56 +0530

    add migration

---
+++ database/migrations/2018_06_05_131125_create_collaborate_addresses.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCollaborateAddresses extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('collaborate_addresses',function(Blueprint $table){
+            $table->unsignedInteger('collaborate_id');
+            $table->foreign("collaborate_id")->references("id")->on("collaborates");
+            $table->string('city');
+            $table->json('location')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('collaborate_addresses');
+    }
+}
