0b459d2189f90033853fad3d3ea6e3f6df13f259
shubhamtagtaste <shubham@tagtaste.com>
2018-06-13 10:39:54 +0000

    changes in location package

--- config/app.php
+++ config/app.php
@@ -197,6 +197,7 @@ return [
         LaravelFCM\FCMServiceProvider::class,
         Spatie\LaravelImageOptimizer\ImageOptimizerServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
+        \Torann\GeoIP\GeoIPServiceProvider::class,
 
     ],
 
@@ -259,6 +260,7 @@ return [
         'FCM'      => LaravelFCM\Facades\FCM::class,
         'FCMGroup' => LaravelFCM\Facades\FCMGroup::class,
         'Image' => Intervention\Image\Facades\Image::class,
+        'GeoIP' => \Torann\GeoIP\Facades\GeoIP::class,
     ],
 
 ];
