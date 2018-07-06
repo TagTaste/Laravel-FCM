0c2fb8a4783e84f0fd4e6df72f48455de3e79a0e
ashokTagtaste <ashok@tagtaste.com>
2018-07-06 15:37:01 +0530

    chnge

--- database/migrations/2018_06_11_122805_create_collaborate_batches_assign.php
+++ database/migrations/2018_06_11_122805_create_collaborate_batches_assign.php
@@ -11,23 +11,23 @@ class CreateCollaborateBatchesAssign extends Migration
      *
      * @return void
      */
-//    public function up()
-//    {
-//        Schema::create('collaborate_batches_assign',function(Blueprint $table){
-//            $table->unsignedInteger('batch_id')->nullable();
-//            $table->foreign("batch_id")->references("id")->on("collaborate_batches");
-//            $table->unsignedInteger('profile_id');
-//            $table->foreign("profile_id")->references("id")->on("profiles");
-//        });
-//    }
-//
-//    /**
-//     * Reverse the migrations.
-//     *
-//     * @return void
-//     */
-//    public function down()
-//    {
-//        Schema::drop('collaborate_batches_assign');
-//    }
+    public function up()
+    {
+        Schema::create('collaborate_batches_assign',function(Blueprint $table){
+            $table->unsignedInteger('batch_id')->nullable();
+            $table->foreign("batch_id")->references("id")->on("collaborate_batches");
+            $table->unsignedInteger('profile_id');
+            $table->foreign("profile_id")->references("id")->on("profiles");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('collaborate_batches_assign');
+    }
 }
