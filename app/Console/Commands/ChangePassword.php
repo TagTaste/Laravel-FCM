dea11a72ad132f2e0bc34e9760ad695afe811e43
ashokTagtaste <ashok@tagtaste.com>
2018-07-08 23:12:22 +0530

    change password updation

--- app/Console/Commands/ChangePassword.php
+++ app/Console/Commands/ChangePassword.php
@@ -42,18 +42,10 @@ class ChangePassword extends Command
     public function handle()
     {
         $data =[];
-        User::select('profiles.id','users.email')->join('profiles','profiles.user_id','=','users.id')
-            ->whereNotIn('users.id',[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,165,44,32,160,161,176,27,253,254,255])
-            ->chunk(100,function($users) use (&$data) {
-           
-            foreach ($users as $user)
-            {
-                $password = sprintf("%06d", random_int(1, 999999));
-                User::where('email',$user->email)->update(['password'=>bcrypt($password)]);
-                $data[] = $user->id .",". $user->email ."," . $password ."\n";
-
+        \DB::table('users')->where('updated_at','<','2018-06-26')->orderBy('id')->chunk(100, function ($models) {
+            foreach ($models as $model) {
+                echo $model->update_at." id is ".$model->id." password is .".$model->password;
             }
         });
-        Storage::disk('local')->put('file.txt', $data);
     }
 }
