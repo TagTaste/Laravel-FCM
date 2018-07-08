93c1e5d197e11e3087a6ed36a3988e8afc6933de
ashokTagtaste <ashok@tagtaste.com>
2018-07-08 23:24:17 +0530

    change

--- app/Console/Commands/ChangePassword.php
+++ app/Console/Commands/ChangePassword.php
@@ -45,7 +45,7 @@ class ChangePassword extends Command
         \DB::table('users')->where('updated_at','<','2018-06-26')->whereNotIn('id',[1,2,7,10,44,685,521,570,27,530,334])->orderBy('id')
             ->chunk(100,function ($models) {
             foreach ($models as $model) {
-                echo $model->update_at." id is ".$model->id." password is .".$model->password;
+                echo $model->updated_at." id is ".$model->id." password is .".$model->password;
             }
         });
     }
