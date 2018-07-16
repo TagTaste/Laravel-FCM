0aff3c0267770778ee765148573b3008b6751b48
ashokTagtaste <ashok@tagtaste.com>
2018-07-16 19:43:35 +0530

    change

--- database/seeds/CollaborateBatchesColorTableSeeder.php
+++ database/seeds/CollaborateBatchesColorTableSeeder.php
@@ -14,22 +14,22 @@ class ProductReviewBatchesColorTableSeeder extends Seeder
         DB::table('collaborate_batches_color')->insert([
 
             [
-                'name' => '2869B5'
+                'name' => '#2869B5'
             ],
             [
-                'name' => 'F5A623'
+                'name' => '#F5A623'
             ],
             [
-                'name' => 'F8E71C'
+                'name' => '#F8E71C'
             ],
             [
-                'name' => '8B572A'
+                'name' => '#8B572A'
             ],
             [
-                'name' => '7ED321'
+                'name' => '#7ED321'
             ],
             [
-                'name' => '417505'
+                'name' => '#417505'
             ]
 
         ]);
