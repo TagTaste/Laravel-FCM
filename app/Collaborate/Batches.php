5788f22734069edbc98ac943a90dbaeab8c2e173
ashokTagtaste <ashok@tagtaste.com>
2018-07-17 10:49:40 +0530

    add count

--- app/Collaborate/Batches.php
+++ app/Collaborate/Batches.php
@@ -13,9 +13,9 @@ class Batches extends Model {
     protected $fillable = ['name','notes','allergens','instruction','color_id','collaborate_id'];
 
     protected $visible = ['id','name','notes','allergens','instruction','color_id','collaborate_id','collaborate',
-        'current_status','collaborate_title','color'];
+        'current_status','collaborate_title','color','assignedCount','reviewedCount'];
 
-    protected $appends = ['current_status','collaborate_title','color'];
+    protected $appends = ['current_status','collaborate_title','color','reviewedCount','assignedCount'];
 
 //    protected $with = ['color'];
 
@@ -33,9 +33,20 @@ class Batches extends Model {
 
     public function getCurrentStatusAttribute()
     {
-        $currentStatus =  \DB::table('collaborate_tasting_user_review')->select('current_status')->where('collaborate_id',$this->collaborate_id)->where('batch_id',$this->id)
-                ->where('profile_id',request()->user()->profile->id)->first();
+        $currentStatus =  \DB::table('collaborate_tasting_user_review')->select('current_status')->where('collaborate_id',$this->collaborate_id)
+            ->where('batch_id',$this->id)->where('profile_id',request()->user()->profile->id)->first();
         return isset($currentStatus) ? $currentStatus->current_status : 0;
+    }
+
+    public function getReviewedCountAttribute()
+    {
+        return \DB::table('collaborate_tasting_user_review')->where('current_status',2)->where('collaborate_id',$this->collaborate_id)
+            ->where('batch_id',$this->id)->distinct('profile_id')->count();
+    }
+
+    public function getAssignedCountAttribute()
+    {
+        return \DB::table('collaborate_batches_assign')->where('batch_id',$this->id)->count();
     }
 
 }
