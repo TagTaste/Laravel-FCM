0258da1c602e38940ea8e26b07e6f3d030df4e4d
vagrant <tushar@tagtaste.com>
2018-07-18 10:02:03 +0000

    extra route removed

--- routes/api.php
+++ routes/api.php
@@ -165,7 +165,7 @@ Route::group(['namespace'=>'Api', 'as' => 'api.' //note the dot.
                 Route::get("jobs/all","JobController@all");
                 Route::get("jobs/filters", "JobController@filters");
                 Route::resource("jobs","JobController");
-                Route::post("jobs/message","ChatController@jobMessage"); 
+                //Route::post("jobs/message","ChatController@jobMessage"); 
 
             //send mail to applicants of job or collaborate
                 Route::post("{feature}/{featureId}/message","ChatController@featureMessage");    
