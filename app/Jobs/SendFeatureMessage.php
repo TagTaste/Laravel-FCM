0893069de718f9810e49d48b9076eb908a93790c
vagrant <tushar@tagtaste.com>
2018-07-23 11:28:20 +0000

    queue removed from chat

--- app/Jobs/SendFeatureMessage.php
+++ app/Jobs/SendFeatureMessage.php
@@ -8,7 +8,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
-class SendFeatureMessage implements ShouldQueue
+class SendFeatureMessage 
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
