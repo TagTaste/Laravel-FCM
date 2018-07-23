9bc9024a1d140ee0d06cf1c541489cf773eb0d62
vagrant <tushar@tagtaste.com>
2018-07-23 10:50:23 +0000

    queue removed

--- app/Mail/JobResponse.php
+++ app/Mail/JobResponse.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class JobResponse extends Mailable implements ShouldQueue
+class JobResponse extends Mailable 
 {
     use Queueable, SerializesModels;
 
