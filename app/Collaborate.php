6fa55184a966ef7d6da7f43d48eeed3fba08ee7c
ashokTagtaste <ashok@tagtaste.com>
2018-07-26 16:54:28 +0530

    change

--- app/Collaborate.php
+++ app/Collaborate.php
@@ -466,7 +466,7 @@ class Collaborate extends Model implements Feedable
                 ->where('collaborate_id',$this->id)->where('current_status',1)->get()->pluck('batch_id')->toArray();
             $batchIds = sort($batchIds);
             $completedBatchIds = sort($completedBatchIds);
-            $meta['is_completed_product_review'] = ($batchIds == $completedBatchIds) && count($completedBatchIds);
+            $meta['is_completed_product_review'] = count($completedBatchIds) ? ($batchIds == $completedBatchIds) : false;
             $meta['is_interested'] = \DB::table('collaborate_applicants')->where('collaborate_id',$this->id)->where('profile_id',request()->user()->profile->id)
                 ->where('is_invited',0)->whereNull('shortlisted_at')->whereNull('rejected_at')->exists();
             $meta['is_invitation_accepted'] = \DB::table('collaborate_applicants')->where('collaborate_id',$this->id)->where('profile_id',request()->user()->profile->id)
