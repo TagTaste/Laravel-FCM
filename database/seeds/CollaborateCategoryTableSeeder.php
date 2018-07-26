ec1b7bb3e4285f23a03bb37dcc0d71a84d45e008
ashokTagtaste <ashok@tagtaste.com>
2018-07-26 12:47:03 +0530

    change

--- database/seeds/CollaborateCategoryTableSeeder.php
+++ database/seeds/CollaborateCategoryTableSeeder.php
@@ -11,16 +11,20 @@ class CollaborateCategoryTableSeeder extends Seeder {
     {
         \App\CollaborateCategory::insert([[
             'name' => 'Solid',
-            'description' => 'Eg. Rusk, cookie, finger chips, etc.'
+            'description' => 'Eg. Rusk, cookie, finger chips, etc.',
+            'category_image'=>'https://s3.ap-south-1.amazonaws.com/static3.tagtaste.com/product-review-images/icons_category_solid.png'
         ],[
             'name' => 'Semi - Solid',
-            'description' => 'Eg. Peanut butter, jam, pickkle, etc.'
+            'description' => 'Eg. Peanut butter, jam, pickkle, etc.',
+            'category_image'=>'https://s3.ap-south-1.amazonaws.com/static3.tagtaste.com/product-review-images/icons_category_semi_solid.png'
         ],[
             'name' => 'Alcoholic Beverage',
-            'description' => 'Eg. Whiskey, beer, vodka, gin, etc.'
+            'description' => 'Eg. Whiskey, beer, vodka, gin, etc.',
+            'category_image'=>'https://s3.ap-south-1.amazonaws.com/static3.tagtaste.com/product-review-images/icons_category_alcoholic_beverage.png'
         ],[
             'name' => 'Non - Alcoholic Beverage',
-            'description' => 'Eg. Coffee, milk, tea, soft drinks, etc.'
+            'description' => 'Eg. Coffee, milk, tea, soft drinks, etc.',
+            'category_image'=>'https://s3.ap-south-1.amazonaws.com/static3.tagtaste.com/product-review-images/icons_category_non_alcoholic_beverage.png'
         ]]);
 
     }
--- database/seeds/CollaborateTypeTableSeeder.php
+++ database/seeds/CollaborateTypeTableSeeder.php
@@ -14,13 +14,16 @@ class CollaborateTypeTableSeeder extends Seeder
         DB::table('collaborate_types')->insert([
 
             [
-                'name' => 'Vegetarian'
+                'name' => 'Vegetarian',
+                'type_image'=>'https://s3.ap-south-1.amazonaws.com/static3.tagtaste.com/product-review-images/icons_type_vegetarian.png'
             ],
             [
-                'name' => 'Contain Egg'
+                'name' => 'Contain Egg',
+                'type_image'=>'https://s3.ap-south-1.amazonaws.com/static3.tagtaste.com/product-review-images/icons_type_contains_egg.png'
             ],
             [
-                'name' => 'Non - Vegetarian'
+                'name' => 'Non - Vegetarian',
+                'type_image'=>'https://s3.ap-south-1.amazonaws.com/static3.tagtaste.com/product-review-images/icons_type_non_vegetarian.png'
             ]
 
         ]);
