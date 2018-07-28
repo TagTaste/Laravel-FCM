ad99e37f970ccb65abd4b73552c8a551cd9f5ae3
ashokTagtaste <ashok@tagtaste.com>
2018-07-28 12:08:47 +0530

    add notification

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -48,6 +48,8 @@ class EventServiceProvider extends ServiceProvider
 
         'App\Events\Actions\Follow' => ['App\Listeners\Notifications\Follow'],
         'App\Events\Actions\Apply' => ['App\Listeners\Notifications\Apply'],
+        'App\Events\Actions\BeginTasting' => ['App\Listeners\Notifications\BeginTasting'],
+        'App\Events\Actions\CompleteTasting' => ['App\Listeners\Notifications\CompleteTasting'],
 
 
         'App\Events\Actions\Admin' => ['App\Listeners\Notifications\Admin'],
@@ -65,8 +67,7 @@ class EventServiceProvider extends ServiceProvider
 
 
         'App\Events\SuggestionEngineEvent' => ['App\Listeners\SuggestionEngineListener'],
-        'App\Events\FeatureMailEvent' => ['App\Listeners\FeatureMailListener'],
-
+        'App\Events\FeatureMailEvent' => ['App\Listeners\FeatureMailListener']
 
     ];
 
