289f5efa7434d4919545c7127db62375aa66a485
ashokTagtaste <ashok@tagtaste.com>
2018-07-28 15:47:41 +0530

    change

--- app/Http/Controllers/Api/Collaborate/BatchController.php
+++ app/Http/Controllers/Api/Collaborate/BatchController.php
@@ -225,8 +225,8 @@ class BatchController extends Controller
     public function userBatches(Request $request, $collaborateId)
     {
         $loggedInProfileId = $request->user()->profile->id;
-        $collaborate = \App\Recipe\Collaborate::where('id',$collaborateId)->get()->toArray();
-        $batchIds = \DB::table('collaborate_batches_assign')->where('collaborate_id',$collaborateId)->where('profile_id',$loggedInProfileId)->first()->pluck('batch_id');
+        $collaborate = \App\Recipe\Collaborate::where('id',$collaborateId)->first()->toArray();
+        $batchIds = \DB::table('collaborate_batches_assign')->where('collaborate_id',$collaborateId)->where('profile_id',$loggedInProfileId)->get()->pluck('batch_id');
         $collaborate['batches'] = Collaborate\Batches::where('collaborate_id',$collaborateId)->whereIn('id',$batchIds)->get();
         $this->model = $collaborate;
         return $this->sendResponse();
