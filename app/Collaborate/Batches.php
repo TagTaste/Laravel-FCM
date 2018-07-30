051bd16f30e989744ec5f94e4346eaf0a59565fc
ashokTagtaste <ashok@tagtaste.com>
2018-07-30 17:42:29 +0530

    change

--- app/Collaborate/Batches.php
+++ app/Collaborate/Batches.php
@@ -33,8 +33,6 @@ class Batches extends Model {
 
         if(isset($currentStatus))
         {
-            \Log::info("current_status".$currentStatus->current_status);
-
             return $currentStatus->current_status;
         }
         else
