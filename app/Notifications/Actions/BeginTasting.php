bf4b27f5284bd1d0e06bdb471c604cf57e1ada08
ashokTagtaste <ashok@tagtaste.com>
2018-07-31 15:52:53 +0530

    change

--- app/Notifications/Actions/BeginTasting.php
+++ app/Notifications/Actions/BeginTasting.php
@@ -44,9 +44,34 @@ class BeginTasting extends Action
         if(view()->exists($this->view)){
             return (new MailMessage())->subject($this->sub)->view(
                 $this->view, ['data' => $this->data,'model'=>$this->allData,'notifiable'=>$notifiable,
-                    'content'=>$this->getContent($this->allData['content']),'batchInfo'=>$this->batchInfo]
+                    'content'=>$this->getContent($this->allData['content'])]
             );
         }
+    }
+
+    public function toArray($notifiable)
+    {
+        $data = [
+            'action' => $this->data->action,
+            'profile' => isset(request()->user()->profile) ? request()->user()->profile : $this->data->who,
+            'notification' => $this->notification
+        ];
+
+        if(method_exists($this->model,'getNotificationContent')){
+            $data['model'] = $this->allData;
+        } else {
+            \Log::warning(class_basename($this->modelName) . " doesn't specify notification content.");
+            $data['model'] = [
+                'name' => $this->modelName,
+                'id' => $this->data->model->id,
+                'content' => $this->data->content,
+                'image' => $this->data->image
+            ];
+        }
+
+        $data['created_at'] = Carbon::now()->toDateTimeString();
+
+        return $data;
     }
 
 }
