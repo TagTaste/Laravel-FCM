b79737ae340d17cb07c25e9998af83e4fae424f4
ashokTagtaste <ashok@tagtaste.com>
2018-08-01 10:28:53 +0530

    change every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -166,7 +166,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('expires_on:job')->dailyAt('12:00');
         $schedule->command('expires_on:collaboration')->dailyAt('12:00');
-        $schedule->command('backup:db')->withoutOverlapping(10)->dailyAt('00:00');
+        $schedule->command('backup:db')->withoutOverlapping()->everyFiveMinutes();
 
         //command for redis store suggestion
         $schedule->command("build:suggestion:collaborate")->dailyAt('00:05');
