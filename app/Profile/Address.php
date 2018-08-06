2264f58841d7daa51a3cead751be5d17c6a8fdc7
vagrant <tushar@tagtaste.com>
2018-08-06 10:42:53 +0000

    id added to visible in address

--- app/Profile/Address.php
+++ app/Profile/Address.php
@@ -9,7 +9,7 @@ class Address extends Model
     //
     protected $fillable = ['pincode','city','state','address1','country','label','profile_id','landmark','locality','house_no'];
 
-    protected $visible = ['pincode','city','state','address1','country','label','profile_id','landmark','locality','house_no'];
+    protected $visible = ['id','pincode','city','state','address1','country','label','profile_id','landmark','locality','house_no'];
 
 
 
