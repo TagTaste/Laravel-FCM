a0e17ce4f4fd0863e662df09a75fb8c92c1a9884
ashokTagtaste <ashok@tagtaste.com>
2018-08-08 17:47:40 +0530

    change

--- app/Http/Controllers/Api/ChatController.php
+++ app/Http/Controllers/Api/ChatController.php
@@ -345,7 +345,7 @@ class ChatController extends Controller
             return $this->sendError("Invalid model name or Id");
         }
         $inputs = $request->except(['_method','_token']);
-        $profileIds = $inputs['profile_id'];
+        $profileIds = isset($inputs['profile_id']) ? $inputs['profile_id'] : [];
         if(!is_array($profileIds))
         {
             $profileIds = [$profileIds];
@@ -364,13 +364,6 @@ class ChatController extends Controller
         else if($model->profile_id != $loggedInProfileId){
             return $this->sendError("Invalid Collaboration Project.");
         }
-        $data['userInfo'] = \DB::table('users')->leftjoin('profiles','users.id','=','profiles.user_id')->whereIn('profiles.id',$profileIds)->get();
-        $data['message'] = $inputs['message'];
-        $data['username'] = $LoggedInUser->name;
-        $data['sender_info'] = $LoggedInUser;
-        $data['model_title'] = $model->title;
-        $data['model_name'] = $feature;
-        $data['model_id'] = $model->id;
         if($request->has('batch_id'))
         {
             $profileIds = \DB::table('collaborate_batches_assign')->where('batch_id',$request->input('batch_id'))
@@ -386,6 +379,13 @@ class ChatController extends Controller
             $profileIds = \DB::table('collaborate_batches_assign')->where('collaborate_id',$featureId)->whereNotNull('rejected_at')
                 ->get()->pluck('profile_id')->unique();
         }
+        $data['userInfo'] = \DB::table('users')->leftjoin('profiles','users.id','=','profiles.user_id')->whereIn('profiles.id',$profileIds)->get();
+        $data['message'] = $inputs['message'];
+        $data['username'] = $LoggedInUser->name;
+        $data['sender_info'] = $LoggedInUser;
+        $data['model_title'] = $model->title;
+        $data['model_name'] = $feature;
+        $data['model_id'] = $model->id;
         event(new \App\Events\FeatureMailEvent($data,$profileIds,$inputs));
         $this->model = true;
         return $this->sendResponse();
