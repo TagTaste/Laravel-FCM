98746348b6823e10ec003f9852e213cce7c29322
ashokTagtaste <ashok@tagtaste.com>
2018-08-10 15:06:41 +0530

    change

--- app/Http/Controllers/Api/Collaborate/BatchController.php
+++ app/Http/Controllers/Api/Collaborate/BatchController.php
@@ -138,6 +138,7 @@ class BatchController extends Controller
 
         $applierProfileIds = $request->input('profile_id');
         $checkUserShortlist = Collaborate\Applicant::whereIn('profile_id',$applierProfileIds)->where('is_invited',1)->whereNull('shortlisted_at')->exists();
+        \Log::info($checkUserShortlist);
         if($checkUserShortlist)
         {
             return $this->sendError("User is not accepted invitations.");
