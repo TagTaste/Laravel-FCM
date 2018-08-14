a406a333c80fb2eeac86788c7b0acaed6399062f
ashokTagtaste <ashok@tagtaste.com>
2018-08-14 15:03:55 +0530

    send array

--- app/Http/Controllers/Api/Collaborate/ReviewController.php
+++ app/Http/Controllers/Api/Collaborate/ReviewController.php
@@ -26,10 +26,6 @@ class ReviewController extends Controller
     {
         $data = [];
         $answers = $request->input('answer');
-        if(count($answers) == 0)
-        {
-            return $this->sendError("Please select valid option.");
-        }
         $loggedInProfileId = $request->user()->profile->id ;
         $batchId = $request->input('batch_id');
         if(!$request->has('batch_id'))
@@ -47,26 +43,29 @@ class ReviewController extends Controller
         Review::where('profile_id',$loggedInProfileId)->where('collaborate_id',$collaborateId)
             ->where('batch_id',$batchId)->where('tasting_header_id',$headerId)->delete();
 
-        foreach ($answers as $answer)
+        if(count($answers))
         {
-            $options = isset($answer['option']) ? $answer['option'] : [];
-            $questionId = $answer['question_id'];
-            foreach ($options as $option)
+            foreach ($answers as $answer)
             {
-                $leafId = isset($option['id']) && $option['id'] != 0 ? $option['id'] : null;
-                $valueId = isset($option['value_id']) && $option['value_id'] != 0 ? $option['id'] : null;
-                $intensity = isset($option['intensity']) && !is_null($option['intensity']) && !empty($option['intensity']) ? $option['intensity'] : null;
-                $data[] = ['key'=>null,'value'=>$option['value'],'leaf_id'=>$leafId,
-                    'question_id'=>$questionId,'tasting_header_id'=>$headerId,
-                    'profile_id'=>$loggedInProfileId,'batch_id'=>$batchId,
-                    'collaborate_id'=>$collaborateId,'intensity'=>$intensity,'current_status'=>$currentStatus,'value_id'=>$valueId];
-            }
-            if(isset($answer['comment']) && !is_null($answer['comment']))
-            {
-                $data[] = ['key'=>"comment",'value'=>$answer['comment'],'leaf_id'=>0,
-                    'question_id'=>$questionId,'tasting_header_id'=>$headerId,
-                    'profile_id'=>$loggedInProfileId,'batch_id'=>$batchId,
-                    'collaborate_id'=>$collaborateId,'intensity'=>null,'current_status'=>$currentStatus,'value_id'=>null];
+                $options = isset($answer['option']) ? $answer['option'] : [];
+                $questionId = $answer['question_id'];
+                foreach ($options as $option)
+                {
+                    $leafId = isset($option['id']) && $option['id'] != 0 ? $option['id'] : null;
+                    $valueId = isset($option['value_id']) && $option['value_id'] != 0 ? $option['id'] : null;
+                    $intensity = isset($option['intensity']) && !is_null($option['intensity']) && !empty($option['intensity']) ? $option['intensity'] : null;
+                    $data[] = ['key'=>null,'value'=>$option['value'],'leaf_id'=>$leafId,
+                        'question_id'=>$questionId,'tasting_header_id'=>$headerId,
+                        'profile_id'=>$loggedInProfileId,'batch_id'=>$batchId,
+                        'collaborate_id'=>$collaborateId,'intensity'=>$intensity,'current_status'=>$currentStatus,'value_id'=>$valueId];
+                }
+                if(isset($answer['comment']) && !is_null($answer['comment']))
+                {
+                    $data[] = ['key'=>"comment",'value'=>$answer['comment'],'leaf_id'=>0,
+                        'question_id'=>$questionId,'tasting_header_id'=>$headerId,
+                        'profile_id'=>$loggedInProfileId,'batch_id'=>$batchId,
+                        'collaborate_id'=>$collaborateId,'intensity'=>null,'current_status'=>$currentStatus,'value_id'=>null];
+                }
             }
         }
         if(count($data)>0)
