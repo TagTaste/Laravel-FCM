4691bafbeb2af88b1c199810112075436cf1cb41
ashokTagtaste <ashok@tagtaste.com>
2018-08-22 18:57:38 +0530

    change

--- app/Http/Controllers/Api/Collaborate/BatchController.php
+++ app/Http/Controllers/Api/Collaborate/BatchController.php
@@ -377,7 +377,7 @@ class BatchController extends Controller
                             ->where('batch_id',$batchId)->where('question_id',$item->id)->distinct()->get(['profile_id'])->count();
                         $subReports['answer'] = \DB::table('collaborate_tasting_user_review')->select('value','intensity',\DB::raw('count(*) as total'))->where('current_status',3)
                             ->where('collaborate_id',$collaborateId)->where('batch_id',$batchId)->where('question_id',$item->id)
-                            ->orderBy('question_id')->groupBy('question_id','value','leaf_id','intensity')->get();
+                            ->orderBy('question_id')->groupBy('question_id','value','leaf_id')->get();
                         $reports['nestedAnswers'][] = $subReports;
                     }
                 }
@@ -386,7 +386,7 @@ class BatchController extends Controller
                     ->where('batch_id',$batchId)->where('question_id',$data->id)->distinct()->get(['profile_id'])->count();
                 $reports['answer'] = \DB::table('collaborate_tasting_user_review')->select('leaf_id','value','intensity',\DB::raw('count(*) as total'))->where('current_status',3)
                     ->where('collaborate_id',$collaborateId)->where('batch_id',$batchId)->where('question_id',$data->id)
-                    ->orderBy('question_id')->groupBy('question_id','value','leaf_id','intensity')->get();
+                    ->orderBy('question_id')->groupBy('question_id','value','leaf_id')->get();
 
                 if(isset($data->questions->nested_option))
                 {
