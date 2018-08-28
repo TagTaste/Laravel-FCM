ea692033b1aa0cf7ee3824ca765b50ea28bbb788
ashokTagtaste <ashok@tagtaste.com>
2018-08-28 15:19:24 +0530

    change all api

--- app/Collaborate/Job.php
+++ app/Collaborate/Job.php
@@ -11,6 +11,21 @@ class Job extends Model {
 
     protected $fillable = ['collaborate_id','job_id'];
 
-    protected $visible = ['collaborate_id','job_id'];
+    protected $visible = ['name','description'];
 
+    protected $appends = ['name','description'];
+
+    protected $job = null;
+
+    public function getNameAttribute()
+    {
+        $this->job = \DB::table('profiles_job')->where('id',$this->job_id)->first();
+
+        return isset($this->job->name) ? $this->job->name : null;
+    }
+
+    public function getDescriptionAttribute()
+    {
+        return isset($this->job->description) ? $this->job->description : null;
+    }
 }
--- app/Collaborate/Specialization.php
+++ app/Collaborate/Specialization.php
@@ -11,6 +11,22 @@ class Specialization extends Model {
 
     protected $fillable = ['collaborate_id','specialization_id'];
 
-    protected $visible = ['collaborate_id','specialization_id'];
+    protected $visible = ['name','description'];
+
+    protected $appends = ['name','description'];
+
+    protected $spcialzation = null;
+
+    public function getNameAttribute()
+    {
+        $this->spcialzation = \DB::table('profiles_specialization')->where('id',$this->specialization_id)->first();
+
+        return isset($this->spcialzation->name) ? $this->spcialzation->name : null;
+    }
+
+    public function getDescriptionAttribute()
+    {
+        return isset($this->spcialzation->description) ? $this->spcialzation->description : null;
+    }
 
 }
