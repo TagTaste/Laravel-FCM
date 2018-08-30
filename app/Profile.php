d09cf03225d4dcc65be3076e49c15469130a2389
ashokTagtaste <ashok@tagtaste.com>
2018-08-30 18:25:05 +0530

    add phone verification

--- app/Profile.php
+++ app/Profile.php
@@ -728,7 +728,22 @@ class Profile extends Model
 
     public function getAddressAttribute($value)
     {
-        return $value;
+        if (!empty($value)) {
+            if(request()->user()->profile->id == $this->id)
+            {
+                return $value;
+            }
+
+            if($this->address_private == 3)
+            {
+                return null;
+            }
+            if(!\Redis::sIsMember("followers:profile:".request()->user()->profile->id,$this->id) && $this->address_private == 2)
+            {
+                return null;
+            }
+            return $value;
+        }
     }
 
     public function getPhoneAttribute($value)
