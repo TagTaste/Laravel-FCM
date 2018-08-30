84d40c5b8ae8bc8b4e4aa0ee6364bc2e317fe41a
ashokTagtaste <ashok@tagtaste.com>
2018-08-30 13:50:59 +0530

    change

--- database/seeds/CollaborateTastingMethodologyTableSeeder.php
+++ database/seeds/CollaborateTastingMethodologyTableSeeder.php
@@ -12,8 +12,8 @@ class CollaborateTastingMethodologyTableSeeder extends Seeder
     public function run()
     {
         $data = [];
-        $data[] = ['name'=>'Monadic Sequence (Blind)','description'=>'All products will be provided sequentially to the tasters without revealing their brand & related information.'];
-        $data[] = ['name'=>'Monadic Sequence (Apparent)','description'=>'All products will be provided sequentially to the tasters along with their brand & related information.'];
+        $data[] = ['name'=>'Monadic Sequence - Blind','description'=>'All products will be provided sequentially to the tasters without revealing their brand & related information.'];
+        $data[] = ['name'=>'Monadic Sequence - Apparent','description'=>'All products will be provided sequentially to the tasters along with their brand & related information.'];
 
         \DB::table('collaborate_tasting_methodology')->insert($data);
     }
