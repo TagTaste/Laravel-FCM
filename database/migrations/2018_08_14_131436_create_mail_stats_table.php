119f674086dd455df5c0d632f11737ec86bdd180
ashokTagtaste <ashok@tagtaste.com>
2018-08-31 19:26:55 +0530

    change

--- database/migrations/2018_08_14_131436_create_mail_stats_table.php
+++ database/migrations/2018_08_14_131436_create_mail_stats_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMailAnalysisTable extends Migration
+class CreateMailStatsTable extends Migration
 {
     /**
      * Run the migrations.
