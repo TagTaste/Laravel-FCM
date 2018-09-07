bd37fa63d25feffd31b025ac3204a6eb0946f06c
ashokTagtaste <ashok@tagtaste.com>
2018-09-07 18:45:44 +0530

    change

--- app/Http/Middleware/Api/Auth.php
+++ app/Http/Middleware/Api/Auth.php
@@ -60,10 +60,12 @@ class Auth extends GetUserFromToken
         });
 
         $response = $next($request);
-        $this->recordData($request, $response);
+
         $response->headers->set('Access-Control-Allow-Origin' , '*');
         $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application');
+
+        $this->recordData($request, $response);
         return $response;
     }
     
