bb823dcba0577fa01448856ba0f7dcaffb35c63a
vagrant <tushar@tagtaste.com>
2018-09-17 13:21:05 +0000

    changes new route

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -12,10 +12,7 @@ class SettingController extends Controller
     {
         $hash = $request->input('k');
         $decryptedString = Crypt::decryptString($hash);
-        if (filter_var($decryptedString, FILTER_VALIDATE_EMAIL)) {
-            \DB::table('newsletters')->where('email',$decryptedString)->update(['is_unsubscribed'=>1]);
-            return redirect(env('APP_URL')."/unsubscribed/?k=".$hash);
-        }
+    
         $info = explode("/",$decryptedString);
         for ($i=0; $i <5 ; $i++) { 
             if($info[$i]==="0")
@@ -47,6 +44,26 @@ class SettingController extends Controller
         }
         
         
+    }
+
+    public function reasonUnsubscribe(Request $request)
+    {
+        $hash = $request->input('k');
+        $decryptedString = Crypt::decryptString($hash);
+    
+        $info = explode("/",$decryptedString);
+        for ($i=0; $i <5 ; $i++) { 
+            if($info[$i]==="0")
+            $info[$i]=NULL;
+        }
+
+        $reason_id = $request->input('reason_id');
+            if(is_null($reason_id) || !isset($reason_id))
+            {
+                return $this->sendError("Reason should be selected");
+            }
+            $model = \DB::table('profile_unsubscribe_reasons')->insert(['reason_id'=>$reason_id, 'profile_id'=>$info[0], 'company_id'=>$info[1], 'action'=>$info[2], 'model'=>$info[3]]);
+            return $this->model;
     }
 
 
