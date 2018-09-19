4a9588f538bf9e234cc3fdeddfc3eb82e5a92f92
vagrant <tushar@tagtaste.com>
2018-09-19 10:37:09 +0000

    changes in reason seeder

--- database/seeds/UnsubscribeReasonSeeder.php
+++ database/seeds/UnsubscribeReasonSeeder.php
@@ -13,9 +13,11 @@ class UnsubscribeReasonSeeder extends Seeder
     {
         //
         $data = [];
-        $data[] = ['reason'=>'Your emails are not relevant to me.'];
+        $data[] = ['reason'=>'Your emails are not relevant to me'];
         $data[] = ['reason'=>'Your emails are too frequent'];
+        $data[] = ['reason'=>'I don\'t\remember signing up for this'];
         $data[] = ['reason'=>'I no longer want to recieve these emails'];
+        $data[] = ['reason'=>'The emails are spam and should be reported']; 
         
 
         \DB::table('unsubscribe_reasons')->insert($data);
