0b3813cb5376cf4368f0312f12715e03c3e69f0a
ashokTagtaste <ashok@tagtaste.com>
2018-09-25 19:06:56 +0530

    Change

--- app/Http/Controllers/Api/Collaborate/BatchController.php
+++ app/Http/Controllers/Api/Collaborate/BatchController.php
@@ -874,9 +874,9 @@ class BatchController extends Controller
         }
 
         $questionIds = Collaborate\Questions::select('id')->where('collaborate_id',$collaborateId)->where('questions->select_type',5)->get()->pluck('id');
-
+        \Log::info($questionIds);
         $overAllPreference = \DB::table('collaborate_tasting_user_review')->select('leaf_id','value',\DB::raw('count(*) as total'))->where('current_status',3)
-            ->where('collaborate_id',$collaborateId)->whereIn('profile_id',$profileIds)->whereIn('question_id',$questionIds)
+            ->where('collaborate_id',$collaborateId)->whereIn('question_id',$questionIds)
             ->orderBy('leaf_id')->groupBy('question_id','value','leaf_id')->get();
 
         return $overAllPreference;
