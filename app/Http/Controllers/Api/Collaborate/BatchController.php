5f33f679e6666822e6cbac0bf9500aaa65bf0bca
ashokTagtaste <ashok@tagtaste.com>
2018-09-25 19:06:05 +0530

    Change

--- app/Http/Controllers/Api/Collaborate/BatchController.php
+++ app/Http/Controllers/Api/Collaborate/BatchController.php
@@ -873,12 +873,13 @@ class BatchController extends Controller
             $profileIds = $profileIds->forget($filters['profile_id']);
         }
 
-        $questions = Collaborate\Questions::select('id')->where('collaborate_id',$collaborateId)->where('questions->select_type',5)->get()->pluck('id');
-        return $questions;
+        $questionIds = Collaborate\Questions::select('id')->where('collaborate_id',$collaborateId)->where('questions->select_type',5)->get()->pluck('id');
 
-        $overAllPreference = \DB::table('collaborate_tasting_user_review')->select('leaf_id','value','intensity',\DB::raw('count(*) as total'))->where('current_status',3)
-            ->where('collaborate_id',$collaborateId)->whereIn('profile_id',$profileIds)->where('select',1)
-            ->orderBy('question_id')->groupBy('question_id','value','leaf_id','intensity')->get();
+        $overAllPreference = \DB::table('collaborate_tasting_user_review')->select('leaf_id','value',\DB::raw('count(*) as total'))->where('current_status',3)
+            ->where('collaborate_id',$collaborateId)->whereIn('profile_id',$profileIds)->whereIn('question_id',$questionIds)
+            ->orderBy('leaf_id')->groupBy('question_id','value','leaf_id')->get();
+
+        return $overAllPreference;
 
     }
 }
