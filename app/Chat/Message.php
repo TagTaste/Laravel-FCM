c1e4fec7cb7d2d84b329fb64db22973e01bbe298
ashokTagtaste <ashok@tagtaste.com>
2018-09-26 19:19:36 +0530

    change

--- app/Chat/Message.php
+++ app/Chat/Message.php
@@ -54,7 +54,7 @@ class Message extends Model
             {
                 $preview['image'] = is_null($preview['image']) ? null : \Storage::url($preview['image']);
             }
-            return is_object($preview) ? json_encode($preview) : $preview;
+            return is_object($preview) ? (string)json_encode($preview) : $preview;
 
         } catch(\Exception $e){
             \Log::error("Could not load preview image");
@@ -62,7 +62,7 @@ class Message extends Model
             \Log::error($e->getLine());
             \Log::error($e->getMessage());
         }
-        return empty($preview) ? null : is_object($preview) ? json_encode($preview) : $preview;
+        return empty($preview) ? null : is_object($preview) ? (string)json_encode($preview) : $preview;
     }
     public function isJson($string) {
         json_decode($string);
