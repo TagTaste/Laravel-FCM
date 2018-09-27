9faaaacc2b43939cb22b94619e62c8243ae50533
ashokTagtaste <ashok@tagtaste.com>
2018-09-27 11:21:11 +0530

    change in asc

--- app/Http/Controllers/Api/Collaborate/BatchController.php
+++ app/Http/Controllers/Api/Collaborate/BatchController.php
@@ -875,11 +875,14 @@ class BatchController extends Controller
 
         $questionIds = Collaborate\Questions::select('id')->where('collaborate_id',$collaborateId)->where('questions->select_type',5)->get()->pluck('id');
 
-        $overAllPreference = \DB::table('collaborate_tasting_user_review')->select('tasting_header_id','question_id','leaf_id','batch_id','value',\DB::raw('count(*) as total'))
-            ->where('collaborate_id',$collaborateId)->whereIn('question_id',$questionIds)
-            ->orderBy('batch_id','ASC')->orderBy('leaf_id','ASC')->groupBy('tasting_header_id','question_id','leaf_id','value','batch_id')->get();
+        $overAllPreference = \DB::table('collaborate_tasting_user_review')->join('collaborate_tasting_header','collaborate_tasting_questions.header_type_id','=','collaborate_tasting_header.id')
+            ->select('collaborate_tasting_header.id','collaborate_tasting_header.header_type','collaborate_tasting_user_review.leaf_id',
+                'collaborate_tasting_user_review.batch_id','collaborate_tasting_user_review.value',\DB::raw('count(*) as total'))
+            ->where('collaborate_tasting_user_review.collaborate_id',$collaborateId)->whereIn('collaborate_tasting_user_review.question_id',$questionIds)
+            ->orderBy('collaborate_tasting_header.id','collaborate_tasting_user_review.header_type_id','collaborate_tasting_user_review.batch_id')->groupBy('collaborate_tasting_user_review.question_id',
+                'collaborate_tasting_user_review.value','collaborate_tasting_user_review.leaf_id','collaborate_tasting_user_review.batch_id')->get();
 
-        $batches = \DB::table('collaborate_batches')->where('collaborate_id',$collaborateId)->get();
+//        $batches = \DB::table('collaborate_batches')->where('collaborate_id',$collaborateId)->get();
 //
 //        $overAllPreference = [];
 //        $headers = Collaborate\ReviewHeader::where('collaborate_id',$collaborateId)->get();
