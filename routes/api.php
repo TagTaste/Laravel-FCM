43ff0cf19a5d6a5c3e1524117001a2efc12c5197
ashokTagtaste <ashok@tagtaste.com>
2018-10-04 17:28:24 +0530

    add allergens to profile api

--- routes/api.php
+++ routes/api.php
@@ -386,7 +386,7 @@ Route::group(['namespace'=>'Api', 'as' => 'api.' //note the dot.
             Route::get("establishmentType","ProfileController@establishmentType");
 
 
-            Route::post("addAllergens","ProfileController@addAllergens");
+            Route::post("profile/addAllergens","ProfileController@addAllergens");
             Route::get("profile/tagging",['uses'=>'ProfileController@tagging']);
             Route::post('profile/follow',['uses'=>'ProfileController@follow']);
             Route::post('profile/unfollow',['uses'=>'ProfileController@unfollow']);
