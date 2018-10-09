ea58b61c2613e459cedb92dbf5b897cc9b348d1e
ashokTagtaste <ashok@tagtaste.com>
2018-10-09 18:31:20 +0530

    log

--- app/Http/Controllers/Api/CollaborateController.php
+++ app/Http/Controllers/Api/CollaborateController.php
@@ -589,6 +589,7 @@ class CollaborateController extends Controller
                 if(is_null($datum['parent_id'])||is_null($datum['categories']))
                     break;
                 $extra[] = $datum;
+                \Log::info("hq1");
                 $parentId = $datum['parent_id'] == 0 ? null : $datum['parent_id'];
                 $active = isset($datum['is_active']) ? $datum['is_active'] : 1;
                 $description = isset($datum['description']) ? $datum['description'] : null;
@@ -601,6 +602,7 @@ class CollaborateController extends Controller
             $data[] = ['type'=>'AROMA','s_no'=>$item['s_no'],'parent_id'=>$item['parent_id'],'value'=>$item['value'],'is_active'=>$item['is_active'],'description'=>$item['description']];
         }
         \Log::info($data);
+        \Log::info("hq");
         $this->model = \DB::table('global_nested_option')->insert($data);
         return $this->sendResponse();
     }
