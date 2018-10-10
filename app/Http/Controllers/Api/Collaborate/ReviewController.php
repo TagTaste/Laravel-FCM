596b02649c467b8ad83901cfc6e4f76b511775f0
ashokTagtaste <ashok@tagtaste.com>
2018-10-10 19:13:57 +0530

    Change

--- app/Http/Controllers/Api/Collaborate/ReviewController.php
+++ app/Http/Controllers/Api/Collaborate/ReviewController.php
@@ -39,6 +39,11 @@ class ReviewController extends Controller
             return $this->sendError("Wrong product assigned");
         }
         $currentStatus = $request->has('current_status') ? $request->input('current_status') : 2;
+        $latestCurrentStatus = \Redis::get("current_status:batch:$batchId:profile:$loggedInProfileId");
+        if($currentStatus == $latestCurrentStatus && $latestCurrentStatus == 3)
+        {
+            return $this->sendError("You have already completed this product");
+        }
         \Redis::set("current_status:batch:$batchId:profile:$loggedInProfileId" ,$currentStatus);
         $this->model = Review::where('profile_id',$loggedInProfileId)->where('collaborate_id',$collaborateId)
             ->where('batch_id',$batchId)->where('tasting_header_id',$headerId)->delete();
