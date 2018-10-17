293b0609948b7635c99fb689330414e0dc0cd9f7
ashokTagtaste <ashok@tagtaste.com>
2018-10-17 17:17:13 +0530

    add logic for pdf

--- resources/views/collaborates/reports.blade.php
+++ resources/views/collaborates/reports.blade.php
@@ -7,10 +7,15 @@
     <h1>{{$header['headerName']}}</h1>
     @foreach($header['data'] as $datum)
         <h2>{{$datum['title']}}</h2>
-        $question =
+        @php
+            $questions = $datum['question'];
+            $questions1 = (isset($questions->questions)) ? $questions->questions : [];
+        @endphp
+        @foreach($questions1 as $question)
+            <p>{{$question->title}}</p>
+        @endforeach
     @endforeach
     @if ($loop->last)
-        Do nothing
     @else
         <div class="page-break"></div>
     @endif
