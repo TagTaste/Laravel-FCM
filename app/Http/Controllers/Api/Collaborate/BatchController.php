4b5a543e832b98f7acf865a10d199ebf24eaf4dd
ashokTagtaste <ashok@tagtaste.com>
2018-10-24 14:32:07 +0530

    change

--- app/Http/Controllers/Api/Collaborate/BatchController.php
+++ app/Http/Controllers/Api/Collaborate/BatchController.php
@@ -1043,8 +1043,8 @@ class BatchController extends Controller
 //        dd($data);
         $pdf = PDF::loadView('collaborates.reports',['data' => $data,'filters'=>$filters]);
         $pdf = $pdf->output();
-        file_get_contents("/app/collaborate/collaboratesreport.pdf",$pdf);
-        $pdf = base64_encode(file_get_contents("/app/collaborate/collaboratesreport.pdf",$pdf));
+        file_put_contents(storage_path("app/collaborate/") ."collaboratesreport.pdf",$pdf);
+        $pdf = base64_encode(file_get_contents(storage_path("app/collaborate/") ."collaboratesreport.pdf",$pdf));
         return response()->json(['pdf'=>$pdf]);
     }
 }
