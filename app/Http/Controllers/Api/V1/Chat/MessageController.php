bd92453c211835759cf50da7714a037e5aa0f831
ashokTagtaste <ashok@tagtaste.com>
2018-11-14 15:59:10 +0530

    change

--- app/Http/Controllers/Api/V1/Chat/MessageController.php
+++ app/Http/Controllers/Api/V1/Chat/MessageController.php
@@ -113,6 +113,7 @@ class MessageController extends Controller
                     }
                 }
                 $this->model = Message::where('id',$messageId)->where('chat_id',$chatId)->first();
+                \Log::info("here is message");
                 $this->model->load('profile');
                 \Redis::publish("chat." . $this->model->chat_id,$this->model->toJson());
                 return $this->sendResponse();
