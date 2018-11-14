1f418f6545333f39d4af6c5d4fdccfa57e5e2359
ashokTagtaste <ashok@tagtaste.com>
2018-11-14 15:47:35 +0530

    change

--- app/Http/Controllers/Api/V1/Chat/MessageController.php
+++ app/Http/Controllers/Api/V1/Chat/MessageController.php
@@ -112,6 +112,7 @@ class MessageController extends Controller
                             \DB::table('message_recepients')->insert(['message_id'=>$messageId, 'recepient_id'=>$profileId, 'chat_id'=>$chatId, 'sent_on'=>$this->model["created_at"]]);
                     }
                 }
+                $this->model = Message::where('id',$messageId)->where('chat_id',$chatId)->first();
                 return $this->sendResponse();
             }
         
