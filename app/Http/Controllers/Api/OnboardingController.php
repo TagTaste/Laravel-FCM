7370862b7cc4ccf2ef7728964d0f6faeb1a5b3b7
ashokTagtaste <ashok@tagtaste.com>
2018-11-19 10:51:09 +0530

    change in onboarding item

--- app/Http/Controllers/Api/OnboardingController.php
+++ app/Http/Controllers/Api/OnboardingController.php
@@ -74,8 +74,13 @@ class OnboardingController extends Controller
             $profile->self = false;
             $profileData[] = $profile;
         }
-
-        $this->model['from_selection'] = $profileData;
+        // title is header in boarding
+        // type is data of item is profile or company
+        // ui_type = 0  is horizontal , ui_type = 1 is vertical
+        $this->model[]['title'] = "Your Selection";
+        $this->model[]['type'] = "profile";
+        $this->model[]['ui_type'] = 0;
+        $this->model[]['item'] = $profileData;
 
         $foundationTeamIds = [1,10,32,165,44,556,2,4,13,637,7,2245,12,6,1585,359,1467,8,1775,3379,1574,14,15,7585,1016];
 
@@ -106,10 +111,17 @@ class OnboardingController extends Controller
             $profile->self = false;
             $profileData[] = $profile;
         }
+        $this->model[]['title'] = "Foundation team";
+        $this->model[]['type'] = "profile";
+        $this->model[]['ui_type'] = 0;
+        $this->model[]['item'] = $profileData;
 
-        $this->model['foundation_team'] = $profileData;
+        $this->model[]['title'] = "Activity Based";
+        $this->model[]['type'] = "profile";
+        $this->model[]['ui_type'] = 0;
+        $this->model[]['item'] = $profileData;
 
-        $this->model['activity_based'] = $profileData; // should be later
+//        $this->model['activity_based'] = $profileData; // should be later
 
         $companyIds = [111,137,322,84,11,321,277,271,253,245,204,197,193,187,186];
         $companyData = [];
@@ -127,7 +139,11 @@ class OnboardingController extends Controller
             $company->isFollowing = \Redis::sIsMember("following:profile:" . $loggedInProfileId,"company." . $company->id) === 1;
             $companyData[] = $company;
         }
-        $this->model['company'] = $companyData;
+//        $this->model['company'] = $companyData;
+        $this->model[]['title'] = "Company Profile";
+        $this->model[]['type'] = "company";
+        $this->model[]['ui_type'] = 1;
+        $this->model[]['item'] = $companyData;
         return $this->sendResponse();
     }
 
@@ -140,15 +156,15 @@ class OnboardingController extends Controller
         $ids = \DB::table('profile_specializations')->whereIn('specialization_id',$ids)->get()->pluck('profile_id');
         $profileIds = $profileIds->merge($ids);
 
-        // cuisine
-        $ids = \DB::table('profiles_cuisines')->where('profile_id',$loggedInProfileId)->get()->pluck('cuisine_id');
-        $ids = \DB::table('profiles_cuisines')->whereIn('cuisine_id',$ids)->get()->pluck('profile_id');
-        $profileIds = $profileIds->merge($ids);
-
-        //establishment type
-        $ids = \DB::table('profile_establishment_types')->where('profile_id',$loggedInProfileId)->get()->pluck('establishment_type_id');
-        $ids = \DB::table('profile_establishment_types')->whereIn('establishment_type_id',$ids)->get()->pluck('profile_id');
-        $profileIds = $profileIds->merge($ids);
+//        // cuisine
+//        $ids = \DB::table('profiles_cuisines')->where('profile_id',$loggedInProfileId)->get()->pluck('cuisine_id');
+//        $ids = \DB::table('profiles_cuisines')->whereIn('cuisine_id',$ids)->get()->pluck('profile_id');
+//        $profileIds = $profileIds->merge($ids);
+//
+//        //establishment type
+//        $ids = \DB::table('profile_establishment_types')->where('profile_id',$loggedInProfileId)->get()->pluck('establishment_type_id');
+//        $ids = \DB::table('profile_establishment_types')->whereIn('establishment_type_id',$ids)->get()->pluck('profile_id');
+//        $profileIds = $profileIds->merge($ids);
 
         // job profile
         $ids = \DB::table('profile_occupations')->where('profile_id',$loggedInProfileId)->get()->pluck('occupation_id');
