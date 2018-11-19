62c110320cb17bf8ab9359cb8a03520bce77009b
ashokTagtaste <ashok@tagtaste.com>
2018-11-19 20:01:48 +0530

    change

--- app/Http/Controllers/Api/V1/Chat/MessageController.php
+++ app/Http/Controllers/Api/V1/Chat/MessageController.php
@@ -91,16 +91,15 @@ class MessageController extends Controller
                 $inputs['preview']['image'] = $resp;
                 }
                 $preview = $inputs['preview'];
-                $fileMeta = $request->input('file_meta');
             }
             else
             {
                 $preview = null;
-                $fileMeta = null;
             }
             $this->model = Message::create(['profile_id'=>$loggedInProfileId, 'chat_id'=>$chatId,
                 'message'=>$request->input('message'), 'parent_message_id'=>$parentMessageId,
-                'preview'=> $preview, 'signature'=>$request->input('signature'),'file'=>$request->input('file'),'file_meta'=>$fileMeta]);
+                'preview'=> $preview, 'signature'=>$request->input('signature'),'file'=>isset($request->input('file')) ? $request->input('file') : null,
+                'file_meta'=>isset($request->input('file_meta')) ? $request->input('file_meta') : null]);
             $messageId = $this->model->id;
 
             if(isset($messageId))
