a7bc40400bfd5b3527f0e7cfa73e48b7d1d43165
ashokTagtaste <ashok@tagtaste.com>
2018-11-19 14:48:38 +0530

    add signature

--- app/Http/Controllers/Api/V1/Chat/MessageController.php
+++ app/Http/Controllers/Api/V1/Chat/MessageController.php
@@ -204,7 +204,9 @@ class MessageController extends Controller
                 })->blur(1)->stream('jpg',70);
                 \Storage::disk('s3')->put($tinyImagePath, (string) $thumbnail,['visibility'=>'public']);
                 $response['tiny_photo'] = \Storage::url($tinyImagePath);
-                $response['meta'] = getimagesize($file);
+//                $response['meta'] = getimagesize($file);
+                list($width, $height, $type, $attr) = getimagesize($file);
+                dd($width);
                 array_push($response['meta'], $response['tiny_photo']);
                 }
                 else
