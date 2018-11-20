90f334b058d33fd5394e38d06e9ab26ff1d8a342
ashokTagtaste <ashok@tagtaste.com>
2018-11-20 18:08:12 +0530

    change

--- app/Http/Controllers/Api/V1/ChatController.php
+++ app/Http/Controllers/Api/V1/ChatController.php
@@ -292,7 +292,7 @@ class ChatController extends Controller
             else
                 $isAdmin = 0;
             $chatMembers[] = ['chat_id'=>$chatId,'profile_id'=>$profileId,'created_at'=>$this->now,'is_admin'=>$isAdmin];
-            \Redis::publish("new-chat", $chatId);
+            \Redis::publish("new-chat", $this->model->toJson());
         }
 
         $this->model->members()->insert($chatMembers);
