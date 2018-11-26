8725bd2fd0c5ab13d4e22355925e2b02e9abe91e
ashokTagtaste <ashok@tagtaste.com>
2018-11-26 11:44:26 +0530

    change in onboarding seeder table

--- database/seeds/OnboardingInterestCollectionPivotSubCategoryTableSeeder.php
+++ database/seeds/OnboardingInterestCollectionPivotSubCategoryTableSeeder.php
@@ -11,33 +11,33 @@ class OnboardingInterestCollectionPivotSubCategoryTableSeeder extends Seeder
      */
     public function run()
     {
-        $data = [];
-        $data[] = ['interested_collection_id'=>1,'product_sub_category_id'=>1];
-        $data[] = ['interested_collection_id'=>1,'product_sub_category_id'=>2];
-        $data[] = ['interested_collection_id'=>1,'product_sub_category_id'=>3];
-        $data[] = ['interested_collection_id'=>1,'product_sub_category_id'=>4];
-        $data[] = ['interested_collection_id'=>1,'product_sub_category_id'=>5];
+//        $data = [];
+//        $data[] = ['interested_collection_id'=>1,'product_sub_category_id'=>1];
+//        $data[] = ['interested_collection_id'=>1,'product_sub_category_id'=>2];
+//        $data[] = ['interested_collection_id'=>1,'product_sub_category_id'=>3];
+//        $data[] = ['interested_collection_id'=>1,'product_sub_category_id'=>4];
+//        $data[] = ['interested_collection_id'=>1,'product_sub_category_id'=>5];
+//
+//        $data[] = ['interested_collection_id'=>2,'product_sub_category_id'=>6];
+//        $data[] = ['interested_collection_id'=>2,'product_sub_category_id'=>7];
+//        $data[] = ['interested_collection_id'=>2,'product_sub_category_id'=>8];
+//        $data[] = ['interested_collection_id'=>2,'product_sub_category_id'=>2];
+//        $data[] = ['interested_collection_id'=>2,'product_sub_category_id'=>3];
+//
+//        $data[] = ['interested_collection_id'=>3,'product_sub_category_id'=>9];
+//        $data[] = ['interested_collection_id'=>3,'product_sub_category_id'=>10];
+//        $data[] = ['interested_collection_id'=>3,'product_sub_category_id'=>11];
+//
+//        $data[] = ['interested_collection_id'=>4,'product_sub_category_id'=>12];
+//        $data[] = ['interested_collection_id'=>4,'product_sub_category_id'=>13];
+//
+//        $data[] = ['interested_collection_id'=>5,'product_sub_category_id'=>14];
+//
+//        $data[] = ['interested_collection_id'=>6,'product_sub_category_id'=>15];
+//
+//        $data[] = ['interested_collection_id'=>3,'product_sub_category_id'=>16];
+//        $data[] = ['interest_collection_id'=>6,'product_sub_category_id'=>17];
 
-        $data[] = ['interested_collection_id'=>2,'product_sub_category_id'=>6];
-        $data[] = ['interested_collection_id'=>2,'product_sub_category_id'=>7];
-        $data[] = ['interested_collection_id'=>2,'product_sub_category_id'=>8];
-        $data[] = ['interested_collection_id'=>2,'product_sub_category_id'=>2];
-        $data[] = ['interested_collection_id'=>2,'product_sub_category_id'=>3];
-
-        $data[] = ['interested_collection_id'=>3,'product_sub_category_id'=>9];
-        $data[] = ['interested_collection_id'=>3,'product_sub_category_id'=>10];
-        $data[] = ['interested_collection_id'=>3,'product_sub_category_id'=>11];
-
-        $data[] = ['interested_collection_id'=>4,'product_sub_category_id'=>12];
-        $data[] = ['interested_collection_id'=>4,'product_sub_category_id'=>13];
-
-        $data[] = ['interested_collection_id'=>5,'product_sub_category_id'=>14];
-
-        $data[] = ['interested_collection_id'=>6,'product_sub_category_id'=>15];
-
-        $data[] = ['interested_collection_id'=>3,'product_sub_category_id'=>16];
-        $data[] = ['interest_collection_id'=>6,'product_sub_category_id'=>17];
-
-        \DB::table("interested_pivot_sub_category")->insert($data);
+//        \DB::table("interested_pivot_sub_category")->insert($data);
     }
 }
