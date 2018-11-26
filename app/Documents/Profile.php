2232f9ca6ea6ad9d0e1f3277d387ee0edfb14edd
ashokTagtaste <ashok@tagtaste.com>
2018-11-26 11:25:22 +0530

    change in profile order

--- app/Documents/Profile.php
+++ app/Documents/Profile.php
@@ -4,10 +4,25 @@ class Profile extends Document
 {
     public $type = 'profile';
     
-    public $bodyProperties = ['name','handle','ingredients','about','address','interests','expertise','keywords','city','college'];
+    public $bodyProperties = ['name','handle','about','occupation','specialization','city','company','college'];
     
     public function getValueOfCollege()
     {
         return $this->model->education()->select('college')->get()->pluck('college')->toArray();
+    }
+
+    public function getValueOfCompany()
+    {
+        return $this->model->experience()->select('company')->get()->pluck('company')->toArray();
+    }
+
+    public function getValueOfOccupation()
+    {
+        return $this->model->profile_occupations()->select('name')->get()->pluck('name')->toArray();
+    }
+
+    public function getValueOfSpecialization()
+    {
+        return $this->model->profile_specializations()->select('name')->get()->pluck('name')->toArray();
     }
 }
