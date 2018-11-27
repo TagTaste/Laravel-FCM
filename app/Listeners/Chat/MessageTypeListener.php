32b810f5dc62e0b724cc9d01a7e3481da77cbde9
vagrant <tushar@tagtaste.com>
2018-11-27 09:23:34 +0000

    debug

--- app/Listeners/Chat/MessageTypeListener.php
+++ app/Listeners/Chat/MessageTypeListener.php
@@ -28,7 +28,7 @@ class MessageTypeListener
     {   
         $model = \App\V1\Chat\Message::create($event->info);
         $chatId = $event->info["chat_id"];
-        $membersOfChat = \App\V1\Chat\Member::where('chat_id',$chatId)->pluck('profile_id');
+        $membersOfChat = \App\V1\Chat\Member::where('chat_id',$chatId)->whereNull('exited_on')->pluck('profile_id');
         foreach ($membersOfChat as $member) {
             \DB::table('message_recepients')->insert(['chat_id'=>$chatId, 'message_id'=>$model->id, 'recepient_id'=>$member, 'sent_on'=>$model->created_at, 'read_on'=>$model->created_at]);
         }
