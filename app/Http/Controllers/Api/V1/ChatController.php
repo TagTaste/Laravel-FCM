5c2b5c99bc9c5cce794938dfcf84be29475e2cdb
ashokTagtaste <ashok@tagtaste.com>
2018-11-30 15:46:03 +0530

    change in s3 url

--- app/Http/Controllers/Api/V1/ChatController.php
+++ app/Http/Controllers/Api/V1/ChatController.php
@@ -241,7 +241,7 @@ class ChatController extends Controller
                     $s3 = \Storage::disk('s3');
                     $filePath = 'p/' . $loggedInProfileId . "/ci";
                     $resp = $s3->putFile($filePath, new File(storage_path($image)), 'public');
-                    $inputs['preview']['image'] = $resp;
+                    $inputs['preview']['image'] = \Storage::disk('s3')->url($resp);
                 }
                 if(isset($inputs['preview']))
                 {
