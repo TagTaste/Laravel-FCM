41daa17d720ff084892a49c0f35e58c874455540
ashokTagtaste <ashok@tagtaste.com>
2018-11-30 18:34:13 +0530

    change in command

--- app/Console/Commands/ChatMergeMessage.php
+++ app/Console/Commands/ChatMergeMessage.php
@@ -38,15 +38,19 @@ class ChatMergeMessage extends Command
             foreach ($models as $model)
             {
                 $memberIds = \DB::table('chat_members')->where('chat_id',$model->id)->where('is_single',1)->get()->pluck('profile_id');
-                $chatIds = \DB::table('chat_members')->whereIn('profile_id',$memberIds)->where('is_single',1)->get()->pluck('chat_id');
+                $chatIds = \DB::table('chat_members as m1')->join('chat_members as m2','m2.chat_id','=','m1.chat_id')
+                    ->where('m1.profile_id',$memberIds[0])->where('m2.profile_id',$memberIds[1])->where('m1.is_single',1)->where('m2.is_single',1)
+                    ->get()->pluck('m1.chat_id');
+
                 $messageChatId = \DB::table('chat_messages')->whereIn('chat_id',$chatIds)->orderBy('created_at','desc')->first();
                 foreach ($chatIds as $chatId)
                 {
-                    $count = 0;
-//                    echo "chat id is here ".$chatId."\n";
+                    $count = 1;
+                    echo "chat id is here ".$chatId."\n";
                     $checkChat = Chat::where('id',$chatId)->whereNull('name')->exists();
                     if($checkChat)
                     {
+                        echo "count is here ".$count."\n";
 //                        $count = \DB::table('chat_messages')->where('chat_id',$chatId)->update(['chat_id'=>$messageChatId]);
                         echo "new count is here ".$count."\n";
                     }
