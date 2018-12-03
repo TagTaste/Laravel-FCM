379a1c5efe0554d8fdb4f2f79e295ef7388f12bb
ashokTagtaste <ashok@tagtaste.com>
2018-12-03 11:00:30 +0530

    resolve conflit

--- app/Http/Controllers/Api/OnboardingController.php
+++ app/Http/Controllers/Api/OnboardingController.php
@@ -83,21 +83,13 @@ class OnboardingController extends Controller
         // ui_type = 0  is horizontal , ui_type = 1 is vertical
         // color_code = 0 white and color_code = 1 is gray
         if(count($profileData))
-<<<<<<< HEAD
-            $this->model[] = ['title'=>'Your Selection','type'=>'profile','ui_type'=>0,'item'=>$profileData,'color_code'=>'rgb(255, 255, 255)'];
-=======
             $this->model[] = ['title'=>'TAGTASTE RECOMMENDATIONS','type'=>'profile','ui_type'=>0,'item'=>$profileData,'color_code'=>'rgb(255, 255, 255)'];
->>>>>>> 3846fa638ce56048685dc5d2f7833076b13f5ba8
 
         $foundationTeamIds = [1,10,32,165,44,556,2,4,13,637,7,2245,12,6,1585,359,1467,8,1775,3379,1574,14,15,7585,1016];
 
         foreach ($foundationTeamIds as $key => $value)
         {
-<<<<<<< HEAD
-            if($loggedInProfileId == $value)
-=======
             if($loggedInProfileId == $value || in_array($value,$repeatProfileIds))
->>>>>>> 3846fa638ce56048685dc5d2f7833076b13f5ba8
             {
                 unset($foundationTeamIds[$key]);
                 continue;
@@ -123,11 +115,6 @@ class OnboardingController extends Controller
             $profileData[] = $profile;
         }
         if(count($profileData))
-<<<<<<< HEAD
-            $this->model[] = ['title'=>'Foundation team','type'=>'profile','ui_type'=>0,'item'=>$profileData,'color_code'=>'rgb(247, 247, 247)'];
-        if(count($profileData))
-            $this->model[] = ['title'=>'Activity Based','type'=>'profile','ui_type'=>0,'item'=>$profileData,'color_code'=>'rgb(247, 247, 247)'];
-=======
             $this->model[] = ['title'=>'FOUNDING TEAM','type'=>'profile','ui_type'=>0,'item'=>$profileData,'color_code'=>'rgb(247, 247, 247)'];
 
         $activityBasedIds = [804,70,5555,27,685,626,2376,71,530,1315,48,961,383,1195,354,358,123,238,4338,787];
@@ -162,7 +149,6 @@ class OnboardingController extends Controller
 
         if(count($profileData))
             $this->model[] = ['title'=>'ACTIVE & INFLUENTIAL','type'=>'profile','ui_type'=>1,'item'=>$profileData,'color_code'=>'rgb(247, 247, 247)'];
->>>>>>> 3846fa638ce56048685dc5d2f7833076b13f5ba8
 
 //        $this->model['activity_based'] = $profileData; // should be later
 
@@ -184,11 +170,7 @@ class OnboardingController extends Controller
         }
 //        $this->model['company'] = $companyData;
         if(count($companyData))
-<<<<<<< HEAD
-            $this->model[] = ['title'=>'Company Profile','type'=>'company','ui_type'=>1,'item'=>$companyData,'color_code'=>'rgb(255, 255, 255)'];
-=======
             $this->model[] = ['title'=>'COMPANIES TO FOLLOW','type'=>'company','ui_type'=>0,'item'=>$companyData,'color_code'=>'rgb(255, 255, 255)'];
->>>>>>> 3846fa638ce56048685dc5d2f7833076b13f5ba8
 
         return $this->sendResponse();
     }
