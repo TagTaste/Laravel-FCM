a67aa6562cdc16abf700ab26a0b51f535940baa6
ashokTagtaste <ashok@tagtaste.com>
2018-12-04 17:10:30 +0530

    add log

--- app/Http/Controllers/Api/V1/SearchController.php
+++ app/Http/Controllers/Api/V1/SearchController.php
@@ -228,7 +228,6 @@ class SearchController extends Controller
         $loggedInProfileId = $request->user()->profile->id;
         $profileIds = \DB::table('profile_specializations')->where('specialization_id',$id)->get()->pluck('profile_id');
         $profileIds = $profileIds->unique();
-        \Log::info($profileIds);
         foreach ($profileIds as $key => $value)
         {
             if($loggedInProfileId == $value)
@@ -244,6 +243,7 @@ class SearchController extends Controller
             $data = \Redis::mget($profileIds);
 
         }
+        dd($data);
         $profileData = [];
         if(count($data))
         {
