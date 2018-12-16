28646ce8c22cd7a630f014b61c1e4d872db1c4c3
ashokTagtaste <ashok@tagtaste.com>
2018-12-16 19:04:54 +0530

    feature/new-questionair

--- app/Console/Commands/CollaborationQuestions.php
+++ app/Console/Commands/CollaborationQuestions.php
@@ -75,6 +75,7 @@ class CollaborationQuestions extends Command implements ShouldQueue
                 $isNested = isset($item['is_nested_question']) && $item['is_nested_question'] == 1 ? 1 : 0;
                 $isMandatory = isset($item['is_mandatory']) && $item['is_mandatory'] == 1 ? 1 : 0;
                 $option = isset($item['option']) ? $item['option'] : null;
+                \Log::info($option);
                 if(isset($item['select_type']))
                 {
                     $value = $item['option'];
