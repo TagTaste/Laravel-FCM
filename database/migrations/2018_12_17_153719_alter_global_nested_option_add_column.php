450e58a8537431210507e274a0601d0234811b25
ashokTagtaste <ashok@tagtaste.com>
2018-12-17 15:57:20 +0530

    change

---
+++ database/migrations/2018_12_17_153719_alter_global_nested_option_add_column.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterGlobalNestedOptionAddColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('global_nested_option', function (Blueprint $table) {
+            $table->boolean('is_different_intensity')->default(0);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('global_nested_option', function (Blueprint $table) {
+            $table->dropColumn('last_seen');
+        });
+    }
+}
