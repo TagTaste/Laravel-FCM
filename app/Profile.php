b14c159c9f782b621f4ea889ea2845fddd481b35
ashokTagtaste <ashok@tagtaste.com>
2018-12-21 19:07:46 +0530

    chane

--- app/Profile.php
+++ app/Profile.php
@@ -190,6 +190,7 @@ class Profile extends Model
     public function getDobAttribute($value)
     {
         if (!empty($value)) {
+            return date("d-m-Y", strtotime($value));
             if(request()->user()->profile->id == $this->id)
             {
                 return date("d-m-Y", strtotime($value));
@@ -718,6 +719,7 @@ class Profile extends Model
     public function getAddressAttribute($value)
     {
         if (!empty($value)) {
+            return $value;
             if(request()->user()->profile->id == $this->id)
             {
                 return $value;
@@ -738,6 +740,7 @@ class Profile extends Model
     public function getPhoneAttribute($value)
     {
         if (!empty($value)) {
+            return $value;
             if(request()->user()->profile->id == $this->id)
             {
                 return $value;
