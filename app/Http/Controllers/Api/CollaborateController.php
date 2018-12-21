796faad037c392391928f763bb037e2927826bc4
ashokTagtaste <ashok@tagtaste.com>
2018-12-21 17:42:17 +0530

    change

--- app/Http/Controllers/Api/CollaborateController.php
+++ app/Http/Controllers/Api/CollaborateController.php
@@ -633,4 +633,26 @@ class CollaborateController extends Controller
         $this->model = $data;
         return $this->sendResponse();
     }
+
+    public function uploadBrandLogo(Request $request)
+    {
+        $profileId = $request->user()->profile->id;
+        $imageName = str_random("32") . ".jpg";
+        $path = "images/p/$profileId/collaborate";
+        $randnum = rand(10,1000);
+        //create a tiny image
+        $path = $path."/brand_logo/$randnum";
+        $thumbnail = \Image::make($request->file('image'))->resize(640, null,function ($constraint) {
+            $constraint->aspectRatio();
+        })->blur(1)->stream('jpg',70);
+        \Storage::disk('s3')->put($path, (string) $thumbnail,['visibility'=>'public']);
+        $response = \Storage::url($path);
+        if(!$response){
+            throw new \Exception("Could not save image " . $imageName . " at " . $path);
+        }
+        $this->model = $response;
+        return $this->sendResponse();
+
+    }
+
 }
