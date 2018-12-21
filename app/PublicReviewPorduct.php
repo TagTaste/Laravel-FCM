a3478f38cd990e7afdbb89af87ff506a1392df1c
ashokTagtaste <ashok@tagtaste.com>
2018-12-21 17:53:18 +0530

    change

--- app/PublicReviewPorduct.php
+++ app/PublicReviewPorduct.php
@@ -108,8 +108,8 @@ class PublicReviewPorduct extends Model
     public function getOverallRatingAttribute()
     {
         $header = ReviewHeader::where('global_question_id',$this->global_question_id)->where('header_selection_type',2)->first();
-        $overallPreferances = \DB::table('public_product_user_review')->where('product_id',$this->product_id)->where('header_id',$header->id)->where('select_type',5)->sum('leaf_id');
-        $userCount = \DB::table('public_product_user_review')->where('product_id',$this->product_id)->where('header_id',$header->id)->where('select_type',5)->count();
+        $overallPreferances = \DB::table('public_product_user_review')->where('product_id',$this->id)->where('header_id',$header->id)->where('select_type',5)->sum('leaf_id');
+        $userCount = \DB::table('public_product_user_review')->where('product_id',$this->id)->where('header_id',$header->id)->where('select_type',5)->get()->count();
         $meta = [];
         $meta['max_rating'] = 8;
         $meta['overall_rating'] = $userCount > 0 ? $overallPreferances/$userCount : 0.00;
