9c7e79fe2a522fc4b52d0a14e20fe4fa82fdde61
ashokTagtaste <ashok@tagtaste.com>
2019-01-08 16:39:46 +0530

    change in data

--- app/PublicReviewProduct/ReviewHeader.php
+++ app/PublicReviewProduct/ReviewHeader.php
@@ -18,6 +18,13 @@ class ReviewHeader extends Model
     public function getHeaderInfoAttribute($value)
     {
         if(isset($value))
-            return $value;
+        {
+            $value = json_decode($value,true);
+            $images = isset($value->images) ? $value->images : [];
+            $imageValues = [];
+            foreach ($images as $image)
+                $imageValues[] = $image;
+            return ['text'=> isset($value->text) ? $value->text : null,"images"=>$imageValues , "video_link"=>isset($value->video_link) ? $value->video_link : null ];
+        }
     }
 }
