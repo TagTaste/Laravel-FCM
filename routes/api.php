25c3a81b2aa2b08c3a2b542bc7010a9c8058f9ee
ashokTagtaste <ashok@tagtaste.com>
2019-01-08 17:31:14 +0530

    change in data

--- routes/api.php
+++ routes/api.php
@@ -141,6 +141,7 @@ Route::group(['namespace'=>'Api', 'as' => 'api.' //note the dot.
 
 
                 //search apis new
+                Route::get("public-review/explore","SearchController@explore");
                 Route::get("search/explore","SearchController@explore");
                 Route::get("search/discover",'SearchController@discover');
                 Route::get("search/{type?}",'SearchController@search');
