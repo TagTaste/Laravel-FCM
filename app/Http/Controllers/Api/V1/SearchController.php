d9d8e26a17df01d0f84a24846f0e9eebed38c3d4
ashokTagtaste <ashok@tagtaste.com>
2019-01-10 14:37:04 +0530

    change in questionair

--- app/Http/Controllers/Api/V1/SearchController.php
+++ app/Http/Controllers/Api/V1/SearchController.php
@@ -708,7 +708,7 @@ class SearchController extends Controller
             $model[] = ['title'=>'Categories','subtitle'=>'LENSES FOR THE F&B INDUSTRY','item'=>$categories,
                 'ui_type'=>13,'color_code'=>'rgb(255, 255, 255)','type'=>'category','is_see_more'=>1];
 
-        $recommended = PublicReviewProduct\ProductCategory::where('mark_featured',1)->inRandomOrder()->limit(6)->get();
+        $recommended = PublicReviewProduct\ProductCategory::where('is_active',1)->inRandomOrder()->limit(6)->get();
         if($recommended->count())
             $model[] = ['title'=>'Featured Products','subtitle'=>'Products in focus this week','item'=>$recommended,
                 'ui_type'=>14,'color_code'=>'rgb(255, 255, 255)','type'=>'product','is_see_more'=>1];
