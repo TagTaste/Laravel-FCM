e6e8ee2b89830b031af2f92582b52405c76e7c41
vagrant <tushar@tagtaste.com>
2019-01-16 12:51:57 +0000

    spelling correction

--- app/Notifications/Actions/Comment.php
+++ app/Notifications/Actions/Comment.php
@@ -21,7 +21,7 @@ class Comment extends Action
     
     public function __construct($event)
     {
-        \Log::info($event);
+        \Log::info("here");
         parent::__construct($event);
         $this->view = 'emails.'.$this->data->action;
         $this->sub = $this->data->who['name'] ." commented on your post";
