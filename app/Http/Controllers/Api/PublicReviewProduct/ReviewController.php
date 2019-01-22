1618d166f5a34f9f32886def6117a36ed8af7704
ashokTagtaste <ashok@tagtaste.com>
2019-01-22 14:17:25 +0530

    change

--- app/Http/Controllers/Api/PublicReviewProduct/ReviewController.php
+++ app/Http/Controllers/Api/PublicReviewProduct/ReviewController.php
@@ -229,9 +229,9 @@ class ReviewController extends Controller
         $review->comments()->attach($comment->id);
         event(new \App\Events\Actions\ReviewComment($review,$request->user()->profile, $comment->content, null, null, null, $comment));
 
-        if($comment->has_tags){
-            event(new Tag($review,$request->user()->profile,$comment->content, null, null, null, $comment));
-        }
+//        if($comment->has_tags){
+//            event(new Tag($review,$request->user()->profile,$comment->content, null, null, null, $comment));
+//        }
 
         $this->model = $comment;
 
