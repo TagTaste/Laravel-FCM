30b91d2577ad06a94166114146874d2138e43e5a
ashokTagtaste <ashok@tagtaste.com>
2019-01-23 12:11:42 +0530

    add log

--- app/Notifications/Action.php
+++ app/Notifications/Action.php
@@ -13,7 +13,7 @@ use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Crypt;
 
-class Action extends Notification implements ShouldQueue
+class Action extends Notification
 {
     use GetTags, Queueable;
     
