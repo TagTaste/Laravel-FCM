56c7b39749452b40b0127849078ba7d5247fccc6
ashokTagtaste <ashok@tagtaste.com>
2019-02-12 17:25:16 +0530

    change in data

--- app/Jobs/ProductSuggestion.php
+++ app/Jobs/ProductSuggestion.php
@@ -8,7 +8,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
-class ProductSuggestion implements ShouldQueue
+class ProductSuggestion
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
