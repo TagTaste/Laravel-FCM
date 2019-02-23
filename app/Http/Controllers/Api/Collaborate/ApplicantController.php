002c06ec6c59bb563826d0e8491c896fa2ec984e
ashokTagtaste <ashok@tagtaste.com>
2019-02-23 11:52:51 +0530

    change in applier address

--- app/Http/Controllers/Api/Collaborate/ApplicantController.php
+++ app/Http/Controllers/Api/Collaborate/ApplicantController.php
@@ -125,6 +125,11 @@ class ApplicantController extends Controller
             $address = json_decode($applierAddress,true);
             $city = $address['city'];
             $profile = Profile::where('id',$loggedInprofileId)->first();
+            if(!isset($profile->ageRange) || is_null($profile->ageRange) || !isset($profile->gender) || is_null($profile->gender))
+            {
+                $this->model = null;
+                return $this->sendError("Please fill mandatory feild.");
+            }
             $inputs = ['is_invite'=>$isInvited,'profile_id'=>$loggedInprofileId,'collaborate_id'=>$collaborateId,
                 'message'=>$request->input('message'),'applier_address'=>$applierAddress,'hut'=>$hut,
                 'shortlisted_at'=>$now,'city'=>$city,'age_group'=>$profile->ageRange,'gender'=>$profile->gender];
