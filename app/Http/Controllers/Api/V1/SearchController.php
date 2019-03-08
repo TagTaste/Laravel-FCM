3f04ea2b0c75d86ffe90bbdc197ce1b6f5cad901
ashokTagtaste <ashok@tagtaste.com>
2019-03-08 17:07:49 +0530

    chage in data explrore constant change

--- app/Http/Controllers/Api/V1/SearchController.php
+++ app/Http/Controllers/Api/V1/SearchController.php
@@ -1207,9 +1207,10 @@ class SearchController extends Controller
             /* ui type = 1 is start */
 
         $chefOfTheWeekProfileData = \DB::table('constant_variable_model')->where('ui_type',1)->where('model_name','profile')->where('is_active',1)->first();
+        return $chefOfTheWeekProfileData;
         if($chefOfTheWeekProfileData)
             $chefOfTheWeekProfileData->data_json = json_decode($chefOfTheWeekProfileData->data_json);
-        $chefOfTheWeekProfileId = (int)$chefOfTheWeekProfileData->model_id;
+        $chefOfTheWeekProfileId = isset($chefOfTheWeekProfileData->model_id)? (int)$chefOfTheWeekProfileData->model_id : null;
         $chefOfTheWeekProfile = Redis::get('profile:small:' . $chefOfTheWeekProfileId);
         $data = json_decode($chefOfTheWeekProfile);
         if(!is_null($data))
