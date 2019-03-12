e9c0c72af477031bae60c5d5ae06023014b8c8f5
TagTaste <tagtaste@Tushars-MacBook-Pro.local>
2019-03-12 12:04:02 +0530

    new model

--- app/Http/Controllers/FaqsController.php
+++ app/Http/Controllers/FaqsController.php
@@ -90,8 +90,8 @@ class FaqsController extends Controller
     public function allCategories()
     {
         $checkExist =
-        $this->model = \DB::table('faq_categories')->rightJoin('faq_question_answer',function($join){
-            $join->on('faq_categories.id','=','faq_question_answer.faq_category_id')->where('faq_question_answer.is_active',1)->whereNull('faq_question_answer.deleted_at');
+        $this->model = \DB::table('faq_categories')->rightJoin('faqs_question_answer',function($join){
+            $join->on('faq_categories.id','=','faqs_question_answer.faq_category_id')->where('faqs_question_answer.is_active',1)->whereNull('faqs_question_answer.deleted_at');
         })->whereNUll('faq_categories.deleted_at')->get();
         return response()->json(['data'=>$this->model]);
     }
