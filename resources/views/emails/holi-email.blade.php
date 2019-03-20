e0afab425571c47c0f2f0391f800d9f76d876612
Harsh Arora <arora.harsh@yahoo.com>
2019-03-20 23:42:37 +0530

    WIP: Holi email

---
+++ resources/views/emails/holi-email.blade.php
@@ -0,0 +1,70 @@
+@include("emails.header.definition")
+<body style="margin: 0; padding: 0;">
+<center>
+    <table width="100%" border="0" cellspacing="0" cellpadding="0" style="font-family:Arial" bgcolor="#F8F6F9">
+        @include("emails.header.header")
+        <tr>
+            <td align="center" valign="top">
+                <table class="container" width="620" align="center" border="0" cellspacing="0" cellpadding="0"
+                       border-collapse="collapse">
+                    <tr>
+                        <td align="center" width="100%" bgcolor="#F8F6F9">
+                            <table width="550" align="center" border="0" cellspacing="0" cellpadding="0"
+                                   border-collapse="collapse">
+                                <tr>
+                                    <td align="center" bgcolor="#FFFFFF" style="padding:20px 0px 20px 0px;border-radius: 10px 10px 0 0;width:100%!important;" width="100%">
+                                        <div style="font-size: 20px;font-weight: bold;color: #181818;padding: 0 40px 0 40px">
+                                            Happy Holi
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="background-color: #FAFAFA;height: 10px;">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="width:100%!important;background-color:#FFFFFF!important;padding: 0px 40px 0px 40px;border-bottom: 1px solid rgba(0, 0, 0, 0.04);">
+                                        <table style="margin: 20px 0px 20px 0px;" width="100%" bgcolor="#FFFFFF"
+                                               style="border-bottom: 1px solid rgba(0, 0, 0, 0.04);">
+                                            <tr>
+                                                <td align="center" valign="top" width="100%"
+                                                    class="templateColumnContainer">
+                                                    <table border="0" cellpadding="0" cellspacing="0" width="100%"
+                                                           bgcolor="#FFFFFF">
+                                                        <tr>
+                                                            <td>
+                                                                <div>
+                                                                    <br>
+                                                                    <div style="width:100%; text-align: center;"><img style="max-width: 100%; height: auto; padding: 0; margin: 0;" src="https://s3.ap-south-1.amazonaws.com/static4.tagtaste.com/dashboard/images/mom3y87it1ibk4ydxs4g.png" alt="Happy Holi"></div>
+                                                                    <br>
+                                                                    <p style="font-size: 14px;color: #181818;margin:0px">
+                                                                        Dear {{$userName}}
+                                                                        <br>
+                                                                        <br>
+                                                                        May the splash of Holi colors brighten your life with happiness and prosperity. Happy Holi to you and your family.
+                                                                        <br>
+                                                                        <br>
+                                                                        Regards,
+                                                                        <br>
+                                                                        Team TagTaste
+                                                                    </p>
+                                                                </div>
+                                                            </td>
+                                                        </tr>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                                @include("emails.footer.download")
+                                @include("emails.footer.footer")
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</center>
+</body>
