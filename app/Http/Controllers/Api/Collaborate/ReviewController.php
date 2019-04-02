a559bb3694e10a4235c93b4ba6661ac6d6af935c
ashokTagtaste <ashok@tagtaste.com>
2019-04-02 12:27:46 +0530

    change

--- app/Http/Controllers/Api/Collaborate/ReviewController.php
+++ app/Http/Controllers/Api/Collaborate/ReviewController.php
@@ -86,6 +86,7 @@ class ReviewController extends Controller
                 $mandatoryQuestion = \DB::table('collaborate_tasting_questions')->where('collaborate_id',$collaborateId)
                     ->where('is_mandatory',1)->get();
                 $mandatoryQuestionsId = $mandatoryQuestion->pluck('id');
+                \Log::info($mandatoryQuestionsId);
                 $mandatoryReviewCount = \DB::table('collaborate_tasting_user_review')->where('collaborate_id',$collaborateId)->whereIn('question_id',$mandatoryQuestionsId)->where('batch_id',$batchId)->where('profile_id',$loggedInProfileId)->distinct('question_id')->get()->count();
                 \Log::info($mandatoryQuestion->count());
                 \Log::info("here");
