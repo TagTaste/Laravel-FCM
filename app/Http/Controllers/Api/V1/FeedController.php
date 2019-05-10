96ef619ce5ab32a5b2be15e038a2927aa9243924
ashokTagtaste <ashok@tagtaste.com>
2019-05-10 14:28:46 +0530

    change

--- app/Http/Controllers/Api/V1/FeedController.php
+++ app/Http/Controllers/Api/V1/FeedController.php
@@ -93,24 +93,6 @@ class FeedController extends Controller
 //            $this->errors[] = 'No more feeds';
 //            return;
 //        }
-        $position1 = rand(5,8);
-        $position2 = rand(15,20);
-        $followingCount = Redis::sCard("following:profile:".request()->user()->profile->id);
-        $suggestion = new SuggestionEngine();
-        $rand1 = rand(1,4);
-        $rand2 = rand(1,4);
-
-        if($followingCount <= 200)
-        {
-            $position1 = rand(3,6);
-            $rand1 = rand(1,4);
-            if($rand1 == $rand2)
-                $rand2 = $rand2 + 1 >= 4 ? 1 : $rand2 + 1;
-        }
-
-        if($rand1 == $rand2)
-            $rand2 = $rand2 + 1 >= 4 ? 1 : $rand2 + 1;
-
         $index = 0;
         foreach($payloads as $payload){
             $type = null;
@@ -135,23 +117,6 @@ class FeedController extends Controller
             }
             $data['type'] = $type;
             $this->model[] = $data;
-            $data = [];
-            if($index == $position1)
-            {
-                $data['item'] = $suggestion->suggestion[$rand1 - 1];
-                $data['type'] = 'suggestion';
-                $count = Redis::sCard('suggested:'.$data['item'].':'.request()->user()->profile->id);
-                if($count)
-                    $this->model[] = $data;
-            }
-            elseif ($index == $position2)
-            {
-                $data['item'] = $suggestion->suggestion[$rand2 - 1];
-                $data['type'] = 'suggestion';
-                $count = Redis::sCard('suggested:'.$data['item'].':'.request()->user()->profile->id);
-                if($count)
-                    $this->model[] = $data;
-            }
 
         }
     }
