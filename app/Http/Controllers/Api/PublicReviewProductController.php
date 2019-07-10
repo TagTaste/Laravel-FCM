9623ff5a4f3918b144c952dd078c24a4587ef790
TagTaste <tagtaste@Tushars-MacBook-Pro.local>
2019-07-10 14:13:38 +0530

    model

--- app/Http/Controllers/Api/PublicReviewProductController.php
+++ app/Http/Controllers/Api/PublicReviewProductController.php
@@ -330,7 +330,7 @@ class PublicReviewProductController extends Controller
                 $products = $this->model['product'];
                 $this->model = [];
                 foreach($products as $product){
-                    $this->logSlack($product);
+                    $product =  \App\PublicReviewProduct::where('id',$product['id'])->first();
                     $meta = $product->getMetaFor($profileId);
                     $this->model[] = ['product'=>$product,'meta'=>$meta];
                 }
