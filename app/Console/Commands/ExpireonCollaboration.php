911848c509419217ff267ef604a214f90cd0e825
TagTaste <tagtaste@Tushars-MacBook-Pro.local>
2019-07-31 15:07:46 +0530

    model

--- app/Console/Commands/ExpireonCollaboration.php
+++ app/Console/Commands/ExpireonCollaboration.php
@@ -111,27 +111,27 @@ class ExpireonCollaboration extends Command
 //            });
 
         //notify 2 days before expiry
-        \App\Collaborate::with([])->where('expires_on','>=',Carbon::now()->addDays(1)->toDateTimeString())
-            ->where('expires_on','<=',Carbon::now()->addDays(2)->toDateTimeString())->whereNull('deleted_at')->orderBy('id')->chunk(100,function($models){
-                foreach($models as $model){
-                    $companyId = $model->company_id;
-                    if(isset($companyId))
-                    {
-                        $profileIds = CompanyUser::where('company_id',$companyId)->get()->pluck('profile_id');
-                        foreach ($profileIds as $profileId)
-                        {
-                            $model->profile_id = $profileId;
-                            event(new \App\Events\Actions\Expire($model));
-                        }
-                    }
-                    else {
-                        event(new \App\Events\Actions\Expire($model));
-                    }
-
-                }
-
-
-            });
+//        \App\Collaborate::with([])->where('expires_on','>=',Carbon::now()->addDays(1)->toDateTimeString())
+//            ->where('expires_on','<=',Carbon::now()->addDays(2)->toDateTimeString())->whereNull('deleted_at')->orderBy('id')->chunk(100,function($models){
+//                foreach($models as $model){
+//                    $companyId = $model->company_id;
+//                    if(isset($companyId))
+//                    {
+//                        $profileIds = CompanyUser::where('company_id',$companyId)->get()->pluck('profile_id');
+//                        foreach ($profileIds as $profileId)
+//                        {
+//                            $model->profile_id = $profileId;
+//                            event(new \App\Events\Actions\Expire($model));
+//                        }
+//                    }
+//                    else {
+//                        event(new \App\Events\Actions\Expire($model));
+//                    }
+//
+//                }
+//
+//
+//            });
 
 
         //notify 8 days before
