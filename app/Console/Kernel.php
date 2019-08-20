f026b056d8ed530ca1237c3a8a2b9289f68dc204
TagTaste <tagtaste@Tushars-MacBook-Pro.local>
2019-08-20 12:28:44 +0530

    cron job applied

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -236,8 +236,8 @@ class Kernel extends ConsoleKernel
         $schedule->command('expires_on:polling')->dailyAt('12:00');
 
 
-        $schedule->command('backup:db')->withoutOverlapping(15)->dailyAt('00:00');
-        //$schedule->command('follow:company')->dailyAt('23:00');
+        //$schedule->command('backup:db')->withoutOverlapping(15)->dailyAt('00:00');
+        $schedule->command('follow:company')->weekly()->wednesdays()->at('00:00');;
         //$schedule->command('ServiceInterruptionMail')->dailyAt('18:20');
 
         //command for redis store suggestion
