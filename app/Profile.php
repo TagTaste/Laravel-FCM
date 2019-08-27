1e6d9507604f0df039c757a26ed6afacd1b6268a
tt-shubham <tt-shubham@MacBook-Air-3.local>
2019-08-27 13:01:41 +0530

    change in the allergens

--- app/Profile.php
+++ app/Profile.php
@@ -1330,7 +1330,7 @@ class Profile extends Model
 
     public function getAllergensAttribute()
     {
-        return \DB::table('allergens')->join('profiles_allergens','profiles_allergens.allergens_id','=','allergens.id')->where('profiles_allergens.profile_id',$this->id)->get(['name', 'description', 'image', 'allergens_id']);
+        return \DB::table('allergens')->join('profiles_allergens','profiles_allergens.allergens_id','=','allergens.id')->where('profiles_allergens.profile_id',$this->id)->get(['id', 'name', 'description', 'image']);
     }
 
     public function getTotalPostCountAttribute()
