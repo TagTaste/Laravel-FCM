b67433efe03db29e48d0975e6fc4e073057fa758
tt-shubham <tt-shubham@MacBook-Air-9.local>
2019-09-19 19:09:35 +0530

    change in the data

--- app/V2/Collaborate.php
+++ app/V2/Collaborate.php
@@ -15,42 +15,4 @@ class Collaborate extends BaseCollaborate
 
 	protected $visible = ["id", "title", "description", "profile_id", "company_id", "has_tags", "collaborate_type", "expires_on", "updated_at", "created_at", "deleted_at"];
 
-	public function getOwnerAttribute()
-    {
-    	$data = array();
-    	$owner = $this->owner();
-    	if (!is_null($this->profile_id)) {
-    		$keyRequired = [
-	            'id',
-	            'user_id',
-	            'name',
-	            'designation',
-	            'handle',
-	            'tagline',
-	            'image_meta',
-	            'isFollowing'
-	        ];
-	        $data = array_intersect_key(
-	            $owner->toArray(), 
-	            array_flip($keyRequired)
-	        );
-	        
-	        foreach ($data as $key => $value) {
-	            if (is_null($value) || $value == '')
-	                unset($data[$key]);
-	        }
-	        return $data;
-    	} else {
-    		if (!is_null($this->company_id)) {
-    			$data = [
-		            'id' => $owner->id,
-		            'profile_id' => $owner->profileId,
-		            'name' => $owner->name,
-		            'logo_meta' => $owner->logo_meta
-		        ];
-		        return $data;
-    		}
-    	}
-    	return $data;
-    }
 }
