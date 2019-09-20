e23fa825f2812f0a54c847196a619399fbd946cd
tt-shubham <tt-shubham@MacBook-Air-9.local>
2019-09-20 14:16:57 +0530

    changes in the collaborate, polling and product shared page

--- app/Shareable/Collaborate.php
+++ app/Shareable/Collaborate.php
@@ -64,7 +64,7 @@ class Collaborate extends Share
         $meta['likeCount'] = Redis::sCard($key);
         $meta['commentCount'] = $this->comments()->count();
         $collaborate = \App\Collaborate::where('id',$this->collaborate_id)->whereNull('deleted_at')->first();
-        $meta['original_post_meta'] = $collaborate->getMetaForV2($profileId);
+        $meta['originalPostMeta'] = $collaborate->getMetaForV2($profileId);
         return $meta;
     }
 
