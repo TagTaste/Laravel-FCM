1c845ee92dd34ef3fb879e918b6dba17195c4f9d
tt-shubham <tt-shubham@MacBook-Air-9.local>
2019-09-24 15:16:09 +0530

    changes in the feedtracker, feed detail pages

--- app/V2/Shoutout.php
+++ app/V2/Shoutout.php
@@ -19,35 +19,34 @@ class Shoutout extends BaseShoutout
     {
     	$data = array();
     	$owner = $this->owner();
-    	if (!is_null($this->profile_id)) {
-    		$keyRequired = [
-	            'id',
-	            'user_id',
-	            'name',
-	            'designation',
-	            'handle',
-	            'tagline',
-	            'image_meta',
-	            'isFollowing'
+    	if (!is_null($this->company_id)) {
+    		$data = [
+	            'id' => $owner->id,
+	            'profile_id' => $owner->profileId,
+	            'name' => $owner->name,
+	            'logo_meta' => $owner->logo_meta
 	        ];
-	        $data = array_intersect_key(
-	            $owner->toArray(), 
-	            array_flip($keyRequired)
-	        );
-	        
-	        foreach ($data as $key => $value) {
-	            if (is_null($value) || $value == '')
-	                unset($data[$key]);
-	        }
 	        return $data;
     	} else {
-    		if (!is_null($this->company_id)) {
-    			$data = [
-		            'id' => $owner->id,
-		            'profile_id' => $owner->profileId,
-		            'name' => $owner->name,
-		            'logo_meta' => $owner->logo_meta
+    		if (!is_null($this->profile_id)) {
+    			$keyRequired = [
+		            'id',
+		            'user_id',
+		            'name',
+		            'designation',
+		            'handle',
+		            'tagline',
+		            'image_meta',
+		            'isFollowing'
 		        ];
+		        $data = array_intersect_key(
+		            $owner->toArray(), 
+		            array_flip($keyRequired)
+		        );
+		        foreach ($data as $key => $value) {
+		            if (is_null($value) || $value == '')
+		                unset($data[$key]);
+		        }
 		        return $data;
     		}
     	}
