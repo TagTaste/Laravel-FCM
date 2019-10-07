e2af682c365c3cbc397d603fd973d247c0b19ed0
TagTaste <tagtaste@Tushars-MacBook-Pro.local>
2019-10-07 15:46:43 +0530

    new model

---
+++ database/migrations/2019_10_07_154323_alter_table_public_user_review.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTablePublicUserReview extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('public_product_user_review', function (Blueprint $table){
+           $table->integer('option_type')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('public_product_user_review', function (Blueprint $table){
+            $table->dropColumn(['option_type']);
+        });
+    }
+}
