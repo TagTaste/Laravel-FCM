b8e7e24319a31fd65783f8d2144a586a7600e0fc
TagTaste <tagtaste@Tushars-MacBook-Pro.local>
2019-10-07 16:58:47 +0530

    model

--- app/Http/Controllers/Api/PublicReviewProduct/ReportController.php
+++ app/Http/Controllers/Api/PublicReviewProduct/ReportController.php
@@ -322,7 +322,7 @@ class ReportController extends Controller
                         } else if ($answer->option_type == 2) {
                             $answer->value = 'None';
                         } else {
-                            $answer->value = \DB::table('public_product_user_review')->select('value')->where('id',$answer->id)->first().value;
+                            $answer->value = \DB::table('public_product_user_review')->select('value')->where('id',$answer->id)->first()['value'];
                         }
                         $value = [];
                         if(isset($data->questions->is_nested_option) && $data->questions->is_nested_option == 1 && isset($data->questions->intensity_value) && isset($answer->intensity))
