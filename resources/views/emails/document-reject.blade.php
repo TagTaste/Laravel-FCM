80bdbde001150786d8ddefa6ac8485721c18fe3b
tt-shubham <tt-shubham@tt-shubham.local>
2019-11-07 17:33:55 +0530

    changes in the document reject

--- resources/views/emails/document-reject.blade.php
+++ resources/views/emails/document-reject.blade.php
@@ -14,7 +14,7 @@
                                 <tr>
                                     <td align="center" bgcolor="#FFFFFF" style="padding:20px 0px 20px 0px;border-radius: 10px 10px 0 0;width:100%!important;" width="100%">
                                         <div style="font-size: 20px;font-weight: bold;color: #181818;padding: 0 40px 0 40px">
-                                            Documents do not match our Criteria Collaboration: {{$data->title}}
+                                            Documents do not match our Criteria
                                         </div>
                                     </td>
                                 </tr>
