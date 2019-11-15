30ecf972d8dfc83138033dc67f15ee3e8c52e016
TagTaste <tagtaste@Tushars-MacBook-Pro.local>
2019-11-15 15:04:43 +0530

    new mdoel

--- app/Http/Controllers/Api/PublicReviewProduct/ReportController.php
+++ app/Http/Controllers/Api/PublicReviewProduct/ReportController.php
@@ -45,7 +45,7 @@ class ReportController extends Controller
             return $this->sendError("Product is not available");
         }
         $count = Review::where('product_id',$productId)->where('current_status',2)->distinct('profile_id')->count('profile_id');
-        if($count < 1)
+        if($count < 10)
         {
             $this->model = [];
             $this->model['title'] = 'Sensogram';
