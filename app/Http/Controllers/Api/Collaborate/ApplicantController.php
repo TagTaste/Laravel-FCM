85c2028889328dcd247c256d6867b8d71791e127
TagTaste <tagtaste@Tushars-MacBook-Pro.local>
2020-03-22 15:51:46 +0530

    new model

--- app/Http/Controllers/Api/Collaborate/ApplicantController.php
+++ app/Http/Controllers/Api/Collaborate/ApplicantController.php
@@ -489,9 +489,9 @@ class ApplicantController extends Controller
         $page = $request->input('page');
         list($skip,$take) = \App\Strategies\Paginator::paginate($page);
         $this->model = [];
-        $this->model['rejectedApplicantsCount'] = Collaborate\Applicant::where('collaborate_id',$collaborateId)->whereNull('shortlisted_at')
+        $this->model['rejectedApplicantsCount'] = Collaborate\Applicant::where('collaborate_id',$collaborateId)
             ->whereNotNull('rejected_at')->count();
-        $this->model['rejectedApplicantList'] = Collaborate\Applicant::where('collaborate_id',$collaborateId)->whereNull('shortlisted_at')
+        $this->model['rejectedApplicantList'] = Collaborate\Applicant::where('collaborate_id',$collaborateId)
             ->whereNotNull('rejected_at')->skip($skip)->take($take)->get();
 
         return $this->sendResponse();
