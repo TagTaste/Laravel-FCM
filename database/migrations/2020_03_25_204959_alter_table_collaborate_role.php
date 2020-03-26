3f8178172ab30ee8d62d4777c4f8b4f8a0931ac0
TagTaste <tagtaste@Tushars-MacBook-Pro.local>
2020-03-26 20:01:04 +0530

    new model

---
+++ database/migrations/2020_03_25_204959_alter_table_collaborate_role.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableCollaborateRole extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('collaborate_role', function(Blueprint $table){
+            $table->string('can_action');
+         });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('collaborate_role', function (Blueprint $table){
+            $table->dropColumn(['can_action']);
+        });
+    }
+}
