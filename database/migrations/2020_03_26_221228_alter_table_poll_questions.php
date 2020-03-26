95529f39bde01be6f7ce86d2f41fcd33f0103ba7
TagTaste <tagtaste@Tushars-MacBook-Pro.local>
2020-03-26 22:35:29 +0530

    migrations created

---
+++ database/migrations/2020_03_26_221228_alter_table_poll_questions.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTablePollQuestions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::alter('poll_question',   function( Blueprint $table ) {
+            $table->json('images')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::alter('poll_question',   function( Blueprint $table ) {
+            $table->dropColumn(['images']);
+        });
+    }
+}
