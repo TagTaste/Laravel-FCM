e960a533855b94391eb348dfb3b555f461f2130d
tt-shubham <tt-shubham@tt-shubham.local>
2020-03-26 16:39:32 +0530

    change in the merge with master

--- config/database.php
+++ config/database.php
@@ -68,8 +68,7 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_general_ci',
             'prefix' => '',
-            // 'strict' => true,
-            'strict' => false,
+            'strict' => true,
             'engine' => null,
         ],
 
