2f1caf0434dbdcbe6335599bbe891e9f1a5997d5
TagTaste <tagtaste@Tushars-MacBook-Pro.local>
2020-05-25 17:08:31 +0530

    new model

--- app/V2/Detailed/Collaborate.php
+++ app/V2/Detailed/Collaborate.php
@@ -13,11 +13,7 @@ class Collaborate extends BaseCollaborate
 {
 	use IdentifiesOwner, GetTags, HasPreviewContent;
 
-<<<<<<< HEAD
-	protected $visible = ['id','title','expires_on','profile_id','company_id','created_at','updated_at','deleted_at','video','location','privacy_id','description','duration','file1','state','step','type','type_id','is_taster_residence','collaborate_type','brand_name','brand_logo','methodology_id','no_of_batches','global_question_id','images_meta','applicationCount','type','product_review_meta','tasting_methodology','profile','company','categories','addresses','collaborate_specializations','collaborate_allergens','description_updated', 'document_required', 'financials','financial_min','financial_max','is_contest'];
-=======
 	protected $visible = ['id','title','expires_on','profile_id','company_id','created_at','updated_at','deleted_at','video','location','privacy_id','description','duration','file1','state','step','type','type_id','is_taster_residence','collaborate_type','brand_name','brand_logo','methodology_id','no_of_batches','global_question_id','images_meta','applicationCount','type','product_review_meta','tasting_methodology','profile','company','categories','addresses','collaborate_specializations','collaborate_allergens','description_updated', 'document_required', 'financials','financial_min','financial_max','track_consistency','is_contest'];
->>>>>>> a4d1b8359cf4791b8a1cff7c93906cf632dbe8b2
 
     protected $appends = ['description_updated', 'applicationCount', 'product_review_meta', 'type','tasting_methodology','addresses'];
 
