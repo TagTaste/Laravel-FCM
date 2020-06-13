59c6cb76003af97b3121d9aa453607b971bfb92e
Harsh Arora <arora.harsh@yahoo.com>
2020-06-13 17:06:39 +0530

    Updated email

---
+++ resources/views/emails/cranberry-update-email-2.blade.php
@@ -0,0 +1,73 @@
+@include("emails.header.v1.definition")
+<body style="margin: 0; padding: 0;">
+<center>
+    <table width="100%" border="0" cellspacing="0" cellpadding="0" style="font-family:Arial" bgcolor="#F8F6F9">
+        @include("emails.header.v1.header")
+        <tr>
+            <td align="center" valign="top">
+                <table class="container" width="620" align="center" border="0" cellspacing="0" cellpadding="0"
+                       border-collapse="collapse">
+                    <tr>
+                        <td align="center" width="100%" bgcolor="#F8F6F9">
+                            <table width="550" align="center" border="0" cellspacing="0" cellpadding="0"
+                                   border-collapse="collapse">
+                                <tr>
+                                    <td align="center" bgcolor="#FFFFFF" style="padding:20px 0 0;border-radius: 4px 4px 0 0;width:100%!important;" width="100%">
+                                        <div style="font-size: 24px;font-weight: normal;color: #171717;padding: 0 40px 0 40px">
+                                            Welcome to the Pit Stop 2, please download the attachment to access the material.
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="width:100%!important;background-color:#FFFFFF!important;padding: 0px 40px 0px 40px;border-bottom: 1px solid rgba(0, 0, 0, 0.04);">
+                                        <table style="margin: 20px 0px 20px 0px;" width="100%" bgcolor="#FFFFFF" style="border-bottom: 1px solid rgba(0, 0, 0, 0.04);">
+                                            <tr>
+                                                <td align="center" valign="top" width="100%">
+                                                    <table border="0" cellpadding="0" cellspacing="0" width="100%" bgcolor="#FFFFFF">
+                                                        <tr>
+                                                            <td>
+                                                                <div>
+                                                                    <div style="width:100%; text-align: center;"><img style="max-width: 100%; height: auto; padding: 0; margin: 0;" src="https://s3.ap-south-1.amazonaws.com/static3.tagtaste.com/dashboard/images/z9lk5jo8vuhu4p2rycvn2e.png" /></div>
+                                                                </div>
+                                                            </td>
+                                                        </tr>
+
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="width:100%!important;background-color:#FFFFFF!important;padding: 0px 40px 0px 40px;border-bottom: 1px solid rgba(0, 0, 0, 0.04);">
+                                        <table style="margin: 20px 0px 20px 0px;" width="100%" bgcolor="#FFFFFF" style="border-bottom: 1px solid rgba(0, 0, 0, 0.04);">
+                                            <tr>
+                                                <td align="center" valign="top" width="100%">
+                                                    <table border="0" cellpadding="0" cellspacing="0" width="100%" bgcolor="#FFFFFF">
+                                                        <tr>
+                                                            <td>
+                                                                <div style="font-size: 16px;">
+                                                                    <p>Hi {{$userName}},</p>
+                                                                    <p>Welcome to the Pit Stop 2, please download the attachment to access the material.</p>
+                                                                    <p>Regards,</p>
+                                                                    <p>Team TagTaste</p>
+                                                                </div>
+                                                            </td>
+                                                        </tr>
+
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                                @include("emails.footer.v1.footer")
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</center>
+</body>
