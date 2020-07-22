08cf6a32f720462d6e8c01d7f268afc925759997
tt-shubham <tt-shubham@tt-shubham.local>
2020-07-22 19:23:51 +0530

    chnages

--- config/database.php
+++ config/database.php
@@ -68,7 +68,7 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_general_ci',
             'prefix' => '',
-            'strict' => false,
+            'strict' => true,
             'engine' => null,
         ],
 
