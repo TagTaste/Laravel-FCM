77ea732e7e5b74e1977f35c50ef95a5c295ff431
tt-shubham <tt-shubham@tt-shubham.local>
2020-07-22 13:17:25 +0530

    changes in the premium profile

--- app/Recipe/Profile.php
+++ app/Recipe/Profile.php
@@ -17,7 +17,7 @@ class Profile extends BaseProfile
     protected $with = [];
 
     protected $visible = ['id','name', 'designation','imageUrl','tagline','about','handle','city','expertise','user_id',
-        'keywords','image','isFollowing','ageRange','gender',"image_meta","hero_image_meta",'is_ttfb_user','verified','is_tasting_expert','phone','tasting_instructions'];
+        'keywords','image','isFollowing','ageRange','gender',"image_meta","hero_image_meta",'is_ttfb_user','verified','is_tasting_expert','phone','tasting_instructions','is_premium'];
 
     protected $appends = ['name','designation','imageUrl','ageRange'];
     
