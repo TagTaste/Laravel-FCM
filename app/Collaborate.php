fabeae11aa2d3aee5153782c0ae278d945c91a8b
TagTaste <tagtaste@Tushars-MacBook-Pro.local>
2020-08-12 15:30:48 +0530

    new model

--- app/Collaborate.php
+++ app/Collaborate.php
@@ -43,10 +43,10 @@ class Collaborate extends Model implements Feedable
         'step','financial_min','financial_max','type','type_id','addresses','collaborate_type',
         'is_taster_residence','product_review_meta','methodology_id','age_group','gender_ratio',
         'no_of_expert','no_of_veterans','is_product_endorsement','tasting_methodology','collaborate_occupations','collaborate_specializations',
-        'brand_name','brand_logo','no_of_batches','collaborate_allergens','global_question_id','taster_instruction','images_meta','owner','document_required','track_consistency','is_contest','max_submissions'];
+        'brand_name','brand_logo','no_of_batches','collaborate_allergens','global_question_id','taster_instruction','images_meta','owner','document_required','track_consistency','is_contest','max_submissions','mandatory_fields'];
 
 
-    protected $appends = ['applicationCount','type','product_review_meta','tasting_methodology','owner','addresses'];
+    protected $appends = ['applicationCount','type','product_review_meta','tasting_methodology','owner','addresses','mandatory_fields'];
 
     protected $casts = [
         'privacy_id' => 'integer',
@@ -651,6 +651,14 @@ class Collaborate extends Model implements Feedable
     public function getGenderRatioAttribute($value)
     {
         return !is_null($value) ? json_decode($value) : null;
+    }
+
+    public function getMandatoryFieldsAttribute()
+    {
+        return \DB::table('mandatory_fields')
+                ->join('collaborate_mandatory_mapping','mandatory_fields.id','=','collaborate_mandatory_mapping.mandatory_field_id')
+                ->where('collaborate_mandatory_mapping.collaborate_id',$this->id)
+                ->get();
     }
 
 //    public function getOwnerAttribute()
