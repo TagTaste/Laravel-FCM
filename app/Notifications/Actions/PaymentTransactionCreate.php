71e55fca808e97336ef023c363c8137bef79200d
Hussein Shaikh <hussein.nikk@yahoo.in>
2021-08-12 22:57:21 +0530

    push notification

--- app/Notifications/Actions/PaymentTransactionCreate.php
+++ app/Notifications/Actions/PaymentTransactionCreate.php
@@ -26,7 +26,7 @@ class PaymentTransactionCreate extends Action
         if(!is_null($this->data->content)) {
             $this->allData['message'] = ['id' => null,'image'=>null,'content'=>$this->data->content];
         }
-        ECHO $this->notification = $this->sub;
+        $this->notification = $this->sub;
 
     }
 
