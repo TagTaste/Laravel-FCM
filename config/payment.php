322801808fab08d02ab4d2af5596a12c39185ecc
hussein-c <hussein-c@everledger.io>
2021-12-28 12:54:41 +0530

    fixe

--- config/payment.php
+++ config/payment.php
@@ -8,8 +8,10 @@ return  [
     "PAYTM_GUID_TASTING"=>env("PAYTM_GUID_TASTING","c96d6228-4f7c-11eb-b4be-fa163e429e83"),
     "PAYTM_ENDPOINT" => env("PAYTM_ENDPOINT","https://staging-dashboard.paytm.com"),
     "PAYTM_CALLBACK_URL"=>env("PAYTM_CALLBACK_URL","https://apidev.tagtaste.com/api/link/status/callback"),
-    "CASHFREE_CLIENT_ID" => env("CASHFREE_CLIENT_ID","CF115943C70QPEOSN97UHOLPCFBG"),
-    "CASHFREE_SECRET_ID"=>env("CASHFREE_SECRET_ID","13e99c462909d19a8ab00cdeccea3c930acf7de0"),
+
+
+    "CASHFREE_CLIENT_ID" => env("CASHFREE_CLIENT_ID","CF114964C710418SN97UHOLPCFDG"),
+    "CASHFREE_SECRET_ID"=>env("CASHFREE_SECRET_ID","9c357596c821b197f674b0b90658df66dc545751"),
     "CASHFREE_ENDPOINT" => env("CASHFREE_ENDPOINT","https://payout-gamma.cashfree.com"),
     "CASHFREE_CALLBACK_URL"=>env("CASHFREE_CALLBACK_URL","https://apidev.tagtaste.com/api/link/status/callback"),
 
