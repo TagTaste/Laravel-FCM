4338ea84d87ab25c165ce1986a6d0082d43c0c81
hussein-c <hussein-c@everledger.io>
2022-01-10 16:07:33 +0530

    rollback notification event listner added

---
+++ app/Notifications/Actions/RollbackTaster.php
@@ -0,0 +1,79 @@
+<?php
+
+
+namespace App\Notifications\Actions;
+
+use App\Notifications\Action;
+
+use App\FCMPush;
+use Carbon\Carbon;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class RollbackTaster extends Action
+{
+    public $view;
+    public $sub;
+    public $notification ;
+    public $batchInfo;
+
+    public function __construct($event)
+    {
+        parent::__construct($event);
+        $this->batchInfo = $event->batchInfo;
+        // $this->view = 'emails.begintasting';
+
+        $this->sub = htmlspecialchars_decode($this->data->who['name']) ." has rollbacked the product (".$event->batchInfo->name.") for now";
+        if(!is_null($this->data->content)) {
+            $this->allData['message'] = ['id' => null,'image'=>null,'content'=>$this->data->content];
+
+        }
+        $this->notification = $this->sub;
+
+    }
+
+    public function via($notifiable)
+    {
+        $via = ['database',FCMPush::class];
+
+        if($this->view && view()->exists($this->view)){
+            $via[] = 'mail';
+        }
+        return $via;
+    }
+
+    public function toMail($notifiable)
+    {
+        if(view()->exists($this->view)){
+            return (new MailMessage())->subject($this->sub)->view(
+                $this->view, ['data' => $this->data,'model'=>$this->allData,'notifiable'=>$notifiable,
+                    'content'=>$this->getContent($this->allData['content']),'batchInfo'=>$this->batchInfo]
+            );
+        }
+    }
+
+    public function toArray($notifiable)
+    {
+        $data = [
+            'action' => $this->data->action,
+            'profile' => isset(request()->user()->profile) ? request()->user()->profile : $this->data->who,
+            'notification' => $this->notification,
+            'batchInfo'=>$this->batchInfo
+        ];
+
+        if(method_exists($this->model,'getNotificationContent')){
+            $data['model'] = $this->allData;
+        } else {
+            \Log::warning(class_basename($this->modelName) . " doesn't specify notification content.");
+            $data['model'] = [
+                'name' => $this->modelName,
+                'id' => $this->data->model->id,
+                'content' => $this->data->content,
+                'image' => $this->data->image
+            ];
+        }
+        $data['created_at'] = Carbon::now()->toDateTimeString();
+
+        return $data;
+    }
+
+}
