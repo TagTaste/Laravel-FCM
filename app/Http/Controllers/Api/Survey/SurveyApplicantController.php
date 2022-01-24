3f0bb83578c553acfe4ca7c9ca871cc72ed9915d
Nikhil Bharti <nikhilbharti@Nikhils-MacBook-Air.local>
2022-01-24 19:35:38 +0530

    redis issue resolved

--- app/Http/Controllers/Api/Survey/SurveyApplicantController.php
+++ app/Http/Controllers/Api/Survey/SurveyApplicantController.php
@@ -194,16 +194,17 @@ class SurveyApplicantController extends Controller
                     return $this->sendError("Cannot begin survey for specified user");
                 }
             }
-
+            
             $currentStatus = Redis::get("surveys:application_status:$id:profile:$profileId");
             if ($currentStatus == 0) {
-                Redis::set("surveys:application_status:$id:profile:$profileId", 1);
                 $b = surveyApplicants::where("profile_id", $profileId)->where('survey_id', $id)->where('application_status', 0)->update(["application_status" => 1]);
                 if ($b == false) {
                     $this->model = false;
+                }else{
+                    Redis::set("surveys:application_status:$id:profile:$profileId", 1);
                 }
             }
-            
+
             $who = null;
             if ($checkIFExists->company_id) {
                 $company = Company::where('id', $checkIFExists->company_id)->first();
