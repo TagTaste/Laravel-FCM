c1a67fe120e8313998561381cb759b1db0ad37d1
hussein <hussein.nikk@yahoo.in>
2022-01-25 21:56:35 +0530

    new changes fixes for permission

--- app/Http/Controllers/Api/Survey/SurveyController.php
+++ app/Http/Controllers/Api/Survey/SurveyController.php
@@ -302,7 +302,7 @@ class SurveyController extends Controller
             if (!empty($getCompanyId)) {
                 $company = Company::find($getCompanyId);
                 $userBelongsToCompany = $company->checkCompanyUser($request->user()->id);
-                if ($userBelongsToCompany && $request->$company->is_premium == 1) {
+                if ($userBelongsToCompany && $company->is_premium == 1) {
                     return true;
                     // return $next($request);
                 }
