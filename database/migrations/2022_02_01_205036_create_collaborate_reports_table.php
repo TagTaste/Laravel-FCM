f70825384674c00a809d5a18e5188ecfeddfa607
Nikhil Bharti <nikhilbharti@Nikhils-MacBook-Air.local>
2022-02-01 21:14:33 +0530

    migration added for collaborate reports

---
+++ database/migrations/2022_02_01_205036_create_collaborate_reports_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCollaborateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('collaborate_reports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('collaborate_id')->unsigned();
+            $table->foreign('collaborate_id')->references('id')->on('collaborates');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('link')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('collaborate_reports');
+    }
+}
