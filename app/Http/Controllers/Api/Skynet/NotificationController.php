7890c7bbac541d0b81995a3040505278880c02d2
Nikhil Bharti <nikhilbharti@Nikhils-MacBook-Air.local>
2022-02-24 08:08:30 +0530

    notification report upload

--- app/Http/Controllers/Api/Skynet/NotificationController.php
+++ app/Http/Controllers/Api/Skynet/NotificationController.php
@@ -24,28 +24,30 @@ class NotificationController extends Controller
     public function notifyUsers(Request $request)
     {
         $data = $request->input();
-        $collaborate = Collaborate::where('id',$data["collaborate_id"])->first();
-        if ($collaborate === null) {
-            return $this->sendResponse(false);
-        }
-        
-        if (isset($collaborate->company_id)&& (!is_null($collaborate->company_id))) {
-            $company = Redis::get('company:small:' . $collaborate->company_id);
-            $company = json_decode($company);
-            $profileIds = CompanyUser::where('company_id',$collaborate->company_id)->get()->pluck('profile_id');
-            foreach ($profileIds as $profileId) {
-                $collaborate->profile_id = $profileId;
-                file_put_contents(storage_path("logs") . "/skynet_test.txt", "\nHere profile id ". $profileId. "\n", FILE_APPEND);
+        if($data["type"] == "report_upload" || $data["type"] == "report_removed"){
+            $collaborate = Collaborate::where('id',$data["collaborate_id"])->first();
+            if ($collaborate === null) {
+                return $this->sendResponse(false);
+            }
+            $collaborate->report_link = $data["report_link"] ?? "";
+            
+            if($data["type"] == "report_upload")
+                $data["notification"] = "TagTaste has added a report to your collaboration ";
+            else if($data["type"] == "report_removed")
+                $data["notification"] = "A report has been removed from your collaboration ";
+
+            if (isset($collaborate->company_id)&& (!is_null($collaborate->company_id))) {
+                $company = Redis::get('company:small:' . $collaborate->company_id);
+                $company = json_decode($company);
+                $profileIds = CompanyUser::where('company_id',$collaborate->company_id)->get()->pluck('profile_id');
+                foreach ($profileIds as $profileId) {
+                    $collaborate->profile_id = $profileId;
+                    event(new \App\Events\CollaborationReportUpload($collaborate, $data["notification"], $data["mode"]));
+                }
+            } else {
                 event(new \App\Events\CollaborationReportUpload($collaborate, $data["notification"], $data["mode"]));
             }
-        } else {
-            file_put_contents(storage_path("logs") . "/skynet_test.txt", "\nHere profile id ". $collaborate->profile_id. "\n", FILE_APPEND);
-            event(new \App\Events\CollaborationReportUpload($collaborate, $data["notification"], $data["mode"]));
-            // event(new \App\Events\Actions\Apply($collaborate, $request->user()->profile, $request->input("message","")));
         }
-        
-
-        file_put_contents(storage_path("logs") . "/skynet_test.txt", "\nTrying to push notification", FILE_APPEND);
         return $this->sendResponse(true);
     }
 }
