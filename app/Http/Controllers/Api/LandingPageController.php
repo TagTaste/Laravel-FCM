5c2759ee5cc824f88d0cdf73806150f8674e2ca5
v-Hussein <v-hussein@tagtaste.com>
2022-04-05 11:37:25 +0530

    error format modified

--- app/Http/Controllers/Api/LandingPageController.php
+++ app/Http/Controllers/Api/LandingPageController.php
@@ -27,7 +27,7 @@ class LandingPageController extends Controller
      *
      * @return Response
      */
-    
+
     public function quickLinks()
     {
 
@@ -86,30 +86,66 @@ class LandingPageController extends Controller
         return $this->sendResponse();
     }
 
+    protected function validatePayloadForVersion($request)
+    {
+        if (($request->header('x-version') != null
+                && $request->header('x-version') < 80) ||
+            ($request->header('x-version-ios') != null
+                && version_compare("4.2.7", $request->header('x-version-ios'), ">"))
+        ) {
+            $pollPayloadIds = $this->getNewVersionOfPollPayloads();
+            $this->modelNotIncluded = array_merge($this->modelNotIncluded, $pollPayloadIds);
+        }
+    }
+
+    protected function getNewVersionOfPollPayloads()
+    {
+        $pollPayloadsWithImage = \App\Polling::where('type', '!=', 3)
+            ->pluck('payload_id')->toArray();
+        $sharedPollWithImage = \App\Polling::join('polling_shares', 'polling_shares.poll_id', '=', 'poll_questions.id')
+            ->where('type', '!=', 3)
+            ->pluck('polling_shares.payload_id')
+            ->toArray();
+        //      return array_merge($pollPayloadsWithImage,$pollPayloadWithOptionImage);
+        return  Payload::whereIn('id', array_merge($pollPayloadsWithImage, $sharedPollWithImage))->pluck('channel_payloads.id')->toArray();
+    }
+
+    protected function removeReportedPayloads($profileId)
+    {
+        $reported_payload = Payload::leftJoin('report_content', 'report_content.payload_id', '=', 'channel_payloads.id')
+            ->where('report_content.profile_id', $profileId)
+            ->pluck('channel_payloads.id')->toArray();
+        $this->modelNotIncluded = array_merge($this->modelNotIncluded, $reported_payload);
+    }
+    
     public function feed(Request $request)
     {
+        $this->errors['status'] = 0;
         $limit = 0;
         $limit = $request->input('limit');
-        if(!$limit)
-        {
-            $limit =20;
+        if (!$limit) {
+            $limit = 20;
         }
         $profileId = $request->user()->profile->id;
-        $payloads = Payload::join('subscribers','subscribers.channel_name','=','channel_payloads.channel_name')
-            ->where('subscribers.profile_id',$profileId)
+        $this->validatePayloadForVersion($request);
+        $this->removeReportedPayloads($profileId);
+        $payloads =  Payload::join('subscribers', 'subscribers.channel_name', '=', 'channel_payloads.channel_name')
+            ->where('subscribers.profile_id', $profileId)
+            ->whereNull('subscribers.deleted_at')
+            ->whereNotIn('channel_payloads.id', $this->modelNotIncluded)
             //Query Builder's where clause doesn't work here for some reason.
             //Don't remove this where query.
             //Ofcourse, unless you know what you are doing.
-//            ->whereRaw(\DB::raw('channel_payloads.created_at >= subscribers.created_at'))
-            ->orderBy('channel_payloads.created_at','desc')
+            //            ->whereRaw(\DB::raw('channel_payloads.created_at >= subscribers.created_at'))
+            ->orderBy('channel_payloads.created_at', 'desc')
             ->skip(0)
             ->take($limit)
             ->get();
-        if($payloads->count() === 0){
+        if ($payloads->count() === 0) {
             $this->errors[] = 'No more feed';
             return $this->sendResponse();
         }
-        $this->getMeta($payloads,$profileId);
+        $this->getMeta($payloads, $profileId);
         return $this->sendResponse();
     }
 
@@ -121,44 +157,44 @@ class LandingPageController extends Controller
 
     private function getType($modelName)
     {
-        $exploded = explode('\\',$modelName);
+        $exploded = explode('\\', $modelName);
         return strtolower(end($exploded));
     }
     private function getMeta(&$payloads, &$profileId)
     {
 
-        foreach($payloads as $payload){
+        foreach ($payloads as $payload) {
             $type = null;
             $data = [];
 
             $cached = json_decode($payload->payload, true);
 
-            foreach($cached as $name => $key){
+            foreach ($cached as $name => $key) {
                 $cachedData = Redis::get($key);
-                if(!$cachedData){
+                if (!$cachedData) {
                     \Log::warning("could not get from $key");
                 }
-                $data[$name] = json_decode($cachedData,true);
+                $data[$name] = json_decode($cachedData, true);
             }
-            
-            if($payload->model !== null){
+
+            if ($payload->model !== null) {
                 $model = $payload->model;
                 $type = $this->getType($payload->model);
-                if($model=='App\Surveys'){
-                    $model = $model::with([])->where('payload_id',$payload->id)->where('state','=',config("constant.SURVEY_STATES.PUBLISHED"))->first();
-                }else{
-                    $model = $model::with([])->where('id',$payload->model_id)->first();
+                if ($model == 'App\Surveys') {
+                    $model = $model::with([])->where('payload_id', $payload->id)->where('state', '=', config("constant.SURVEY_STATES.PUBLISHED"))->first();
+                } else {
+                    $model = $model::with([])->where('id', $payload->model_id)->first();
                 }
 
                 if ($model != null && $type == "surveys") {
                     $data["surveys"]["totalApplicants"] = $this->getSurveyApplicantCount($model);
                 }
-                
-                if($model !== null && method_exists($model, 'getMetaFor') && $profileId != null){
+
+                if ($model !== null && method_exists($model, 'getMetaFor') && $profileId != null) {
                     $data['meta'] = $model->getMetaFor($profileId);;
                 }
             }
-            $data['type'] = $type;  
+            $data['type'] = $type;
             $this->model[] = $data;
         }
     }
