e467391e2e2788a35ba7d072474648a1eb2ddfab
v-Hussein <v-hussein@tagtaste.com>
2022-05-13 21:26:18 +0530

    final

--- app/Http/Controllers/Api/Survey/SurveyApplicantController.php
+++ app/Http/Controllers/Api/Survey/SurveyApplicantController.php
@@ -350,7 +350,7 @@ class SurveyApplicantController extends Controller
         $sensoryTrained = ["Yes", "No"];
         $superTaster = ["SuperTaster", "Normal"];
         $applicants = \DB::table('survey_applicants')->where('survey_id', $id)->get();
-        $applicationStatus = ['TO BE NOTIFIED','NOTIFIED','COMPLETED'];
+        $applicationStatus = ['TO BE NOTIFIED','NOTIFIED','COMPLETED','REJECTED'];
         $city = [];
         $profile = [];
         $hometown = [];
@@ -650,7 +650,7 @@ class SurveyApplicantController extends Controller
         return $this->sendResponse();
     }
 
-    public function shortlistPeople(Request $request, $id)
+    public function shortlistApplicant(Request $request, $id)
     {
         $survey = $this->model->where("id", "=", $id)->whereNull("deleted_at")->where(function ($q) {
             $q->orWhere('state', "!=", config("constant.SURVEY_STATES.CLOSED"));
--- routes/api.php
+++ routes/api.php
@@ -917,9 +917,9 @@ Route::group(['namespace' => 'Api', 'as' => 'api.'], function () {
         Route::post('/{id}/inviteForReview', 'SurveyApplicantController@inviteForReview')->middleware('manage.permission'); 
         Route::get('/{id}/applicantFilters', 'SurveyApplicantController@applicantFilters')->middleware('manage.permission'); 
         Route::get('/{id}/applicants/export', 'SurveyApplicantController@export')->middleware('manage.permission'); 
-        Route::post('/{id}/rejectApplicant', 'SurveyApplicantController@rejectPeople')->middleware('manage.permission');
+        Route::post('/{id}/rejectApplicant', 'SurveyApplicantController@rejectApplicant')->middleware('manage.permission');
         Route::get("/{id}/getRejectApplicants", "SurveyApplicantController@getRejectApplicants"); //->middleware('permissionCollaborate');
-        Route::post('/{id}/shortlistPeople', 'SurveyApplicantController@shortlistPeople')->middleware('manage.permission');
+        Route::post('/{id}/shortlistApplicant', 'SurveyApplicantController@shortlistApplicant')->middleware('manage.permission');
         Route::get("/{id}/getShortlistApplicants", "SurveyApplicantController@getShortlistApplicants");
 
         
