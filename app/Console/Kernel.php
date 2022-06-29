91ed5ea8edcc7d9cb9fc1f2a353b2a15442b7981
himani <v-himani@tagtaste.com>
2022-06-29 16:31:23 +0530

    neo4j code modified

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -311,7 +311,9 @@ class Kernel extends ConsoleKernel
         //quiz expiry
         \App\Console\Commands\ExpireQuiz::class,
         \App\Console\Commands\Build\Cache\Quiz::class,
-        \App\Console\Commands\Build\Search\Quiz::class
+        \App\Console\Commands\Build\Search\Quiz::class,
+        \App\Console\Commands\Build\Graph\Build\UserQuiz::class
+
 
 
 
