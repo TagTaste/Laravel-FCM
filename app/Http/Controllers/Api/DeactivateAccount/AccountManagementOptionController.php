4cba2ff52bb17bdd01108cf38058a62868c4c557
Nikhil Bharti <nikhilbharti@Nikhils-MacBook-Air.local>
2022-07-06 08:18:54 +0530

    issues solved

--- app/Http/Controllers/Api/DeactivateAccount/AccountManagementOptionController.php
+++ app/Http/Controllers/Api/DeactivateAccount/AccountManagementOptionController.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Mail;
 use Tagtaste\Api\SendsJsonResponse;
 use Carbon\Carbon;
 use PHPUnit\Util\Json;
+use App\CompanyUser;
+
 
 use function Clue\StreamFilter\append;
 
@@ -37,7 +39,7 @@ class AccountManagementOptionController extends Controller
         
         $data = [];
         
-        $overvire_obj = ['title'=>$request->user()->profile->name.'we’re sorry to see you go', 'description'=>'Just a quick reminder, deactivating your account means you’ll lose access to the following ongoing activity.', 'ui_type'=>'card'];
+        $overvire_obj = ['title'=>$request->user()->profile->name.', we’re sorry to see you go', 'description'=>'Just a quick reminder, deactivating your account means you’ll lose access to the following ongoing activity.', 'ui_type'=>'card'];
 
         //get passbook activity
         $overvire_obj['elements'] = $this->get_user_passbook_details($request->user()->profile->id);
@@ -50,13 +52,13 @@ class AccountManagementOptionController extends Controller
             $post_obj = ['title'=>'Recent Ongoing Activity', 'sub_title'=>'Recent ongoing activity will be closed once you deactive your account.', 'ui_type'=>'list','elements'=>$user_posts];
             $data[] = $post_obj;
         }
-
+        
         //get company activity
-        $user_companies = $this->get_user_companies($request->user()->profile->id);
-        if(count($user_companies) > 0){
+        $user_companies = $this->get_user_companies($request->user()->id);
+        // if(count($user_companies) > 0){
             $user_company_obj = ['title'=>'Transfer Company Access', 'sub_title'=>'Transfer company access before deactiviting your account it will make company active after deactivition of account.', 'ui_type'=> 'comapny_action', 'elements'=>$user_companies];
             $data[] = $user_company_obj;
-        }
+        // }
         
         return $this->sendResponse($data);
     }
@@ -81,41 +83,46 @@ class AccountManagementOptionController extends Controller
         //polls
         $poll_list = Polling::where('profile_id',$profile_id)->where('is_expired',0)->whereNull('deleted_at')->get();
         foreach($poll_list as $poll){
-            $poll_obj = ['model_id'=>$poll->id, 'model_name'=>'polling', 'title'=>$poll->title, 'sub_tit;e'=>''];
+            $poll_obj = ['model_id'=>"$poll->id", 'model_name'=>'polling', 'title'=>$poll->title, 'sub_tit;e'=>''];
             $data[] = $poll_obj;
         }
 
         //surveys
         $survey_list = Surveys::where('profile_id',$profile_id)->where('state', config('constant.SURVEY_STATES.PUBLISHED'))->whereNull('deleted_at')->get();
         foreach($survey_list as $survey){
-            $survey_obj = ['model_id'=>$survey->id, 'model_name'=>'surveys', 'title'=>$survey->title, 'sub_tit;e'=>$survey->description];
+            $survey_obj = ['model_id'=>"$survey->id", 'model_name'=>'surveys', 'title'=>$survey->title, 'sub_tit;e'=>$survey->description];
             $data[] = $survey_obj;
         }
 
         //collaboration
         $collaborate_list = Collaborate::where('profile_id',$profile_id)->where('state', 1)->where('collaborate_type','collaborate')->whereNull('deleted_at')->get();
         foreach($collaborate_list as $collaborate){
-            $collaborate_obj = ['model_id'=>$collaborate->id, 'model_name'=>'collaborate', 'title'=>$collaborate->title, 'sub_tit;e'=>$collaborate->description];
+            $collaborate_obj = ['model_id'=>"$collaborate->id", 'model_name'=>'collaborate', 'title'=>$collaborate->title, 'sub_tit;e'=>$collaborate->description];
             $data[] = $collaborate_obj;
         }
 
         //private review
         $pr_list = Collaborate::where('profile_id',$profile_id)->where('state', 1)->where('collaborate_type','product-review')->whereNull('deleted_at')->get();
         foreach($pr_list as $private_collab){
-            $private_collab_obj = ['model_id'=>$private_collab->id, 'model_name'=>'product-review', 'title'=>$private_collab->title, 'sub_tit;e'=>$private_collab->description];
+            $private_collab_obj = ['model_id'=>"$private_collab->id", 'model_name'=>'product-review', 'title'=>$private_collab->title, 'sub_tit;e'=>$private_collab->description];
             $data[] = $private_collab_obj;
         }
         
         return $data;
     }
 
-    function get_user_companies($profile_id){
+    function get_user_companies($user_id){
         $data = [];
-
-        $company_list = Company::where('user_id',$profile_id)->whereNull('deleted_at')->get();
         
+        $company_list = Company::where('user_id',$user_id)->whereNull('deleted_at')->get();
+
         foreach($company_list as $company){
-            $company_obj = ['model_id'=>$company->id, 'model_name'=>'company','name'=>$company->name, 'image_meta'=>json_decode($company->logo_meta, true)];
+            $admins = CompanyUser::getCompanyAdminIds($company->id);
+            if(count($admins) > 1){
+                $company_obj = ['model_id'=>$company->id, 'model_name'=>'company','name'=>$company->name, 'sub_title'=>count($admins).' Admins','image_meta'=>json_decode($company->logo_meta, true)];
+            }else{
+                $company_obj = ['model_id'=>$company->id, 'model_name'=>'company','name'=>$company->name, 'sub_title'=>'You are sole admin of the company, you will loose company adminship after 15 days if you delete your account. You must add an additional superadmin to keep the company accessible.','image_meta'=>json_decode($company->logo_meta, true)];
+            }
             $data[] = $company_obj;
         }
 
