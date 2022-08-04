c9ef1d09de09cd1a69144477b3e55f682932f80b
himani <v-himani@tagtaste.com>
2022-08-04 13:18:40 +0530

    reports sumarry api done modified

--- app/Http/Controllers/Api/Survey/SurveyController.php
+++ app/Http/Controllers/Api/Survey/SurveyController.php
@@ -1168,7 +1168,9 @@ class SurveyController extends Controller
             $counter++;
         }
 
-
+        if(!$checkIFExists["is_section"]){  //for normal survey
+            $getJson = $prepareNode;
+        }
 
         $this->messages = "Report Successful";
         $this->model = $getJson;
