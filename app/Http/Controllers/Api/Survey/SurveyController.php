d5e3141d6e2bdca9acd9640b60883fd0ef7c15b3
himani <v-himani@tagtaste.com>
2022-08-08 13:07:18 +0530

    final code modified

--- app/Http/Controllers/Api/Survey/SurveyController.php
+++ app/Http/Controllers/Api/Survey/SurveyController.php
@@ -1752,7 +1752,6 @@ class SurveyController extends Controller
         else{
             $finalRespnse =[];
             $finalRespnse["reports"]= $getJson;
-            $finalRespnse["answer_count"] = $prepareNode["answer_count"];
             $this->model = $finalRespnse;
 
         }
