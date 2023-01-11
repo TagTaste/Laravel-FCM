087a9e671b0030569efe5668604c6df227874b37
TagTaste <tagtaste@TagTastes-MacBook-Air.local>
2023-01-11 13:42:29 +0530

    questionnaire migrations

--- database/seeds/QuestionnaireFoodShotPlaceholderSeeder.php
+++ database/seeds/QuestionnaireFoodShotPlaceholderSeeder.php
@@ -14,9 +14,11 @@ class QuestionnaireFoodShotPlaceholderSeeder extends Seeder
     {
         //
         $data = [];
-        $data[] = ['title'=>'Selfie With Product','is_active'=>1, 'sort_order'=>3, 'created_at'=>Carbon::now(), 'updated_at'=>Carbon::now()];
-        $data[] = ['title'=>'Product Shot','is_active'=>1, 'sort_order'=>2, 'created_at'=>Carbon::now(), 'updated_at'=>Carbon::now()];
-        $data[] = ['title'=>'Product Bill Shot','is_active'=>1, 'sort_order'=>1, 'created_at'=>Carbon::now(), 'updated_at'=>Carbon::now()];
+        $data[] = ['title'=>'Selfie With Product','image'=>'https://s3.ap-south-1.amazonaws.com/static3.tagtaste.com/shelfi+with+product.png','is_active'=>1, 'sort_order'=>3, 'created_at'=>Carbon::now(), 'updated_at'=>Carbon::now()];
+
+        $data[] = ['title'=>'Product Shot','image'=>'https://s3.ap-south-1.amazonaws.com/static3.tagtaste.com/shelfi+with+product.png','is_active'=>1, 'sort_order'=>2, 'created_at'=>Carbon::now(), 'updated_at'=>Carbon::now()];
+        
+        $data[] = ['title'=>'Product Bill Shot','image'=>'https://s3.ap-south-1.amazonaws.com/static3.tagtaste.com/shelfi+with+product.png','is_active'=>1, 'sort_order'=>1, 'created_at'=>Carbon::now(), 'updated_at'=>Carbon::now()];
 
         \DB::table('questionnaire_food_shot_placeholders')->insert($data);
     }
