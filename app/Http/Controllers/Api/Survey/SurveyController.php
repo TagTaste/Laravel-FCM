749333ba1649d87f76372648e876070d707cac43
TagTaste <tagtaste@TagTastes-MacBook-Air.local>
2023-01-24 17:20:32 +0530

    text update

--- app/Http/Controllers/Api/Survey/SurveyController.php
+++ app/Http/Controllers/Api/Survey/SurveyController.php
@@ -653,7 +653,7 @@ class SurveyController extends Controller
                 if ($request->current_status ==  config("constant.SURVEY_APPLICANT_ANSWER_STATUS.COMPLETED") && isset($checkApplicant)) { //check if last section ,all mandate questions to be filled
 
                     if ($checkApplicant->application_status == config("constant.SURVEY_APPLICANT_ANSWER_STATUS.COMPLETED") && $sectionCount != 1) {     //1 CASE-WHEN APPLICANT COMPLETED ALREAY AND SECTION COUNT NOT 1
-                        return $this->sendNewError("Please fill ALL mandatory questions!");
+                        return $this->sendNewError("You missed some of the previou sections to fill. Please take a look back.");
                     } elseif ($checkApplicant->application_status == config("constant.SURVEY_APPLICANT_ANSWER_STATUS.INPROGRESS"))       //2 CASE-WHEN APPLICANT IN PROGRESS,CHECK DB ANSWERS
                     {
                         array_pop($sectionWithoutLast);   //remove last section ques
@@ -671,7 +671,7 @@ class SurveyController extends Controller
                         $questionsAnswered =  SurveyAnswers::where("survey_id", $request->survey_id)->where("profile_id", $request->user()->profile->id)->where("attempt", $last_attempt->attempt)->whereNull("deleted_at")->pluck("question_id")->toArray();
                         $mandateQuestions = array_values(array_filter($mandateQuestions));
                         if (!empty(array_diff($mandateQuestions, $questionsAnswered))) {
-                            return $this->sendNewError("Please fill ALL mandatory questions!");
+                            return $this->sendNewError("Please fill all the mandatory questions");
                         }
                     }
                 }
@@ -1373,7 +1373,7 @@ class SurveyController extends Controller
 
             //max ques id calculation
             $getOldJson = Surveys::where("id", "=", $isUpdation)->select("form_json", "is_section")->first()->toArray();
-
+            
             $maxQueId = 1;
             if ($isUpdation) {
                 if (isset($getOldJson["is_section"]) && $getOldJson["is_section"]) {
