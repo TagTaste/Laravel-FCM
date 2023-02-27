82ec8672b2d7bab76ba1ae55a5b2691a912aa8b1
Pooja Sharma <v-pooja@tagtaste.com>
2023-02-27 12:54:53 +0530

    migrations for profile badges

---
+++ database/migrations/2023_02_24_133525_create_badges_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBadgesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('badges', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('description');
+            $table->json("image_meta")->nullable();
+            $table->boolean('is_active')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('badges');
+    }
+}
