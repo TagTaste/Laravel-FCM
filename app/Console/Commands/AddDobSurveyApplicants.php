3b035aac49e691b798574b8529a817a2fe341120
Pooja Sharma <v-pooja@tagtaste.com>
2023-04-26 12:03:51 +0530

    scipts and migrations for dob

---
+++ app/Console/Commands/AddDobSurveyApplicants.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+
+class AddDobSurveyApplicants extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:addDobSurveyApplicant';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'to sync dob from profiles to survey_applicants';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $profiles = \DB::table("profiles")->whereNull("deleted_at")->get();
+        
+        foreach ($profiles as $profile) {
+            $id = $profile->id;
+            $dob = $profile->dob;
+            \DB::table("survey_applicants")->where('profile_id', $id)->update(["dob" => $dob]);    
+        }  
+    }
+ 
+        
+        
+}
