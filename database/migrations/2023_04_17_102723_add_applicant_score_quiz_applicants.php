dd4413d09db299c6a95cbe35d3f1c366fba42abb
nikhil <nikhil@tagtaste.com>
2023-04-28 07:55:57 +0530

    migration correction

--- database/migrations/2023_04_17_102723_add_applicant_score_quiz_applicants.php
+++ database/migrations/2023_04_17_102723_add_applicant_score_quiz_applicants.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddApplicantsScoreQuizApplicants extends Migration
+class AddApplicantScoreQuizApplicants extends Migration
 {
     /**
      * Run the migrations.
