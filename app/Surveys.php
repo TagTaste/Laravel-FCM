a083b3d1f13a7b7ee643d2d94ceed83eedeeb4c2
nikhil <nikhil@tagtaste.com>
2023-07-11 18:53:53 +0530

    update

--- app/Surveys.php
+++ app/Surveys.php
@@ -14,6 +14,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Redis;
 use App\Payment\PaymentDetails;
 use App\Payment\PaymentLinks;
+use App\surveyApplicants;
 
 class Surveys extends Model implements Feedable
 {
@@ -107,10 +108,19 @@ class Surveys extends Model implements Feedable
         // $meta['review_param'] = ["survey_id" => $this->id,"profile"=>$profileId];
         $payment = PaymentDetails::where("model_type", "Survey")->where("model_id", $this->id)->where("is_active", 1)->first();
 
+
         $meta['isPaid'] = PaymentHelper::getisPaidMetaFlag($payment);
         $meta['isReviewed'] = (!empty($reviewed) ? true : false);
 
         $meta['isInterested'] = ((!empty($reviewed)) ? true : false);
+
+        $isReviewed = surveyApplicants::where("survey_id",$this->id)->where("profile_id",$profileId)->whereNull('deleted_at')->first();
+        
+        $meta['isInterested'] = false;
+        if($isReviewed != null){
+            $meta['isInterested'] = true;
+        }
+        
         $k = Redis::get("surveys:application_status:$this->id:profile:$profileId");
         $meta['applicationStatus'] = $k !== null ? (int)$k : null;
         $meta['submissionCount'] = (!empty($reviewed) ? $reviewed->attempt : 0);
@@ -138,6 +148,14 @@ class Surveys extends Model implements Feedable
         $meta['isPaid'] = PaymentHelper::getisPaidMetaFlag($payment);
 
         $meta['isInterested'] = ((!empty($reviewed)) ? true : false);
+
+        $isReviewed = surveyApplicants::where("survey_id",$this->id)->where("profile_id",$profileId)->whereNull('deleted_at')->first();
+
+        $meta['isInterested'] = false;
+        if($isReviewed != null){
+            $meta['isInterested'] = true;
+        }
+
         $payment = PaymentDetails::where("model_type", "Survey")->where("model_id", $this->id)->where("is_active", 1)->first();
 
         $k = Redis::get("surveys:application_status:$this->id:profile:$profileId");
