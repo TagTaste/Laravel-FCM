6df9c2532b0fb02fc36993e8438ec35b61820f82
nikhil <nikhil@tagtaste.com>
2023-10-17 14:59:34 +0530

    tds migration

---
+++ database/migrations/2023_10_17_144418_add_tds_to_payment_links.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTdsToPaymentLinks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('payment_links', function (Blueprint $table){
+            $table->decimal('payout_amount', 13, 2);
+            $table->decimal('tds_amount', 13, 2)->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('payment_links', function (Blueprint $table){
+            $table->dropColumn('payout_amount');
+            $table->dropColumn('tds_amount');
+        });
+    }
+}
