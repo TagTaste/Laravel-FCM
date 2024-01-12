f8a1cb0e7c1d7d86fa244798b87c5b4d928786f3
Jaini Shah <jaini.shah@neosoftmail.com>
2024-01-12 16:01:49 +0530

    update

--- app/Traits/FlagReview.php
+++ app/Traits/FlagReview.php
@@ -18,11 +18,11 @@ trait FlagReview
         // review flagging based on some conditions
         $flag_reasons = FlagReason::get();
         foreach($flag_reasons as $reason){
-            $reason_conditons = $reason->conditions()->pluck('condition_value', 'condition_slug')->toArray();
+            $reason_conditions = $reason->conditions()->pluck('condition_value', 'condition_slug')->toArray();
             $flag_reason_data = ['model_id' => $model_id, 'flag_reason_id' => $reason->id, 'model' => $model];
             if($reason->slug == 'review_duration'){
-                // Check if the duration is less than 3 mins OR greater than 30 mins
-                if (($duration < $reason_conditons['min_duration']) || ($duration > $reason_conditons['max_duration'])) {
+                // Check if the duration is less than 3 mins OR greater than 45 mins
+                if (($duration < $reason_conditions['min_duration']) || ($duration > $reason_conditions['max_duration'])) {
                     // Add flagging reason
                     $this->addModelFlagReasons($flag_reason_data);
                     $flag = true;
@@ -30,8 +30,8 @@ trait FlagReview
             }
 
             if($reason->slug == 'review_start_time'){
-                // Check if the time is greater than 8 PM OR less than 10 AM 
-                if (($start_time >= $reason_conditons['max_start_time']) || ($start_time < $reason_conditons['min_start_time'])) {
+                // Check if the time is greater than 10 PM OR less than 8 AM 
+                if (($start_time > $reason_conditions['max_start_time']) || ($start_time < $reason_conditions['min_start_time'])) {
                     // Add flagging reason
                     $this->addModelFlagReasons($flag_reason_data);
                     $flag = true;
